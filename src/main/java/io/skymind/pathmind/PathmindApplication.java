c5280f5d5e662d6e6f2ce3d26a94f9bcc8b06961
FollowSteph <steph@followsteph.com>
2019-08-23 16:31:09 -0400

    Project setup

---
+++ src/main/java/io/skymind/pathmind/PathmindApplication.java
@@ -0,0 +1,13 @@
+package io.skymind.pathmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathmindApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(PathmindApplication.class, args);
+	}
+}
