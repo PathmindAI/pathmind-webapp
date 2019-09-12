3e52282f15968cbd56f739381ad5b43b457e7d20
Slin Lee <slin@skymind.io>
2019-09-12 16:54:13 -0700

    Polish login form
    - Adds the field labels to the form.
    - Adds a disclaimer about terms of service. (TODO: Add links)
    - Adds forgot password link (TODO: hook this up)

--- src/main/java/io/skymind/pathmind/ui/views/LoginView.java
+++ src/main/java/io/skymind/pathmind/ui/views/LoginView.java
@@ -26,10 +26,9 @@ public class LoginView extends LoginOverlay implements BeforeEnterObserver // , 
 		LoginI18n loginForm = LoginI18n.createDefault();
 		loginForm.setHeader(new LoginI18n.Header());
 		loginForm.getHeader().setTitle("Pathmind");
-		loginForm.setForm(new LoginI18n.Form());
-		loginForm.getForm().setSubmit("Sign in >");
+		loginForm.setAdditionalInformation("By clicking Log In, you agree to Pathmind's Terms of Use and Privacy Policy");
+		loginForm.getForm().setUsername("Email");
 		setI18n(loginForm);
-		setForgotPasswordButtonVisible(false);
 
 		addLoginListener(e -> handleLogin(e));
 	}
