459eba497df3ee35a7563fa1ea1485e4150ddb4b
venkat-nidrive <venkat0011@gmail.com>
2019-09-17 14:24:36 +0900

    extract jar file fix.

--- src/main/java/io/skymind/pathmind/services/project/AnylogicFileChecker.java
+++ src/main/java/io/skymind/pathmind/services/project/AnylogicFileChecker.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -41,16 +42,42 @@ public class AnylogicFileChecker implements FileChecker {
         ZipFile zipfile = null;
         String searchFileName = "model.jar";
         File zippedFile = file;
-        File outputFolder = new File("C:/fileunzip/");
-        File unZippedFolderDetails;
+
+        File unZippedJar =null;
         boolean isJarExist = false;
         try {
             zipfile = new ZipFile(file);
             anylogicFileCheckResult.setCorrectFileType(true);
-            unZippedFolderDetails = unzipFile(zippedFile, outputFolder);
-            log.info("Unzipped path file contents :" + unZippedFolderDetails.getAbsolutePath());
-            checkJarFile(unZippedFolderDetails, anylogicFileCheckResult);
-            //anylogicFileCheckResult.setZipContentFileNames(fileNameList);
+           try {
+                ZipFile zipFile = new ZipFile(zippedFile);
+                Enumeration<?> enu = zipFile.entries();
+                List<String> fileNameList= new ArrayList<>();
+                while (enu.hasMoreElements()) {
+                    ZipEntry zipEntry = (ZipEntry) enu.nextElement();
+
+                    String name = zipEntry.getName();
+
+                    log.info(zipEntry.getName());
+                    fileNameList.add(zipEntry.getName());
+
+
+                    if(zipEntry.getName().toLowerCase().indexOf(searchFileName) != -1){
+                        unZippedJar= unzipFile(zippedFile,searchFileName);
+                        log.error("modelJar PATH file : " + unZippedJar.getAbsolutePath());
+                        checkJarFile(unZippedJar,anylogicFileCheckResult);
+                        isJarExist=true;
+                    }
+                }
+                anylogicFileCheckResult.setZipContentFileNames(fileNameList);
+                zipFile.close();
+                if(isJarExist) anylogicFileCheckResult.setModelJarFilePresent(true);
+
+                checkHelpers(unZippedJar,anylogicFileCheckResult);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
 
         } catch (ZipException ioe) {
             log.error("Error opening zip file" + ioe);
@@ -73,12 +100,12 @@ public class AnylogicFileChecker implements FileChecker {
 
 
     // To Check if the model.jar is a valid
-    private void checkJarFile(File modelJar, AnylogicFileCheckResult anylogicFileCheckResult) {
+    private void checkJarFile(File unZippedJar, AnylogicFileCheckResult anylogicFileCheckResult) {
         // To Check if the Jar file is a valid
         ZipFile jarFile = null;
 
         try {
-            jarFile = new ZipFile(modelJar);
+            jarFile = new ZipFile(unZippedJar);
             log.error("jarFile Name : " + jarFile.getName());
             anylogicFileCheckResult.setModelJarFilePresent(true);
         } catch (ZipException ioe) {
@@ -102,49 +129,52 @@ public class AnylogicFileChecker implements FileChecker {
     // To check the existence of pathmind helpers check
     private void checkHelpers(File file, AnylogicFileCheckResult anylogicFileCheckResult) {
 
+        //anylogicFileCheckResult.setDefinedHelpers();
+
     }
 
 
-    private File unzipFile(File zippedFile, File outputFolder) throws IOException {
+    private File unzipFile(File zippedFile,String searchFileName) throws IOException {
+        File jarTempDir = null;
           try {
             ZipFile zipFile = new ZipFile(zippedFile);
             Enumeration<?> enu = zipFile.entries();
-            while (enu.hasMoreElements()) {
+
+              while (enu.hasMoreElements()) {
                 ZipEntry zipEntry = (ZipEntry) enu.nextElement();
 
                 String name = zipEntry.getName();
                 long size = zipEntry.getSize();
                 long compressedSize = zipEntry.getCompressedSize();
-                System.out.printf("name: %-20s | size: %6d | compressed size: %6d\n",
+                log.info("name: %-20s | size: %6d | compressed size: %6d\n",
                         name, size, compressedSize);
 
-                File file = new File(name);
-                if (name.endsWith("/")) {
-                    file.mkdirs();
-                    continue;
-                }
+                if(zipEntry.getName().toLowerCase().indexOf(searchFileName) != -1){
+                    String dirName = UUID.randomUUID().toString();
+                   jarTempDir = new File( Files.createTempDirectory(dirName).toString() );
+                    if (!jarTempDir.exists()) {
+                        jarTempDir.mkdir();
+                    }
+                    InputStream inputStream = zipFile.getInputStream(zipEntry);
+                    jarTempDir = new File(jarTempDir + "/" + searchFileName);
+                    FileOutputStream fos = new FileOutputStream(jarTempDir);
 
-                File parent = file.getParentFile();
-                if (parent != null) {
-                    parent.mkdirs();
-                }
+                    byte[] bytes = new byte[1024];
+                    int length;
+                    while ((length = inputStream.read(bytes)) >= 0) {
+                        fos.write(bytes, 0, length);
+                    }
+                    inputStream.close();
 
-                InputStream is = zipFile.getInputStream(zipEntry);
-                FileOutputStream fos = new FileOutputStream(file);
-                byte[] bytes = new byte[1024];
-                int length;
-                while ((length = is.read(bytes)) >= 0) {
-                    fos.write(bytes, 0, length);
-                }
-                is.close();
-                fos.close();
+                    fos.close();
 
+                }
             }
             zipFile.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return zippedFile;
+        return jarTempDir;
     }
 }
 
