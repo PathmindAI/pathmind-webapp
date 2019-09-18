9dcde8f07e9d109f0a743f7870716f8868c25f24
Paul Dubs <paul.dubs@gmail.com>
2019-09-18 13:04:16 +0200

    properly handle context shutdown

--- src/main/java/io/skymind/pathmind/services/training/ExecutionProgressWatcher.java
+++ src/main/java/io/skymind/pathmind/services/training/ExecutionProgressWatcher.java
@@ -2,29 +2,29 @@ package io.skymind.pathmind.services.training;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ExecutionProgressWatcher implements ApplicationListener<ContextRefreshedEvent> {
+public class ExecutionProgressWatcher implements ApplicationListener<ContextRefreshedEvent>, DisposableBean {
     private static Logger log = LoggerFactory.getLogger(ExecutionProgressWatcher.class);
     private Runner runner = null;
-    private Thread thread = null;
-    private final ExecutionProgressUpdater updater;
 
-    public ExecutionProgressWatcher(ExecutionProgressUpdater updater) {
-        this.updater = updater;
-
+    public void destroy(){
+        if (runner != null) {
+            log.info("Stopping...");
+            runner.stop();
+        }
     }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-        if (runner != null) {
-            runner.stop();
-        }
+        log.info("Restarting with new context...");
+        ExecutionProgressUpdater updater = event.getApplicationContext().getBean(ExecutionProgressUpdater.class);
         runner = new Runner(updater);
-        thread = new Thread(runner);
+        Thread thread = new Thread(runner);
         thread.setName("ExecutionProgressWatcher");
         thread.setDaemon(true);
         thread.start();
@@ -52,7 +52,11 @@ public class ExecutionProgressWatcher implements ApplicationListener<ContextRefr
                 final long nextRun = lastRun + updateInterval;
                 try {
                     if (nextRun <= System.currentTimeMillis()) {
-                        updater.update();
+                        try {
+                            updater.update();
+                        } catch (Exception e) {
+                            log.error("Exception during progress update", e);
+                        }
                         lastRun = System.currentTimeMillis();
                     } else {
                         Thread.sleep(nextRun - System.currentTimeMillis());
