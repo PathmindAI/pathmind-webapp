d80f5dd499c61a59302c4c974be239426a493389
kepricon <kepricon@gmail.com>
2019-09-26 19:41:02 -0700

    add object mapper holder

--- src/main/java/io/skymind/pathmind/PathmindApplication.java
+++ src/main/java/io/skymind/pathmind/PathmindApplication.java
@@ -1,15 +1,16 @@
 package io.skymind.pathmind;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.skymind.pathmind.bus.PathmindBusEvent;
-import io.skymind.pathmind.services.project.ProjectFileCheckService;
+import io.skymind.pathmind.utils.ObjectMapperHolder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.UnicastProcessor;
 
-import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -32,5 +33,10 @@ public class PathmindApplication
 	@Bean
 	public ExecutorService checkerExecutorService(@Value("${pathmind.filecheck.poolsize}") int poolSize) {
 			return Executors.newFixedThreadPool(poolSize);
+	}
+
+	@Bean
+	public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+		return ObjectMapperHolder.getJsonMapper();
 	}
 }
