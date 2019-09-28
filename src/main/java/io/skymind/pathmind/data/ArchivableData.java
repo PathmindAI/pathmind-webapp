6558ced72c8dca922f9dcf802cfe5251fabf3ca4
FollowSteph <steph@followsteph.com>
2019-09-27 20:21:56 -0400

    Should not be abstract

--- src/main/java/io/skymind/pathmind/data/ArchivableData.java
+++ src/main/java/io/skymind/pathmind/data/ArchivableData.java
@@ -3,7 +3,7 @@ package io.skymind.pathmind.data;
 /**
  * This could also be an interface but honestly it would all be the same code repeated so I went with an abstract class.
  */
-public abstract class ArchivableData extends Data
+public class ArchivableData extends Data
 {
 	// TODO -> Paul -> Please hook up the value archived value in the database.
 	private boolean archived = false;
