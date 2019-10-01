624e97e0d3047a46dac9f6131d8b1edb16650b45
kepricon <kepricon@gmail.com>
2019-10-01 16:39:30 -0700

    set the proper value for training list

--- src/main/java/io/skymind/pathmind/ui/views/experiment/components/TrainingsListPanel.java
+++ src/main/java/io/skymind/pathmind/ui/views/experiment/components/TrainingsListPanel.java
@@ -1,24 +1,34 @@
 package io.skymind.pathmind.ui.views.experiment.components;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridSingleSelectionModel;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import io.skymind.pathmind.constants.Algorithm;
+import io.skymind.pathmind.constants.RunStatus;
+import io.skymind.pathmind.constants.RunType;
 import io.skymind.pathmind.data.Experiment;
 import io.skymind.pathmind.data.Policy;
+import io.skymind.pathmind.services.training.progress.Progress;
+import io.skymind.pathmind.services.training.progress.ProgressInterpreter;
 import io.skymind.pathmind.ui.utils.GuiUtils;
 import io.skymind.pathmind.ui.views.policy.components.PolicySearchBox;
+import io.skymind.pathmind.utils.ObjectMapperHolder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.function.Consumer;
 
 public class TrainingsListPanel extends VerticalLayout
 {
+	private Logger log = LogManager.getLogger(TrainingsListPanel.class);
+
 	private PolicySearchBox searchBox;
 	private Grid<Policy> grid;
 
 	private Experiment experiment;
+	private ObjectMapper objectMapper = ObjectMapperHolder.getJsonMapper();
 
 	public TrainingsListPanel()
 	{
@@ -34,36 +44,80 @@ public class TrainingsListPanel extends VerticalLayout
 		setSizeFull();
 	}
 
+	private String getRunStatus(Policy policy) {
+		if (policy.getRun().getRunTypeEnum().equals(RunType.DiscoverRun) && policy.getRun().getStatusEnum().equals(RunStatus.Running)) {
+			try {
+				return objectMapper.readValue(policy.getProgress(), Progress.class).getStoppedAt() != null ? RunStatus.Completed.name() : RunStatus.Running.name();
+			} catch (Exception e) {
+				log.debug(e.getMessage(), e);
+				return null;
+			}
+		} else {
+			return policy.getRun().getStatusEnum().name();
+		}
+	}
+
 	private void setupGrid()
 	{
 		grid = new Grid<>();
 
 		// TODO -> DH -> Cases #83, #83, #85, and #86 -> Where do specific columns come from?
-		grid.addColumn(policy -> policy.getRun().getStatusEnum().name())
+		grid.addColumn(policy -> getRunStatus(policy))
 				.setHeader("Status")
 				.setAutoWidth(true)
 				.setSortable(true);
-		grid.addColumn(policy -> "Completed TODO")
+
+		grid.addColumn(policy -> {
+			if (!RunStatus.Completed.name().equalsIgnoreCase(getRunStatus(policy))) {
+				return "Not Completed";
+			}
+
+			try {
+				return objectMapper.readValue(policy.getProgress(), Progress.class).getStoppedAt();
+			} catch (Exception e) {
+				log.debug(e.getMessage(), e);
+				return null;
+			}
+		})
 				.setHeader("Completed")
 				.setAutoWidth(true)
 				.setSortable(true);
-		grid.addColumn(policy -> "Score todo")
+
+		grid.addColumn(policy -> {
+			try {
+				return policy.getScores().get(policy.getScores().size() - 1);
+			} catch (Exception e) {
+				log.debug(e.getMessage(), e);
+				return null;
+			}
+		})
 				.setHeader("Score")
 				.setAutoWidth(true)
 				.setSortable(true);
-		grid.addColumn(Policy::getName)
+
+		grid.addColumn(policy -> {
+			try {
+				return policy.getName().split("_")[2];
+			} catch (Exception e) {
+				log.debug(e.getMessage(), e);
+				return null;
+			}
+		})
 				.setHeader("Policy")
 				.setAutoWidth(true)
 				.setSortable(true);
+
 		grid.addColumn(policy -> policy.getRun().getRunTypeEnum().name())
 				.setHeader("Run Type")
 				.setAutoWidth(true)
 				.setSortable(true);
-		grid.addColumn(policy -> Algorithm.DQN)
+
+		grid.addColumn(policy -> ProgressInterpreter.interpretKey(policy.getName()).getAlgorithm())
 				.setHeader("Algorithm")
 				.setAutoWidth(true)
 				.setSortable(true);
-		grid.addColumn(policy -> "Notes TODO")
+
+		grid.addColumn(policy -> ProgressInterpreter.interpretKey(policy.getName()).getHyperParameters().toString().replaceAll("(\\{|\\})", ""))
 				.setHeader("Notes")
 				.setAutoWidth(true)
 				.setSortable(true);
