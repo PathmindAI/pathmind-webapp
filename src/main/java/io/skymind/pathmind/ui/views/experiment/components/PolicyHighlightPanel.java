f9a31c329e94512bb0e39f8d624652ad1b9538ac
kepricon <kepricon@gmail.com>
2019-10-01 19:20:11 -0700

    set policy highlight panel(cont.)

--- src/main/java/io/skymind/pathmind/ui/views/experiment/components/PolicyHighlightPanel.java
+++ src/main/java/io/skymind/pathmind/ui/views/experiment/components/PolicyHighlightPanel.java
@@ -3,11 +3,15 @@ package io.skymind.pathmind.ui.views.experiment.components;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import io.skymind.pathmind.constants.Algorithm;
 import io.skymind.pathmind.data.Policy;
+import io.skymind.pathmind.services.training.progress.ProgressInterpreter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class PolicyHighlightPanel extends VerticalLayout
 {
+	private Logger log = LogManager.getLogger(PolicyHighlightPanel.class);
+
 	private Label policyLabel = new Label();
 	private Label scoreLabel = new Label();
 	private Label algorithmLabel = new Label();
@@ -28,9 +32,25 @@ public class PolicyHighlightPanel extends VerticalLayout
 	}
 
 	public void update(Policy policy) {
-		policyLabel.setText(policy.getName());
-		// TODO -> How do we calculate?
-//		scoreLabel.setText(policy.getProgress());
-		algorithmLabel.setText(Algorithm.DQN.toString());
+		String policyName = null;
+		try {
+			String[] split = policy.getName().split("_");
+//			policyName = split[1] + "_" + split[2];
+			policyName = "#" + split[2];
+		} catch (Exception e) {
+			log.debug(e.getMessage(), e);
+		}
+
+		policyLabel.setText(policyName);
+
+		String policyScore = null;
+		try {
+			policyScore = policy.getScores().get(policy.getScores().size() - 1).toString();
+		} catch (Exception e) {
+			log.debug(e.getMessage(), e);
+		}
+
+		scoreLabel.setText(policyScore);
+		algorithmLabel.setText(ProgressInterpreter.interpretKey(policy.getName()).getAlgorithm());
 	}
 }
