204e9b1ec1e24996e20c3ba1b70d2d80e1d0921d
FollowSteph <steph@followsteph.com>
2019-10-04 22:11:39 -0400

    Fixed the back button on ExperimentsView. Closes #172.

--- src/main/java/io/skymind/pathmind/db/repositories/ExperimentRepository.java
+++ src/main/java/io/skymind/pathmind/db/repositories/ExperimentRepository.java
@@ -5,10 +5,12 @@ import io.skymind.pathmind.data.Model;
 import io.skymind.pathmind.data.Project;
 import org.jooq.DSLContext;
 import org.jooq.Record;
+import org.jooq.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.skymind.pathmind.data.db.tables.Experiment.EXPERIMENT;
 import static io.skymind.pathmind.data.db.tables.Model.MODEL;
@@ -41,10 +43,24 @@ public class ExperimentRepository
     }
 
 	public List<Experiment> getExperimentsForModel(long modelId) {
-		return dslContext
-				.selectFrom(EXPERIMENT)
+		Result<?> result = dslContext
+				.select(EXPERIMENT.asterisk())
+				.select(MODEL.ID, MODEL.NAME, MODEL.GET_OBSERVATION_FOR_REWARD_FUNCTION)
+				.select(PROJECT.ID, PROJECT.NAME, PROJECT.PATHMIND_USER_ID)
+				.from(EXPERIMENT)
+				.leftJoin(MODEL)
+					.on(MODEL.ID.eq(EXPERIMENT.MODEL_ID))
+				.leftJoin(PROJECT)
+					.on(PROJECT.ID.eq(MODEL.PROJECT_ID))
 				.where(EXPERIMENT.MODEL_ID.eq(modelId))
-				.fetchInto(Experiment.class);
+				.fetch();
+
+        return result.stream().map(record -> {
+        	Experiment experiment = record.into(EXPERIMENT).into(Experiment.class);
+        	experiment.setModel(record.into(MODEL).into(Model.class));
+        	experiment.setProject(record.into(PROJECT).into(Project.class));
+			return experiment;
+		}).collect(Collectors.toList());
 	}
 
 	public void archive(long experimentId, boolean isArchive) {
