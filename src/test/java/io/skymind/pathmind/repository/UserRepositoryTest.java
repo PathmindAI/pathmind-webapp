78af4132e6bb9bc547ba2c5ea1c59c3b5dc2710a
Vesa Nieminen <vesa@vaadin.com>
2019-10-14 14:25:56 +0300

    #279 Add change password method to UserService

--- src/test/java/io/skymind/pathmind/repository/UserRepositoryTest.java
+++ src/test/java/io/skymind/pathmind/repository/UserRepositoryTest.java
@@ -7,19 +7,24 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Ignore
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 public class UserRepositoryTest extends PathmindApplicationTests
 {
 
 	@Autowired
 	UserRepository userRepository;
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
 	@Test
 	public void findEmailIgnoreCase()
 	{
 		PathmindUser pathmindUser = userRepository.findByEmailIgnoreCase("STEPH@followsteph.com");
-		Assert.assertEquals("steph@followsteph.com", pathmindUser.getEmail());
+		assertEquals("steph@followsteph.com", pathmindUser.getEmail());
 	}
 
 	@Test
@@ -27,7 +32,20 @@ public class UserRepositoryTest extends PathmindApplicationTests
 	{
 		PathmindUser pathmindUser = userRepository.findByEmailIgnoreCase("steph@followsteph.com");
 		PathmindUser byId = userRepository.findById(pathmindUser.getId());
-		Assert.assertEquals(pathmindUser.getEmail(), byId.getEmail());
+		assertEquals(pathmindUser.getEmail(), byId.getEmail());
+	}
+
+	@Test
+	public void changePassword()
+	{
+		PathmindUser pathmindUser = userRepository.findByEmailIgnoreCase("steph@followsteph.com");
+		String myNewPassw0rd1 = "myNewPassw0rd1";
+		boolean isPasswordChanged = userRepository.changePassword(pathmindUser.getId(), myNewPassw0rd1);
+		Assert.assertTrue(isPasswordChanged);
+
+		pathmindUser = userRepository.findByEmailIgnoreCase("steph@followsteph.com");
+		assertNotEquals(pathmindUser.getPassword(), myNewPassw0rd1);
+		passwordEncoder.matches(myNewPassw0rd1, pathmindUser.getPassword());
 	}
 
 }
