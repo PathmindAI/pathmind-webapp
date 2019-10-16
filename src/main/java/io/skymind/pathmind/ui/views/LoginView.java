0d43095e7e745aab8fe97eb13b87b9b55ea90808
Slin Lee <slin@skymind.io>
2019-10-16 13:52:18 -0700

    Add a section on the login page showing build
    This is hard coded for now and in a temporary space.

--- src/main/java/io/skymind/pathmind/ui/views/LoginView.java
+++ src/main/java/io/skymind/pathmind/ui/views/LoginView.java
@@ -44,8 +44,8 @@ public class LoginView extends LoginOverlay
 		setId("pathmind-login");
 		LoginI18n loginForm = LoginI18n.createDefault();
 		loginForm.setHeader(new LoginI18n.Header());
-		loginForm.getHeader().setTitle("Pathmind DevOps V2");
-		loginForm.setAdditionalInformation("By clicking Log In, you agree to Pathmind's Terms of Use and Privacy Policy");
+		loginForm.getHeader().setTitle("Pathmind");
+		loginForm.setAdditionalInformation("By clicking Log In, you agree to Pathmind's Terms of Use and Privacy Policy. Build - Test");
 		loginForm.getForm().setUsername("Email");
 		setI18n(loginForm);
 
