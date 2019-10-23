c0eae243da8da4336f7fb5374602e9feaf1522b2
Vladyslav Gutsul <vladyslav@vaadin.com>
2019-10-23 11:41:49 +0300

    * added links to Terms of Service and Privacy Policy * make login view look like other dialog views * improved perfomanse of /sign-up link

--- src/main/java/io/skymind/pathmind/ui/views/LoginView.java
+++ src/main/java/io/skymind/pathmind/ui/views/LoginView.java
@@ -24,6 +24,7 @@ import io.skymind.pathmind.security.SecurityUtils;
 import io.skymind.pathmind.ui.plugins.IntercomIntegrationPlugin;
 import io.skymind.pathmind.ui.utils.VaadinUtils;
 import io.skymind.pathmind.ui.views.account.ResetPasswordView;
+import io.skymind.pathmind.ui.views.account.SignUpView;
 import io.skymind.pathmind.ui.views.dashboard.DashboardView;
 import io.skymind.pathmind.ui.views.project.NewProjectView;
 import io.skymind.pathmind.utils.PathmindUtils;
@@ -32,6 +33,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Route("login")
 @Theme(Lumo.class)
 @CssImport(value = "./styles/components/vaadin-button.css", themeFor = "vaadin-button")
+@CssImport(value = "./styles/views/pathmind-dialog-view.css")
 @CssImport(value = "./styles/views/login-view-styles.css")
 @CssImport(value = "./styles/components/vaadin-login-form-wrapper.css", themeFor = "vaadin-login-form-wrapper")
 public class LoginView extends HorizontalLayout
@@ -59,11 +61,22 @@ public class LoginView extends HorizontalLayout
 		error.setClassName("error-message");
 		error.setVisible(false);
 
+		Div innerContent = new Div();
+		innerContent.setClassName("inner-content");
+		innerContent.add(error, createLoginForm(), createSignUp());
+
+		Div policy = new Div();
+		policy.addClassName("policy");
+		policy.add(new Span("By clicking Log In, you agree to Pathmind's "),
+				new Anchor("/terms-of-use", "Terms of Use"),
+				new Span(" and "),
+				new Anchor("/private-policy", "Privacy Policy"),
+				new Span("."));
 
 		Div loginPanel = new Div();
 		add(loginPanel);
-		loginPanel.setClassName("login-panel");
-		loginPanel.add(welcome, img, title, error, createLoginForm(), createSignUp());
+		loginPanel.setClassName("content");
+		loginPanel.add(welcome, img, title, innerContent, policy);
 
 		intercomIntegrationPlugin.addPluginToPage();
 	}
@@ -73,12 +86,11 @@ public class LoginView extends HorizontalLayout
 		dontHaveAccount.getStyle().set("color", "var(--pm-secondary-text-color)");
 		Button start = new Button("Get started");
 		start.setThemeName("tertiary");
-		Anchor signUp = new Anchor("/sign-up");
-		signUp.add(start);
+		start.addClickListener(e -> UI.getCurrent().navigate(SignUpView.class));
 
 		Div signUpCont = new Div();
 		signUpCont.getStyle().set("flex-shrink", "0");
-		signUpCont.add(dontHaveAccount, signUp);
+		signUpCont.add(dontHaveAccount, start);
 
 		return signUpCont;
 	}
@@ -87,7 +99,6 @@ public class LoginView extends HorizontalLayout
 		LoginI18n loginI18n = LoginI18n.createDefault();
 		loginI18n.setHeader(new LoginI18n.Header());
 		loginI18n.getHeader().setTitle("");
-		loginI18n.setAdditionalInformation("By clicking Log In, you agree to Pathmind's Terms of Use and Privacy Policy. Build - Test");
 		loginI18n.getForm().setUsername("Email");
 		loginI18n.getForm().setSubmit("Sign in");
 		loginI18n.getForm().setForgotPassword("Forgot your password?");
