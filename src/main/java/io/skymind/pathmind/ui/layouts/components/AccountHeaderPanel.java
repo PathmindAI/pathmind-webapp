5a22dc49bb1479f880ddcc2586f3b82d097cabe1
Vladyslav Gutsul <vladyslav@vaadin.com>
2019-10-23 14:51:01 +0300

    merged latest changes of account header

--- src/main/java/io/skymind/pathmind/ui/layouts/components/AccountHeaderPanel.java
+++ src/main/java/io/skymind/pathmind/ui/layouts/components/AccountHeaderPanel.java
@@ -1,18 +1,17 @@
 package io.skymind.pathmind.ui.layouts.components;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.contextmenu.MenuItem;
-import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.server.VaadinServlet;
 
-import io.skymind.pathmind.security.PathmindUserDetails;
-import io.skymind.pathmind.security.SecurityUtils;
+import io.skymind.pathmind.bus.PathmindBusEvent;
+import io.skymind.pathmind.bus.utils.PolicyBusEventUtils;
+import io.skymind.pathmind.data.PathmindUser;
+import io.skymind.pathmind.ui.utils.PushUtils;
 import io.skymind.pathmind.ui.utils.WrapperUtils;
 import io.skymind.pathmind.ui.views.account.AccountView;
 import reactor.core.publisher.Flux;
@@ -23,9 +22,11 @@ public class AccountHeaderPanel extends HorizontalLayout
 	private Span usernameLabel = new Span();
 
 	public AccountHeaderPanel(PathmindUser user, Flux<PathmindBusEvent> consumer) {
+		addClassName("nav-account-links");
+
 		MenuBar menuBar = new MenuBar();
-		menuBar.setThemeName("tertiary");
 		add(menuBar);
+		menuBar.setThemeName("tertiary");
 		menuBar.addClassName("account-menu");
 
 		MenuItem account = menuBar.addItem(createItem(new Icon(VaadinIcon.USER), user));
@@ -41,8 +42,6 @@ public class AccountHeaderPanel extends HorizontalLayout
 		HorizontalLayout hl = WrapperUtils.wrapWidthFullHorizontal(icon, usernameLabel);
 		return hl;
 	}
-
-
 
 	private void subscribeToEventBus(Flux<PathmindBusEvent> consumer) {
 		PolicyBusEventUtils.consumerBusEventBasedOnUserUpdate(
