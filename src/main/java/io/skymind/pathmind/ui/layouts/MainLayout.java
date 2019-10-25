1fb482e6f13d8be156c6e88485e27c9bb5f80312
Vladyslav Gutsul <vladyslav@vaadin.com>
2019-10-25 16:44:56 +0300

    Broken styles fixed

--- src/main/java/io/skymind/pathmind/ui/layouts/MainLayout.java
+++ src/main/java/io/skymind/pathmind/ui/layouts/MainLayout.java
@@ -29,7 +29,6 @@ import reactor.core.publisher.Flux;
 @CssImport(value = "./styles/views/experiment-view.css")
 @CssImport(value = "./styles/views/experiments-view.css")
 @CssImport(value = "./styles/views/new-experiment-view.css")
-@CssImport(value = "./styles/views/sign-up-view.css", id = "sign-up-view-styles")
 @CssImport(value = "./styles/views/pathmind-dialog-view.css", id = "pathmind-dialog-view")
 @Theme(Lumo.class)
 public class MainLayout extends AppLayout implements PageConfigurator
