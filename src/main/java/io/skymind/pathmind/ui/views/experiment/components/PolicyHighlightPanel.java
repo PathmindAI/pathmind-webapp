ac336bad9a1138f434b4fe6a87252073d92621be
FollowSteph <steph@followsteph.com>
2019-11-01 22:23:08 -0400

    Re-implementation of #229. Updates the right panel when there are new eventbus updates

--- src/main/java/io/skymind/pathmind/ui/views/experiment/components/PolicyHighlightPanel.java
+++ src/main/java/io/skymind/pathmind/ui/views/experiment/components/PolicyHighlightPanel.java
@@ -3,18 +3,28 @@ package io.skymind.pathmind.ui.views.experiment.components;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import io.skymind.pathmind.bus.PathmindBusEvent;
+import io.skymind.pathmind.bus.utils.PolicyBusEventUtils;
 import io.skymind.pathmind.data.Policy;
 import io.skymind.pathmind.data.utils.PolicyUtils;
 import io.skymind.pathmind.services.training.progress.ProgressInterpreter;
+import io.skymind.pathmind.ui.utils.PushUtils;
+import reactor.core.publisher.Flux;
 
 public class PolicyHighlightPanel extends VerticalLayout
 {
+	private Policy policy;
+
 	private Label policyLabel = new Label();
 	private Label scoreLabel = new Label();
 	private Label algorithmLabel = new Label();
 
-	public PolicyHighlightPanel()
+	private Flux<PathmindBusEvent> consumer;
+
+	public PolicyHighlightPanel(Flux<PathmindBusEvent> consumer)
 	{
+		this.consumer = consumer;
+
 		setWidthFull();
 		setPadding(false);
 
@@ -28,9 +38,27 @@ public class PolicyHighlightPanel extends VerticalLayout
 		add(formLayout);
 	}
 
-	public void update(Policy policy) {
+	public void update(Policy policy)
+	{
+		updateComponentsForPolicy(policy);
+		subscribeToEventBus(consumer);
+	}
+
+	private void updateComponentsForPolicy(Policy policy) {
+		this.policy = policy;
 		policyLabel.setText(PolicyUtils.getParsedPolicyName(policy));
 		scoreLabel.setText(PolicyUtils.getLastScore(policy));
 		algorithmLabel.setText(ProgressInterpreter.interpretKey(policy.getName()).getAlgorithm());
+	}
+
+	private Policy getPolicy() {
+		return policy;
+	}
+
+	private void subscribeToEventBus(Flux<PathmindBusEvent> consumer) {
+		PolicyBusEventUtils.consumerBusEventBasedOnPolicy(
+				consumer,
+				this::getPolicy,
+				updatedPolicy -> PushUtils.push(getUI().orElseGet(null), () -> updateComponentsForPolicy(updatedPolicy)));
 	}
 }
--- src/main/java/io/skymind/pathmind/ui/views/experiment/components/PolicyStatusDetailsPanel.java
+++ src/main/java/io/skymind/pathmind/ui/views/experiment/components/PolicyStatusDetailsPanel.java
@@ -1,16 +1,21 @@
 package io.skymind.pathmind.ui.views.experiment.components;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import io.skymind.pathmind.bus.PathmindBusEvent;
+import io.skymind.pathmind.bus.utils.PolicyBusEventUtils;
 import io.skymind.pathmind.constants.RunStatus;
 import io.skymind.pathmind.data.Policy;
 import io.skymind.pathmind.data.utils.PolicyUtils;
+import io.skymind.pathmind.ui.utils.PushUtils;
 import io.skymind.pathmind.ui.utils.WrapperUtils;
 import io.skymind.pathmind.utils.DateAndTimeUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 import java.util.Arrays;
 
@@ -19,18 +24,24 @@ public class PolicyStatusDetailsPanel extends VerticalLayout
 {
 	private static Logger log = LogManager.getLogger(PolicyStatusDetailsPanel.class);
 
+	private Policy policy;
+
 	private Label statusLabel = new Label(RunStatus.NotStarted.toString());
 	private Label runProgressLabel = new Label();
 	private Label runTypeLabel = new Label();
 	private Label elapsedTimeLabel = new Label();
 
-	public PolicyStatusDetailsPanel()
+	private Flux<PathmindBusEvent> consumer;
+
+	public PolicyStatusDetailsPanel(Flux<PathmindBusEvent> consumer)
 	{
+		this.consumer = consumer;
+
 		Label[] labels = Arrays.asList(
-			getElementLabel("Status"),
-			getElementLabel(""),
-			getElementLabel("Run Type"),
-			getElementLabel("Elapsed"))
+				getElementLabel("Status"),
+				getElementLabel(""),
+				getElementLabel("Run Type"),
+				getElementLabel("Elapsed"))
 				.stream().toArray(Label[]::new);
 
 		removeTopMargins(labels);
@@ -72,9 +83,31 @@ public class PolicyStatusDetailsPanel extends VerticalLayout
 
 	public void update(Policy policy)
 	{
+		updateComponentsForPolicy(policy);
+		subscribeToEventBus(UI.getCurrent(), consumer);
+	}
+
+	private void updateComponentsForPolicy(Policy policy) {
+		this.policy = policy;
 		statusLabel.setText(PolicyUtils.getRunStatus(policy));
 		runProgressLabel.setText(DateAndTimeUtils.formatDateAndTimeShortFormatter(PolicyUtils.getRunCompletedTime(policy)));
 		runTypeLabel.setText(policy.getRun().getRunTypeEnum().toString());
-        elapsedTimeLabel.setText(PolicyUtils.getElapsedTime(policy));
+		elapsedTimeLabel.setText(PolicyUtils.getElapsedTime(policy));
+	}
+
+	private Policy getPolicy() {
+		return policy;
+	}
+
+	// IMPORTANT NOTE -> I cannot use getUI() as requested in the PR 252 because PolicyUtils does not accept an optional and at this stage I want
+	// to minimize any other code impact. Please add the request for PushUtils to accept Optionals as a new github issues. Thank you.
+	private void subscribeToEventBus(UI ui, Flux<PathmindBusEvent> consumer) {
+		PolicyBusEventUtils.consumerBusEventBasedOnPolicy(
+				consumer,
+				this::getPolicy,
+				updatedPolicy -> PushUtils.push(ui, () -> {
+					updateComponentsForPolicy(updatedPolicy);
+					elapsedTimeLabel.setText(PolicyUtils.getElapsedTime(policy));
+				}));
 	}
 }
