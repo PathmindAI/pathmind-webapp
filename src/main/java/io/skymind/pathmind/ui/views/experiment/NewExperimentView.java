c5928c8e0910407f733974ddf0d94b1c909737b0
Slin Lee <slin@skymind.io>
2019-11-06 14:06:11 -0800

    Update src/main/java/io/skymind/pathmind/ui/views/experiment/NewExperimentView.java

--- src/main/java/io/skymind/pathmind/ui/views/experiment/NewExperimentView.java
+++ src/main/java/io/skymind/pathmind/ui/views/experiment/NewExperimentView.java
@@ -139,7 +139,7 @@ public class NewExperimentView extends PathMindDefaultView implements HasUrlPara
                 "1. The \"after\" variable is used to retrieve the observation value after an action is performed. The \"before\" variable is used to retrieve the observation value before an action is performed. So if our observation value needs to be maximized, we can write\n" +
                 "reward= after[0]- before[0];\n" +
                 "\n" +
-                "2. weights can also be added to the reward calculation. In this case, if a condition is satisfied, then the learning agent is rewarded a score of 5 otherwise it gets a zero.\n" +
+                "2. Weights can also be added to the reward calculation. In this case, if a condition is satisfied, then the learning agent is rewarded a score of 5 otherwise it gets a zero.\n" +
                 "reward = after[0] > before[0] ? 5 : 0;\n" +
                 "\n" +
                 "3. For multiple lines, the reward variable can be summed or subtracted. The reward function is calculated between every two actions.\n" +
