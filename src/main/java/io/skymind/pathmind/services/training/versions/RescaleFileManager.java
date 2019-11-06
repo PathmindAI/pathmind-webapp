39ca06d3c2de7a2261aa6bf2753acd5fff088105
kepricon <kepricon@gmail.com>
2019-11-05 21:10:48 -0800

    change TF version

--- src/main/java/io/skymind/pathmind/services/training/versions/RescaleFileManager.java
+++ src/main/java/io/skymind/pathmind/services/training/versions/RescaleFileManager.java
@@ -37,7 +37,8 @@ public class RescaleFileManager {
         ));
         vTable.put(RLLib.VERSION_0_7_0, Arrays.asList(
 //                "LZAENb", // conda
-                "jniWLb", // conda TF 2.0, RAY 0.7.6
+//                "jniWLb", // conda TF 2.0, RAY 0.7.6
+                "xGfzT", // conda TF 1.13.1, RAY 0.7.6
 //                "jKjXa", // nativerl-1.0.0-SNAPSHOT-bin.zip, 2019-10-15 DH version
                 "OoYSOb", // nativerl-1.0.0-SNAPSHOT-bin.zip, 2019-11-02 DH version
                 "fDRBHd"  // OpenJDK8U-jdk_x64_linux_hotspot_8u222b10.tar.gz
