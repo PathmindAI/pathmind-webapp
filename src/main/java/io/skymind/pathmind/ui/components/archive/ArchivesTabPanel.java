fba1fa9b61c8c11cc8a0416a143ef80a529b3d0e
Onur Idrisoglu <33827141+onuridrisoglu@users.noreply.github.com>
2019-11-13 21:39:57 +0200

    Show reward function in experiments (#465)
    * Moved icon size setting to styles.css
    
    * Adding the archive column is now optional, and made some refactoring
    
    * Added showRewardFunction button to the grid, and labels to right panel
    
    * Merged two labels into one
    
    * ShowRewardFunctionButton is now a separate class
    
    in order to align with the rest of the application

--- src/main/java/io/skymind/pathmind/ui/components/archive/ArchivesTabPanel.java
+++ src/main/java/io/skymind/pathmind/ui/components/archive/ArchivesTabPanel.java
@@ -1,58 +1,71 @@
 package io.skymind.pathmind.ui.components.archive;
 
-import com.vaadin.flow.component.grid.Grid;
-import io.skymind.pathmind.data.ArchivableData;
-import io.skymind.pathmind.ui.components.TabPanel;
-
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.Grid.Column;
+
+import io.skymind.pathmind.data.ArchivableData;
+import io.skymind.pathmind.ui.components.TabPanel;
 
 /**
  * If this class is used within a view with a grid then it will automatically add the Archived column in the grid
  * and give you the ability to flip back and forth between the archived and unarchived views as well as take care
  * of filtering out the archived items.
  */
-public class ArchivesTabPanel<T> extends TabPanel
+public class ArchivesTabPanel<T extends ArchivableData> extends TabPanel
 {
 	private static final String ARCHIVES_TAB = "Archives";
 
 	// These need to be here for the same reason as the init() method.
 	private Grid<T> grid;
 	private Supplier<List<T>> getItems;
+	private BiConsumer<Long, Boolean> archiveDAO;
 
+	public ArchivesTabPanel(String tabName, Grid<T> grid, Supplier<List<T>> getItems, BiConsumer<Long, Boolean> archiveDAO) {
+		this(tabName, true, grid, getItems, archiveDAO);
+	}
+	
 	/**
-	 * Because there is no ability to search for a column by the header's name (https://vaadin.com/forum/thread/17861491) I then
-	 * use the next best thing which is that all our archives columns are the last column on the grid and so get the last column
-	 * and adjust that column header's name.
+	 * By default ArchivesTabPanel creates an component column, you can set
+	 * isAutoAppendColumn to false, in order to disable this feature
 	 */
-	public ArchivesTabPanel(String tabName, Grid<T> grid, Supplier<List<T>> getItems, BiConsumer<Long, Boolean> archiveDAO)
+	public ArchivesTabPanel(String tabName, boolean isAutoCreateActionColumn, Grid<T> grid, Supplier<List<T>> getItems, BiConsumer<Long, Boolean> archiveDAO)
 	{
 		super(tabName, ARCHIVES_TAB);
 
 		this.grid = grid;
 		this.getItems = getItems;
+		this.archiveDAO = archiveDAO;
 
 		setAlignItems(Alignment.START);
 
-		// Add archive column to grid as the last column.
-		grid.addComponentColumn(data -> gettArchivesButton(grid, getItems, (ArchivableData) data, archiveDAO))
-				.setHeader("Archive")
-				.setSortable(false);
+		Column<T> archiveColumn = isAutoCreateActionColumn
+				? grid.addComponentColumn(data -> getArchivesButton(data)).setHeader("Archive").setSortable(false)
+				: null;
 
 		addTabClickListener(name -> {
-			updateArchiveColumnHeader(grid, ARCHIVES_TAB.equals(name) ? "Unarchive" : "Archive");
+			if (archiveColumn != null) {
+				updateArchiveColumnHeader(archiveColumn, ARCHIVES_TAB.equals(name) ? "Unarchive" : "Archive");
+			}
 			grid.setItems(getFilteredModels(getItems.get(), ARCHIVES_TAB.equals(name)));
 		});
 	}
 
-	private ArchivesButton<T> gettArchivesButton(Grid<T> grid, Supplier<List<T>> getItems, ArchivableData data, BiConsumer<Long, Boolean> archiveDAO) {
+	/**
+	 * This is public because, in case the action column is not created
+	 * automatically, the parent can still ask ArchievesTabPanel to create the
+	 * archivesButton. Since it has all the information already.
+	 */
+	public ArchivesButton<T> getArchivesButton(T data) {
 		return new ArchivesButton<T>(grid, data, isArchived -> getFilteredModels(getItems.get(), isArchived), archiveDAO);
 	}
 
-	private void updateArchiveColumnHeader(Grid<T> grid, String header) {
-		grid.getColumns().get(grid.getColumns().size()-1).setHeader(header);
+	private void updateArchiveColumnHeader(Column<T> archiveColumn, String header) {
+		archiveColumn.setHeader(header);
 	}
 
 	private List<T> getFilteredModels(List<T> data, boolean isArchived) {
