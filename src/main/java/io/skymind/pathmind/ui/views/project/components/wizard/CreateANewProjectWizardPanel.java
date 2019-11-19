92f3c959b948586277dccb5f7348065dc7e502b0
Onur Idrisoglu <onur@vaadin.com>
2019-11-19 15:43:46 +0200

    Changed styles for 'Create Nnew Project' screen

--- src/main/java/io/skymind/pathmind/ui/views/project/components/wizard/CreateANewProjectWizardPanel.java
+++ src/main/java/io/skymind/pathmind/ui/views/project/components/wizard/CreateANewProjectWizardPanel.java
@@ -3,13 +3,14 @@ package io.skymind.pathmind.ui.views.project.components.wizard;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import io.skymind.pathmind.data.Project;
 import io.skymind.pathmind.ui.binders.ProjectBinders;
-import io.skymind.pathmind.ui.utils.GuiUtils;
+import io.skymind.pathmind.ui.components.LabelFactory;
+import io.skymind.pathmind.ui.constants.CssMindPathStyles;
 import io.skymind.pathmind.ui.utils.WrapperUtils;
 
 public class CreateANewProjectWizardPanel extends VerticalLayout
@@ -20,11 +21,12 @@ public class CreateANewProjectWizardPanel extends VerticalLayout
 	public CreateANewProjectWizardPanel(Binder<Project> binder)
 	{
 		projectNameTextField.setWidthFull();
+		createProjectButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+		createProjectButton.setClassName(CssMindPathStyles.CREATE_PROJECT_BUTTON);
 
 		// TODO -> https://github.com/SkymindIO/pathmind-webapp/issues/50 -> Do we allow duplicate project names? Are there any validation rules?
-		add(new H3("Start a New Project!"),
-				GuiUtils.getSubtitleLabel("Projects organize your Pathmind Experiments based on your AnyLogic model"),
-				GuiUtils.getHeightSpacer("40px"),
+		add(	LabelFactory.createLabel("Start a New Project!", CssMindPathStyles.SECTION_TITLE_LABEL),
+				LabelFactory.createLabel("Projects organize your Pathmind Experiments based on your AnyLogic model", CssMindPathStyles.SECTION_SUBTITLE_LABEL),
 				projectNameTextField,
 				WrapperUtils.wrapWidthFullCenterHorizontal(createProjectButton));
 
