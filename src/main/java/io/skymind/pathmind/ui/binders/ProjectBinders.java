1647b02560f7754318304dc34f627ffe4f1119b8
Onur Idrisoglu <onur@vaadin.com>
2019-11-19 14:03:36 +0200

    Added duplicated project name check

--- src/main/java/io/skymind/pathmind/ui/binders/ProjectBinders.java
+++ src/main/java/io/skymind/pathmind/ui/binders/ProjectBinders.java
@@ -2,7 +2,11 @@ package io.skymind.pathmind.ui.binders;
 
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
+
 import io.skymind.pathmind.data.Project;
+import io.skymind.pathmind.db.dao.ProjectDAO;
+import io.skymind.pathmind.security.SecurityUtils;
+import io.skymind.pathmind.utils.SpringUtils;
 
 public class ProjectBinders
 {
@@ -10,6 +14,12 @@ public class ProjectBinders
 	{
 		binder.forField(projectNameTextField)
 				.asRequired("Project must have a name")
+				.withValidator(projectName -> isUniqueForUser(projectName), "Project name should be unique")
 				.bind(Project::getName, Project::setName);
+	}
+
+	private static boolean isUniqueForUser(String name) {
+		ProjectDAO projectDao = SpringUtils.getBean(ProjectDAO.class);
+		return !projectDao.getProjectsForUser(SecurityUtils.getUserId()).stream().anyMatch(p -> p.getName().equalsIgnoreCase(name));
 	}
 }
