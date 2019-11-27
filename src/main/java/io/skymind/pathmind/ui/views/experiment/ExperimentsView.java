697fc8e30ab25edc2f058d9acdd59edf17944c48
Onur Idrisoglu <33827141+onuridrisoglu@users.noreply.github.com>
2019-11-27 04:20:24 +0200

    Turn off spellcheck for getObservations (#516)
    * Turn off spellcheck
    
    * Added PathmindTextArea with adds setSpellcheck onto TextArea
    
    * TextArea usages are converted into PathmindTextArea

--- src/main/java/io/skymind/pathmind/ui/views/experiment/ExperimentsView.java
+++ src/main/java/io/skymind/pathmind/ui/views/experiment/ExperimentsView.java
@@ -28,6 +28,7 @@ import io.skymind.pathmind.db.dao.UserDAO;
 import io.skymind.pathmind.db.repositories.ExperimentRepository;
 import io.skymind.pathmind.exception.InvalidDataException;
 import io.skymind.pathmind.security.Routes;
+import io.skymind.pathmind.ui.components.PathmindTextArea;
 import io.skymind.pathmind.ui.components.ScreenTitlePanel;
 import io.skymind.pathmind.ui.components.SearchBox;
 import io.skymind.pathmind.ui.components.archive.ArchivesTabPanel;
@@ -63,7 +64,7 @@ public class ExperimentsView extends PathMindDefaultView implements HasUrlParame
 
     private ArchivesTabPanel<Experiment> archivesTabPanel;
     private ExperimentGrid experimentGrid;
-    private TextArea getObservationTextArea;
+    private PathmindTextArea getObservationTextArea;
     private RewardFunctionEditor rewardFunctionEditor;
     private Label rewardFunctionTitle;
     private ScreenTitlePanel titlePanel;
@@ -112,7 +113,7 @@ public class ExperimentsView extends PathMindDefaultView implements HasUrlParame
     }
 
     private void setupGetObservationTextArea() {
-        getObservationTextArea = new TextArea("getObservations");
+        getObservationTextArea = new PathmindTextArea("getObservations");
         getObservationTextArea.setSizeFull();
         getObservationTextArea.setReadOnly(true);
     }
