120ad09dd07608d9b4ec38cb599eea5bdf9e1661
Henri Kerola <henri.kerola@vaadin.com>
2019-12-01 14:46:07 -0800

    Encapsulate title setting into PathmindButton.setTitle

--- src/main/java/io/skymind/pathmind/ui/components/buttons/ShowRewardFunctionButton.java
+++ src/main/java/io/skymind/pathmind/ui/components/buttons/ShowRewardFunctionButton.java
@@ -1,15 +1,15 @@
 package io.skymind.pathmind.ui.components.buttons;
 
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import io.skymind.pathmind.ui.components.PathmindButton;
 
-public class ShowRewardFunctionButton extends Button {
+public class ShowRewardFunctionButton extends PathmindButton {
 
 	public ShowRewardFunctionButton() {
 		super(VaadinIcon.CODE.create());
 		addThemeVariants(ButtonVariant.LUMO_TERTIARY);
 		addClassName("action-button");
-		getElement().setAttribute("title", "Show reward function");
+		setTitle("Show reward function");
 	}
 }
