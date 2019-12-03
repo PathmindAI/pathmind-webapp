ec6dad45eeef120869cb27892c117b1c75810351
kepricon <kepricon@gmail.com>
2019-12-03 12:09:02 -0800

    add model analyzer test

--- src/test/java/io/skymind/pathmind/services/project/rest/ModelAnalyzerApiClientTest.java
+++ src/test/java/io/skymind/pathmind/services/project/rest/ModelAnalyzerApiClientTest.java
@@ -1,16 +1,23 @@
 package io.skymind.pathmind.services.project.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.skymind.pathmind.services.project.rest.dto.HyperparametersDTO;
 import io.skymind.pathmind.utils.ObjectMapperHolder;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
+
+@Ignore
 public class ModelAnalyzerApiClientTest {
 
     @Test
-    public void testBasicModelAnlayze() {
+    public void testBasicModelAnalyze() throws IOException {
         ObjectMapper objectMapper = ObjectMapperHolder.getJsonMapper();
 
         final ModelAnalyzerApiClient client = new ModelAnalyzerApiClient(
@@ -19,7 +26,12 @@ public class ModelAnalyzerApiClientTest {
                 objectMapper,
                 WebClient.builder());
 
-        client.health();
+        File model = new ClassPathResource("model/call_center.zip").getFile();
+        HyperparametersDTO hyperparametersDTO = client.analyze(model);
+
+        assertEquals(hyperparametersDTO.getActions(), "70");
+        assertEquals(hyperparametersDTO.getObservations(), "125");
+        assertEquals(hyperparametersDTO.getRewardFunction(), "new double[]{this.getReward()}");
     }
 
 }
