c612eb7bfb4eb5877430ca41eb72fc534a9195d1
Onur Idrisoglu <onur@vaadin.com>
2020-01-08 17:04:52 +0200

    removed the try catch block from PathMindDefaultView

--- src/main/java/io/skymind/pathmind/ui/views/errors/ErrorView.java
+++ src/main/java/io/skymind/pathmind/ui/views/errors/ErrorView.java
@@ -1,25 +1,35 @@
 package io.skymind.pathmind.ui.views.errors;
 
+import javax.servlet.http.HttpServletResponse;
+
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.router.Route;
-import io.skymind.pathmind.security.Routes;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.ErrorParameter;
+import com.vaadin.flow.router.HasErrorParameter;
+import com.vaadin.flow.router.ParentLayout;
+
 import io.skymind.pathmind.ui.components.ScreenTitlePanel;
 import io.skymind.pathmind.ui.layouts.MainLayout;
-import io.skymind.pathmind.ui.views.PathMindDefaultView;
 import io.skymind.pathmind.ui.utils.WrapperUtils;
+import io.skymind.pathmind.ui.views.PathMindDefaultView;
 
 /**
  * TODO -> Implement correctly, this is just a quick stub.
  */
 @CssImport("./styles/styles.css")
-@Route(value = Routes.ERROR_URL, layout = MainLayout.class)
-public class ErrorView extends PathMindDefaultView
+@ParentLayout(MainLayout.class)
+public class ErrorView extends PathMindDefaultView implements HasErrorParameter<Exception>
 {
 	public ErrorView() {
 		super();
 	}
+	
+	@Override
+ 	public int setErrorParameter(BeforeEnterEvent event, ErrorParameter<Exception> parameter) {
+ 		return HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+ 	}
 
 	@Override
 	protected Component getTitlePanel() {
--- src/main/java/io/skymind/pathmind/ui/views/errors/InvalidDataView.java
+++ src/main/java/io/skymind/pathmind/ui/views/errors/InvalidDataView.java
@@ -1,26 +1,36 @@
 package io.skymind.pathmind.ui.views.errors;
 
+import javax.servlet.http.HttpServletResponse;
+
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.router.Route;
-import io.skymind.pathmind.security.Routes;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.ErrorParameter;
+import com.vaadin.flow.router.HasErrorParameter;
+import com.vaadin.flow.router.ParentLayout;
+
+import io.skymind.pathmind.exception.InvalidDataException;
 import io.skymind.pathmind.ui.components.ScreenTitlePanel;
 import io.skymind.pathmind.ui.layouts.MainLayout;
-import io.skymind.pathmind.ui.views.PathMindDefaultView;
 import io.skymind.pathmind.ui.utils.WrapperUtils;
+import io.skymind.pathmind.ui.views.PathMindDefaultView;
 
 /**
  * TODO -> Implement correctly, this is just a quick stub.
  */
 @CssImport("./styles/styles.css")
-@Route(value = Routes.INVALID_DATA_URL, layout = MainLayout.class)
-public class InvalidDataView extends PathMindDefaultView
+@ParentLayout(MainLayout.class)
+public class InvalidDataView extends PathMindDefaultView implements HasErrorParameter<InvalidDataException>
 {
 	public InvalidDataView() {
 		super();
 	}
+	
+	@Override
+ 	public int setErrorParameter(BeforeEnterEvent event, ErrorParameter<InvalidDataException> parameter) {
+ 		return HttpServletResponse.SC_FORBIDDEN;
+ 	}
 
 	@Override
 	protected Component getMainContent() {
