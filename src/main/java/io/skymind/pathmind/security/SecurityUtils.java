2b592474c02e5ea2e18e621bcb297f15f2e3c76f
Onur Idrisoglu <onur@vaadin.com>
2020-01-08 11:00:05 +0200

    removed PageNotFoundView from public page list

--- src/main/java/io/skymind/pathmind/security/SecurityUtils.java
+++ src/main/java/io/skymind/pathmind/security/SecurityUtils.java
@@ -1,22 +1,23 @@
 package io.skymind.pathmind.security;
 
-import com.vaadin.flow.server.ServletHelper.RequestType;
-import com.vaadin.flow.shared.ApplicationConstants;
+import java.util.stream.Stream;
 
-import io.skymind.pathmind.ui.views.login.LoginView;
-import io.skymind.pathmind.ui.views.errors.PageNotFoundView;
-import io.skymind.pathmind.ui.views.login.EmailVerificationView;
-import io.skymind.pathmind.ui.views.login.ResetPasswordView;
-import io.skymind.pathmind.ui.views.login.SignUpView;
+import javax.naming.AuthenticationException;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.naming.AuthenticationException;
-import javax.servlet.http.HttpServletRequest;
-import java.util.stream.Stream;
+import com.vaadin.flow.server.ServletHelper.RequestType;
+import com.vaadin.flow.shared.ApplicationConstants;
+
+import io.skymind.pathmind.ui.views.login.EmailVerificationView;
+import io.skymind.pathmind.ui.views.login.LoginView;
+import io.skymind.pathmind.ui.views.login.ResetPasswordView;
+import io.skymind.pathmind.ui.views.login.SignUpView;
 
 public class SecurityUtils
 {
@@ -87,11 +88,9 @@ public class SecurityUtils
 	 */
 	public static boolean isAccessGranted(Class<?> securedClass) {
 		final boolean publicView = LoginView.class.equals(securedClass)
-				//|| AccessDeniedView.class.equals(securedClass)
 				|| SignUpView.class.equals(securedClass)
 				|| ResetPasswordView.class.equals(securedClass)
-				|| EmailVerificationView.class.equals(securedClass)
-				|| PageNotFoundView.class.equals(securedClass);
+				|| EmailVerificationView.class.equals(securedClass);
 
 		// Always allow access to public views
 		if (publicView) {
