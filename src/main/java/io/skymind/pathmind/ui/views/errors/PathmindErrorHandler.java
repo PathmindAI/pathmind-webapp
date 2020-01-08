50a8b3f0c547898dd9828f59df6d9f0d1be2b852
Onur Idrisoglu <onur@vaadin.com>
2020-01-08 17:05:59 +0200

    Added PathmindErrorHandler

--- src/main/java/io/skymind/pathmind/ui/views/errors/PathmindErrorHandler.java
+++ src/main/java/io/skymind/pathmind/ui/views/errors/PathmindErrorHandler.java
@@ -1,9 +1,13 @@
 package io.skymind.pathmind.ui.views.errors;
 
-import com.vaadin.flow.component.UI;
+import java.time.LocalDateTime;
+import java.util.Random;
+
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.server.ErrorEvent;
 import com.vaadin.flow.server.ErrorHandler;
 
+import io.skymind.pathmind.ui.components.CloseableNotification;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -11,8 +15,30 @@ public class PathmindErrorHandler implements ErrorHandler {
 
 	@Override
 	public void error(ErrorEvent event) {
-		log.error(event.getThrowable().getMessage(), event.getThrowable());
-		UI.getCurrent().navigate(ErrorView.class);
+		String errorId = generateUniqueErrorId();
+		
+		log.error(String.format("Error #%s: %s", errorId, event.getThrowable().getMessage()), event.getThrowable());
+		String errorMessage = String.format("An unexpected error occurred: #%s <br>Please contact Skymind for assistance.", errorId);
+		
+		CloseableNotification errorNotification = new CloseableNotification(errorMessage);
+		errorNotification.setDuration(-1);
+		errorNotification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+		errorNotification.open();
+	}
+	
+	/**
+	 * Generates a unique error id of length 12 chars
+	 * 6 random numbers - HHmmss
+	 */
+	public static String generateUniqueErrorId() {
+		int start = 48; // number 0
+		int end = 57; // number 9
+		Random random = new Random();
+		String generated = random.ints(start, end + 1)
+				.limit(6)
+				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+				.toString();
+		return String.format("%s-%2$tH%2$tM%2$tS", generated , LocalDateTime.now());
 	}
 
 }
