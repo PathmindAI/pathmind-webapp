3a62d9abb083b547a41c379cc0ee5f3fa02acdb7
kepricon <kepricon@gmail.com>
2020-01-10 15:19:29 -0800

    more work on getting progress from aws

--- src/main/java/io/skymind/pathmind/services/training/cloud/aws/AWSExecutionProgressUpdater.java
+++ src/main/java/io/skymind/pathmind/services/training/cloud/aws/AWSExecutionProgressUpdater.java
@@ -1,14 +1,21 @@
 package io.skymind.pathmind.services.training.cloud.aws;
 
+import io.skymind.pathmind.constants.RunStatus;
+import io.skymind.pathmind.data.Policy;
+import io.skymind.pathmind.data.policy.RewardScore;
 import io.skymind.pathmind.db.dao.ExecutionProviderMetaDataDAO;
 import io.skymind.pathmind.db.dao.RunDAO;
 import io.skymind.pathmind.services.notificationservice.EmailNotificationService;
 import io.skymind.pathmind.services.training.ExecutionProgressUpdater;
+import io.skymind.pathmind.services.training.constant.TrainingFile;
+import io.skymind.pathmind.services.training.progress.ProgressInterpreter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -32,13 +39,41 @@ public class AWSExecutionProgressUpdater implements ExecutionProgressUpdater {
         // Getting all these values beforehand in single database calls rather than in loops of database calls.
         final List<Long> runIds = runDAO.getExecutingRuns();
         final Map<Long, List<String>> stoppedPoliciesNamesForRuns = runDAO.getStoppedPolicyNamesForRuns(runIds);
-        final Map<Long, String> rescaleJobIds = executionProviderMetaDataDAO.getRescaleRunJobIds(runIds);
+//        final Map<Long, String> rescaleJobIds = executionProviderMetaDataDAO.getRescaleRunJobIds(runIds);
 
-        runIds.parallelStream().forEach(run -> {
-            provider.progress("id" + run);
+        log.info("kepricondebug0 : " + runIds);
+
+        runIds.parallelStream().forEach(runId -> {
+
+            String jobHandle = "id" + runId;
+            RunStatus runStatus = provider.status("id" + runId);
+
+            final List<Policy> policies = getPoliciesFromProgressProvider(stoppedPoliciesNamesForRuns, runId);
+            final List<String> finishedPolicyNamesFromAWS = getTerminatedPolicesFromProvider(jobHandle);
+
+
+            log.info("kepricondebug1 : " + runStatus);
+            log.info("kepricondebug2 : " + policies.stream().map(Policy::getExternalId).collect(Collectors.toList()));
+            log.info("kepricondebug3 : " + finishedPolicyNamesFromAWS);
         });
 
-        log.info("kepricondebug1 : " + runIds);
-        log.info("kepricondebug2 : " + rescaleJobIds);
+
+
+//        log.info("kepricondebug2 : " + rescaleJobIds);
+    }
+
+    private List<Policy> getPoliciesFromProgressProvider(Map<Long, List<String>> stoppedPoliciesNamesForRuns, Long runId) {
+        final Map<String, String> rawProgress = provider.progress("id" + runId);
+        return rawProgress.entrySet().stream()
+                .filter(e -> !stoppedPoliciesNamesForRuns.getOrDefault(runId, Collections.emptyList()).contains(e.getKey()))
+                .map(e -> {
+                    List<RewardScore> previousScores = runDAO.getScores(runId, e.getKey());
+                    return ProgressInterpreter.interpret(e, previousScores);
+                })
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getTerminatedPolicesFromProvider(String jobHandle) {
+        return provider.getTrialStatus(jobHandle, TrainingFile.RAY_TRIAL_COMPLETE);
     }
 }
