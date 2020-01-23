9ee9811311dddf2fe38202d23ab66f1af867e8c6
Fionna <fionnacst@gmail.com>
2020-01-23 14:46:04 +0800

    Update menu styles & add global style variables

--- frontend/src/guide/guide-menu.js
+++ frontend/src/guide/guide-menu.js
@@ -9,29 +9,42 @@ class GuideMenu extends PolymerElement {
           align-self: flex-start;
           flex: 0 0 auto;
           position: sticky;
-          top: 0;
-          background-color: white;
+          top: 31px;
           padding: var(--lumo-space-xl);
         }
         a {
           display: block;
-          color: var(--pm-text-color);
+          color: var(--pm-blue-color-darkest-25pct);
           line-height: 1.3em;
           margin: 0 0 1em;
           text-decoration: none;
-          opacity: 0.25;
         }
         a:last-of-type {
           margin-bottom: 0;
         }
-        a:hover,
+        a:hover {
+          color: var(--pm-blue-color-darkest-50pct);
+        }
         .current {
+          color: var(--pm-blue-color-darkest);
           font-weight: 700;
-          opacity: 1;
+        }
+        .completed {
+          position: relative;
+        }
+        .completed::before {
+          content: "";
+          position: absolute;
+          width: 11px;
+          height: 11px;
+          top: 50%;
+          left: -15px;
+          background-image: url("/frontend/icons/checkmark.svg");
+          background-size: cover;
+          transform: translateY(-50%);
         }
         vaadin-button {
           font-size: var(--lumo-font-size-s);
-          padding: 0;
           margin: var(--lumo-space-xl) 0 0;
         }
       </style>
@@ -41,7 +54,7 @@ class GuideMenu extends PolymerElement {
             <a class$="{{item.state}}" href$="{{item.path}}">{{item.name}}</a>
           </template>
         </dom-repeat>
-        <vaadin-button id="skipToUploadModelBtn" theme="tertiary">
+        <vaadin-button id="skipToUploadModelBtn" theme="tertiary-inline">
           Skip to Upload Model
         </vaadin-button>
       </div>
