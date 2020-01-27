385dadfa3262fc3d9fab35e5996bc84e147f20f5
Onur Idrisoglu <onur@vaadin.com>
2020-01-27 15:54:10 +0200

    Removed eventbus subscription from PolicyHighlightPanel, it is refreshed by ExperimentView now

--- src/main/java/io/skymind/pathmind/ui/views/experiment/components/PolicyHighlightPanel.java
+++ src/main/java/io/skymind/pathmind/ui/views/experiment/components/PolicyHighlightPanel.java
@@ -1,24 +1,17 @@
 package io.skymind.pathmind.ui.views.experiment.components;
 
-import com.vaadin.flow.component.AttachEvent;
-import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import io.skymind.pathmind.bus.EventBus;
-import io.skymind.pathmind.bus.events.PolicyUpdateBusEvent;
-import io.skymind.pathmind.bus.subscribers.PolicyUpdateSubscriber;
+
 import io.skymind.pathmind.data.Policy;
 import io.skymind.pathmind.data.utils.PolicyUtils;
-import io.skymind.pathmind.ui.utils.PushUtils;
 
-public class PolicyHighlightPanel extends VerticalLayout implements PolicyUpdateSubscriber
+public class PolicyHighlightPanel extends VerticalLayout
 {
 	private Label policyLabel = new Label();
 	private Label scoreLabel = new Label();
 	private Label algorithmLabel = new Label();
-
-	private Policy policy;
 
 	public PolicyHighlightPanel()
 	{
@@ -36,29 +29,9 @@ public class PolicyHighlightPanel extends VerticalLayout implements PolicyUpdate
 	}
 
 	public void update(Policy policy) {
-		this.policy = policy;
 		policyLabel.setText(policy.getName());
 		scoreLabel.setText(PolicyUtils.getFormattedLastScore(policy));
 		algorithmLabel.setText(policy.getAlgorithmEnum().name());
 	}
 
-	@Override
-	protected void onDetach(DetachEvent event) {
-		EventBus.unsubscribe(this);
-	}
-
-	@Override
-	protected void onAttach(AttachEvent event) {
-		EventBus.subscribe(this);
-	}
-
-	@Override
-	public void handleBusEvent(PolicyUpdateBusEvent event) {
-		PushUtils.push(this, () -> update(event.getPolicy()));
-	}
-
-	@Override
-	public boolean filterBusEvent(PolicyUpdateBusEvent event) {
-		return policy.getId() == event.getPolicy().getId();
-	}
 }
