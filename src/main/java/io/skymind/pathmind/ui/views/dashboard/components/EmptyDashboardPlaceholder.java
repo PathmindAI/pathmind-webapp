53ba895075a060ad20a8fc633eafcf7953dc418e
Onur Idrisoglu <onur@vaadin.com>
2020-01-27 12:03:19 +0200

    Created a generic greeting component, and replaced existing usages

--- src/main/java/io/skymind/pathmind/ui/views/dashboard/components/EmptyDashboardPlaceholder.java
+++ src/main/java/io/skymind/pathmind/ui/views/dashboard/components/EmptyDashboardPlaceholder.java
@@ -1,10 +1,10 @@
 package io.skymind.pathmind.ui.views.dashboard.components;
 
-import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 import io.skymind.pathmind.ui.components.LabelFactory;
+import io.skymind.pathmind.ui.components.PathmindGreetingComponent;
 import io.skymind.pathmind.ui.constants.CssMindPathStyles;
 import io.skymind.pathmind.ui.utils.WrapperUtils;
 
@@ -12,16 +12,13 @@ public class EmptyDashboardPlaceholder extends VerticalLayout {
 	
 	public EmptyDashboardPlaceholder() {
 		setSpacing(false);
-		Image pathMindLogo = new Image("frontend/images/pathmind-logo.png", "Pathmind Logo");
-		pathMindLogo.addClassName("navbar-logo");
-		add(LabelFactory.createLabel("Welcome to", CssMindPathStyles.SMALL_LIGHT_LABEL),
-				pathMindLogo);
 		
 		VerticalLayout placeholderContent = WrapperUtils.wrapFormCenterVertical(
 				LabelFactory.createLabel("Get started by creating your first project.", CssMindPathStyles.SECTION_TITLE_LABEL),
 				VaadinIcon.ARROW_DOWN.create());
 		placeholderContent.addClassName("dashboard-placeholder");
-		add(placeholderContent);
+		
+		add(new PathmindGreetingComponent(), placeholderContent);
 
 		setDefaultHorizontalComponentAlignment(Alignment.CENTER);
 		setWidthFull();
