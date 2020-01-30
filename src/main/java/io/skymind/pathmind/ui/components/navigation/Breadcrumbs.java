1c2038058171da5452b5b5670941554dc11ee4a8
Onur Idrisoglu <onur@vaadin.com>
2020-01-30 16:43:41 +0200

    The last item in breadcrumb is generated as span instead of link

--- src/main/java/io/skymind/pathmind/ui/components/navigation/Breadcrumbs.java
+++ src/main/java/io/skymind/pathmind/ui/components/navigation/Breadcrumbs.java
@@ -1,17 +1,22 @@
 package io.skymind.pathmind.ui.components.navigation;
 
-import com.vaadin.flow.router.RouterLink;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.HasUrlParameter;
+import com.vaadin.flow.router.RouterLink;
 
-import io.skymind.pathmind.data.Project;
-import io.skymind.pathmind.data.Model;
 import io.skymind.pathmind.data.Experiment;
-import io.skymind.pathmind.ui.views.project.ProjectsView;
-import io.skymind.pathmind.ui.views.model.ModelsView;
-import io.skymind.pathmind.ui.views.experiment.ExperimentsView;
+import io.skymind.pathmind.data.Model;
+import io.skymind.pathmind.data.Project;
 import io.skymind.pathmind.ui.views.experiment.ExperimentView;
+import io.skymind.pathmind.ui.views.experiment.ExperimentsView;
 import io.skymind.pathmind.ui.views.experiment.utils.ExperimentViewNavigationUtils;
+import io.skymind.pathmind.ui.views.model.ModelsView;
+import io.skymind.pathmind.ui.views.project.ProjectsView;
 
 public class Breadcrumbs extends HorizontalLayout
 {
@@ -29,40 +34,70 @@ public class Breadcrumbs extends HorizontalLayout
 		this(project, model, null);
 	}
 
+	@SuppressWarnings({"rawtypes", "unchecked"})
 	public Breadcrumbs(Project project, Model model, Experiment experiment) {
-		RouterLink projectsPageLink = createBreadcrumb("Projects", ProjectsView.class, null);
-		add(projectsPageLink);
+		List<BreadcrumbItem> items = new ArrayList<>();
+		items.add(new BreadcrumbItem("Projects", ProjectsView.class, null));
 
 		if (project != null) {
-			add(createSeparator());	
-			add(createBreadcrumb(project.getName(), ModelsView.class, project.getId()));
+			items.add(new BreadcrumbItem(project.getName(), ModelsView.class, project.getId()));
 		}
 		if (model != null) {
-			add(createSeparator());
-			add(createBreadcrumb("Model #" + model.getName(), ExperimentsView.class, model.getId()));
+			items.add(new BreadcrumbItem("Model #" + model.getName(), ExperimentsView.class, model.getId()));
 		}
 		if (experiment != null) {
-			add(createSeparator());
-			add(createBreadcrumbExperiment("Experiment #" + experiment.getName(), ExperimentView.class, ExperimentViewNavigationUtils.getExperimentParameters(experiment)));
+			items.add(new BreadcrumbItem("Experiment #" + experiment.getName(), ExperimentView.class, ExperimentViewNavigationUtils.getExperimentParameters(experiment)));
 		}
+		
+		items.get(items.size() - 1).asCurrentStep();
+		items.forEach(item -> {
+			if (getComponentCount() > 0) {
+				add(createSeparator());
+			}
+			add(item.createComponent());
+		});
 
 		setSpacing(false);
 		setAlignItems(Alignment.START);
 	}
 
-	private RouterLink createBreadcrumb(String name, Class navigationTarget, Long pageParameter) {
-		RouterLink breadcrumb = new RouterLink(name, navigationTarget, pageParameter);
-		breadcrumb.addClassName(BREADCRUMB_CLASSNAME);
-		return breadcrumb;
-	}
-
-	private RouterLink createBreadcrumbExperiment(String name, Class navigationTarget, String pageParameter) {
-		RouterLink breadcrumb = new RouterLink(name, navigationTarget, pageParameter);
-		breadcrumb.addClassName(BREADCRUMB_CLASSNAME);
-		return breadcrumb;
-	}
-
 	private Span createSeparator() {
 		return new Span(">");
+	}
+	
+	private class BreadcrumbItem<T, C extends Component & HasUrlParameter<T>> {
+		private String name;
+		private Class<C> navigationTarget;
+		private T parameter;
+		private boolean isCurrentStep = false;
+		
+		public BreadcrumbItem(String name, Class<C> navigationTarget, T parameter) {
+			this.name = name;
+			this.navigationTarget = navigationTarget;
+			this.parameter = parameter;
+		}
+		
+		private void asCurrentStep() {
+			isCurrentStep = true;
+		}
+		
+		private Component createComponent() {
+			if (isCurrentStep) {
+				return createLabel();
+			} else {
+				return createLink();
+			}
+		}
+		
+		private RouterLink createLink() {
+			RouterLink routerLink = new RouterLink(name, navigationTarget, parameter);
+			routerLink.addClassName(BREADCRUMB_CLASSNAME);
+			return routerLink;
+		}
+		private Span createLabel() {
+			Span label = new Span(name);
+			label.addClassName(BREADCRUMB_CLASSNAME);
+			return label;		
+		}
 	}
 }
