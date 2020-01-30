660808b34b471b1ecae7f222129d398e7e8b23ca
Fionna <fionnacst@gmail.com>
2020-01-30 09:39:03 +0800

    Clean up styles for guide

--- frontend/src/guide/guide-menu.js
+++ frontend/src/guide/guide-menu.js
@@ -11,9 +11,11 @@ class GuideMenu extends PolymerElement {
           position: sticky;
           top: 31px;
           padding: var(--lumo-space-xl);
+          padding-left: var(--lumo-space-xxl);
         }
         a {
           display: block;
+          position: relative;
           color: var(--pm-blue-color-darkest-25pct);
           line-height: 1.3em;
           margin: 0 0 1em;
@@ -25,33 +27,52 @@ class GuideMenu extends PolymerElement {
         a:hover {
           color: var(--pm-blue-color-darkest-50pct);
         }
+        a::before {
+          content: "";
+          position: absolute;
+          width: 18px;
+          height: 18px;
+          top: 50%;
+          left: -30px;
+          border: 1px solid var(--pm-gray-color);
+          transform: translateY(-50%);
+        }
         .current {
           color: var(--pm-blue-color-darkest);
           font-weight: 700;
         }
-        .completed {
-          position: relative;
-        }
-        .completed::before {
+        .completed::after {
           content: "";
           position: absolute;
-          width: 11px;
-          height: 11px;
+          width: 23px;
+          height: 17px;
           top: 50%;
-          left: -15px;
+          left: -26px;
           background-image: url("/frontend/icons/checkmark_grey.svg");
           background-size: cover;
-          transform: translateY(-50%);
+          transform: translateY(-60%);
+        }
+        a:not(.current)::before,
+        a:not(.current)::after {
+          opacity: 0.25;
+        }
+        a:hover::before,
+        a:hover::after {
+          opacity: 0.5;
         }
         vaadin-button {
           font-size: var(--lumo-font-size-s);
           margin: var(--lumo-space-xl) 0 0;
         }
       </style>
-      <div class="content">
+      <div>
         <dom-repeat items="[[checklist]]">
           <template>
-            <a router-link class$="[[item.state]]" href$="[[item.path]]">
+            <a
+              router-link
+              class$="[[item.state]][[isCurrent(item.path)]]"
+              href$="[[item.path]]"
+            >
               [[item.name]]
             </a>
           </template>
@@ -68,13 +89,11 @@ class GuideMenu extends PolymerElement {
   }
 
   static get properties() {
-    return {
-      location: {
-        value() {
-          return window.location.pathname.substr(1);
-        }
-      }
-    };
+    return {};
+  }
+
+  isCurrent(targetPath) {
+    return targetPath === window.location.pathname.substr(1) ? " current" : "";
   }
 }
 
