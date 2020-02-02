3cacf0f1278688ff38d6c4d28a6c55f728773224
FollowSteph <steph@followsteph.com>
2020-02-02 00:34:39 -0500

    #702 - Create UI for guided walkthrough of setting up simulation helper with Pathmind functions - Added the database hooks

--- src/main/java/io/skymind/pathmind/constants/GuideStep.java
+++ src/main/java/io/skymind/pathmind/constants/GuideStep.java
@@ -28,19 +28,19 @@ public enum GuideStep {
 		return id;
     }
     
-    // STEPH -> TODO -> For now if you try to go back it just stays the same. We should however throw a RuntimeException much like an ArrayOutOfBoundException. But I didn't
+    // TODO -> For now if you try to go back it just stays the same. We should however throw a RuntimeException much like an ArrayOutOfBoundException. But I didn't
     // want to go beyond this basic setup until we decide which options we want to go with.
     public GuideStep nextStep() {
         return ordinal() >= GuideStep.values().length - 1 ? Completed : GuideStep.values()[ordinal() + 1];
     }
 
-    // STEPH -> TODO -> For now if you try to go back it just stays the same. We should however throw a RuntimeException much like an ArrayOutOfBoundException. But I didn't
+    // TODO -> For now if you try to go back it just stays the same. We should however throw a RuntimeException much like an ArrayOutOfBoundException. But I didn't
     // want to go beyond this basic setup until we decide which options we want to go with.
     public GuideStep previousStep() {
         return ordinal() == 0 ? Overview : GuideStep.values()[ordinal() - 1];
     }
 
-    // STEPH -> TODO -> Only here to explain how I plan to save to the database in the Repository layer of the code.
+    // Only here in case the values change over time and no longer match their ordinal value.
     public int getValue() {
         return ordinal();
     }
--- src/main/java/io/skymind/pathmind/db/dao/GuideDAO.java
+++ src/main/java/io/skymind/pathmind/db/dao/GuideDAO.java
@@ -4,32 +4,20 @@ import io.skymind.pathmind.constants.GuideStep;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-
 @Repository
 public class GuideDAO
 {
 	private final DSLContext ctx;
 
-	// STEPH -> TODO -> Remove once implemented.
-	private static final HashMap<Long, GuideStep> memory = new HashMap<>();
-
 	GuideDAO(DSLContext ctx) {
 		this.ctx = ctx;
 	}
 
-	// STEPH -> TODO -> Implement
-	public void updateGuideStep(long projectId, GuideStep guideStep) {
-		memory.put(projectId, guideStep);
+	public GuideStep getGuideStep(long projectId) {
+		return GuideRepository.getGuideStep(ctx, projectId);
 	}
 
-	// STEPH -> TODO -> Implement
-	public GuideStep getGuideStep(long projectId) {
-		GuideStep guideStep = memory.get(projectId);
-		// We have several options here:
-		// 		1. The guideStep is part of the user table in which case we can add a default value of GuideStep.Overview and enforce not null to the column.
-		// 		2. The guideStep is a separate table so that it can be expanded a lot. We then need to also insert a new default row entry each time a new account is created. Not null constraint it still in effect.
-		// 		3. We add the null check in the UI or DAO layer. Least recommended option since it removes the null check constraint from the database.
-		return guideStep == null ? GuideStep.Overview : guideStep;
+	public void updateGuideStep(long projectId, GuideStep guideStep) {
+		GuideRepository.updateGuideStep(ctx, projectId, guideStep);
 	}
 }
