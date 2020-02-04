b71f6690c6e407a04191b19fb495ec8ccb1e884f
Onur Idrisoglu <onur@vaadin.com>
2020-02-04 15:18:18 +0200

    Label is replaced with Span

--- src/main/java/io/skymind/pathmind/ui/views/experiment/NewExperimentView.java
+++ src/main/java/io/skymind/pathmind/ui/views/experiment/NewExperimentView.java
@@ -12,7 +12,6 @@ import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
@@ -131,7 +130,7 @@ public class NewExperimentView extends PathMindDefaultView implements HasUrlPara
 
         return WrapperUtils.wrapCenterAlignmentFullSplitLayoutVertical(
                 WrapperUtils.wrapSizeFullVertical(
-                        new Label("Write your reward function:"),
+                        new Span("Write your reward function:"),
                         rewardFunctionEditor),
                 WrapperUtils.wrapSizeFullVertical(errorsTextArea),
                 70);
