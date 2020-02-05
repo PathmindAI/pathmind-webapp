53fd055c1b887cf28dc7d61a169cce9b6e9bfac7
Fionna <fionnacst@gmail.com>
2020-02-05 10:01:41 +0800

    Edit the 'Run Starting' confirmation message (#741)

--- src/main/java/io/skymind/pathmind/ui/components/dialog/RunConfirmDialog.java
+++ src/main/java/io/skymind/pathmind/ui/components/dialog/RunConfirmDialog.java
@@ -1,12 +1,22 @@
 package io.skymind.pathmind.ui.components.dialog;
 
 import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Paragraph;
 
 public class RunConfirmDialog extends ConfirmDialog {
     public RunConfirmDialog() {
         this.setHeader("Starting the training…");
-        this.setText("You’ll see the results as the training starts. This could take a couple minutes! (We’re working on fixing this.)");
+        this.setText(DialogContent());
         this.setConfirmButton("Okay", confirmEvent -> {});
     }
 
+    private Div DialogContent() {
+        Div dialogWrapper = new Div();
+        dialogWrapper.add(
+            new Paragraph("You’ll see the results as the training starts. This could take a couple minutes."),
+            new Paragraph("We'll send you an email when training completes!")
+        );
+        return dialogWrapper;
+    }
 }
