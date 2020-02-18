c6c8fcbdfe823fcb23ab33e72c41af503edeaa8d
Fionna <fionnacst@gmail.com>
2020-02-18 11:17:58 +0800

    Fix styles on Verification Email sent page (#884)

--- frontend/src/account/verification-email-sent-view.js
+++ frontend/src/account/verification-email-sent-view.js
@@ -11,38 +11,32 @@ import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
 class VerificationEmailViewSent extends PolymerElement {
   static get template() {
     return html`
-      <style include="pathmind-dialog-view">
-        .title {
-          margin: 20px auto 0;
-          font-size: 1.1em;
+      <style include="pathmind-dialog-view sign-up-view-styles">
+        .inner-content {
+          padding: var(--lumo-space-l) var(--lumo-space-l);
         }
         .icon {
           --iron-icon-height: 40px;
           --iron-icon-width: 40px;
-          margin: 25px auto 15px;
+          margin: 0 auto var(--lumo-space-xl);
           color: var(--lumo-success-color);
-        }
-        .note {
-          margin: 25px auto 15px;
         }
       </style>
       <div class="content">
+        <span class="welcome-text">Welcome to</span>
+        <img
+          class="logo"
+          src="frontend/images/pathmind-logo.png"
+          alt="Pathmind logo"
+        />
         <vaadin-vertical-layout style="width: 100%;" class="inner-content">
-          <div class="title">
-            A verification email is sent.
-          </div>
+          <h3>A verification email is sent.</h3>
           <iron-icon class="icon" icon="vaadin:check-circle"></iron-icon>
-          <div class="note">
+          <p>
             Follow the instructions in the email to verify your account.
-          </div>
-
+          </p>
           <vaadin-vertical-layout id="buttonsCont">
-            <vaadin-button
-              id="backToLogin"
-              theme="primary"
-              class="positive-action-btn"
-              >Done</vaadin-button
-            >
+            <vaadin-button id="backToLogin" theme="primary">Done</vaadin-button>
           </vaadin-vertical-layout>
         </vaadin-vertical-layout>
       </div>
