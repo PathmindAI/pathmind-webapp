6cc44d8cfba8a5bdc92346bedcb45e8479022eea
Slin Lee <slin@skymind.io>
2020-03-06 16:18:06 -0800

    Remove unused import

--- src/main/java/io/skymind/pathmind/ui/views/experiment/NewExperimentView.java
+++ src/main/java/io/skymind/pathmind/ui/views/experiment/NewExperimentView.java
@@ -9,7 +9,6 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
