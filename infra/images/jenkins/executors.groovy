4975c028a06861d3ad1914fa60959db5165a4eb3
Daniel Jaramillo <xneyder@gmail.com>
2020-03-12 16:27:16 -0500

    Avoid parallel jenkins jobs (#1173)
    * resume training
    
    * avoid-parallel-jenkins-jobs
    
    * Change executors to 1
    
    Co-authored-by: Slin Lee <slin@skymind.io>
    Co-authored-by: DH <kepricon@gmail.com>

---
+++ infra/images/jenkins/executors.groovy
@@ -0,0 +1,16 @@
+#!groovy
+
+// imports
+import jenkins.model.Jenkins
+
+// parameter
+Integer numberOfExecutors = 1
+
+// get Jenkins instance
+Jenkins jenkins = Jenkins.getInstance()
+
+// set the number of slaves
+jenkins.setNumExecutors(numberOfExecutors)
+
+// save current Jenkins state to disk
+jenkins.save()
