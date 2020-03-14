5807c1435174cae8d0c70123946f132f6844d679
Slin Lee <slin@skymind.io>
2020-03-14 15:12:37 -0700

    Extend training time to 24 hour per training

--- src/main/java/io/skymind/pathmind/services/training/constant/RunConstants.java
+++ src/main/java/io/skymind/pathmind/services/training/constant/RunConstants.java
@@ -15,7 +15,7 @@ public final class RunConstants {
 	 * Number of possible iterations for PBT run
 	 */
 	public static final int PBT_RUN_ITERATIONS = 250;
-	public static final int PBT_MAX_TIME_IN_SEC = 150 * MINUTE;
+	public static final int PBT_MAX_TIME_IN_SEC = 24 * HOUR; // Setting this for GHD March 14, 2020 - Slin
 	public static final int PBT_NUM_SAMPLES = 10;
 
 	public static final String DISCOVERY_RUN_LEARNING_RATES = "DISCOVERY_RUN_LEARNING_RATES";
