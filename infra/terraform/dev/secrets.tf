c86dfde3330d5ec818a68b2aeb5eb969baa0e856
Daniel Jaramillo <xneyder@gmail.com>
2020-03-22 22:42:58 -0500

    Add support fro pathmind admin server

--- infra/terraform/dev/secrets.tf
+++ infra/terraform/dev/secrets.tf
@@ -70,3 +70,58 @@ resource "null_resource" "segment_server_key_secret" {
   depends_on = ["null_resource.validate_k8s"]
 }
 
+resource "null_resource" "db_url_secret_prod" {
+  provisioner "local-exec" {
+    command = "kubectl create secret generic dburlprod --from-literal DB_URL_PROD=${var.DB_URL_PROD}"
+  }
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "kubectl delete secret dburlprod"
+  }
+  depends_on = ["null_resource.validate_k8s"]
+}
+
+resource "null_resource" "db_url_secret_dev" {
+  provisioner "local-exec" {
+    command = "kubectl create secret generic dburldev --from-literal DB_URL_DEV=${var.DB_URL_DEV}"
+  }
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "kubectl delete secret dburldev"
+  }
+  depends_on = ["null_resource.validate_k8s"]
+}
+
+resource "null_resource" "db_url_secret_test" {
+  provisioner "local-exec" {
+    command = "kubectl create secret generic dburltest --from-literal DB_URL_TEST=${var.DB_URL_TEST}"
+  }
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "kubectl delete secret dburltest"
+  }
+  depends_on = ["null_resource.validate_k8s"]
+}
+
+resource "null_resource" "githubuser" {
+  provisioner "local-exec" {
+    command = "kubectl create secret generic githubuser --from-literal GITHUB_USER=${var.githubuser}"
+  }
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "kubectl delete secret githubuser"
+  }
+  depends_on = ["null_resource.validate_k8s"]
+}
+
+resource "null_resource" "githubsecret" {
+  provisioner "local-exec" {
+    command = "kubectl create secret generic githubsecret --from-literal GITHUB_SECRET=${var.githubsecret}"
+  }
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "kubectl delete secret githubsecret"
+  }
+  depends_on = ["null_resource.validate_k8s"]
+}
+
