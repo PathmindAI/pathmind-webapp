cc5dd5472868a93c5680e313ad33286493576a2e
Slin Lee <slin@skymind.io>
2020-03-26 15:01:45 -0700

    Don't roll up hours into days
    Applies IntelliJ syntax formatting too.

--- pathmind-shared/src/main/java/io/skymind/pathmind/shared/utils/DateAndTimeUtils.java
+++ pathmind-shared/src/main/java/io/skymind/pathmind/shared/utils/DateAndTimeUtils.java
@@ -9,81 +9,76 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class DateAndTimeUtils
-{
-	public static final DateTimeFormatter STANDARD_DATE_ONLY_FOMATTER = DateTimeFormatter.ofPattern("MMM d yyyy");
-	public static final DateTimeFormatter STANDARD_DATE_AND_TIME_FOMATTER = DateTimeFormatter.ofPattern("MMM d yyyy H:mm");
-	public static final DateTimeFormatter STANDARD_DATE_AND_TIME_SHORT_FOMATTER = DateTimeFormatter.ofPattern("MMM d H:mm");
+public class DateAndTimeUtils {
+    public static final DateTimeFormatter STANDARD_DATE_ONLY_FOMATTER = DateTimeFormatter.ofPattern("MMM d yyyy");
+    public static final DateTimeFormatter STANDARD_DATE_AND_TIME_FOMATTER = DateTimeFormatter.ofPattern("MMM d yyyy H:mm");
+    public static final DateTimeFormatter STANDARD_DATE_AND_TIME_SHORT_FOMATTER = DateTimeFormatter.ofPattern("MMM d H:mm");
 
-	public static final String formatDateAndTimeShortFormatter(LocalDateTime localDateTime, String userZoneId) {
-		if(localDateTime == null) {
-			return "-";
-		}
-		if (userZoneId == null) {
-			return STANDARD_DATE_AND_TIME_SHORT_FOMATTER.format(localDateTime);
-		} else {
-			ZonedDateTime serverDateTime = ZonedDateTime.of(localDateTime, ZoneId.systemDefault());
-			ZonedDateTime userDateTime = serverDateTime.withZoneSameInstant(ZoneId.of(userZoneId));
-			return STANDARD_DATE_AND_TIME_SHORT_FOMATTER.format(userDateTime);
-		}
-	}
-	
-	/**
-	 * We could use org.apache.commons.lang3.time.DurationFormatUtils but it seems overkill for what we need and I didn't want to spend the time
-	 * to figure out how to conditionally printout the hours, minutes, and seconds.
-	 */
-	public static final String formatDurationTime(long totalSeconds)
-	{
-		if(totalSeconds == 0)
-			return "0 sec";
+    public static final String formatDateAndTimeShortFormatter(LocalDateTime localDateTime, String userZoneId) {
+        if (localDateTime == null) {
+            return "-";
+        }
+        if (userZoneId == null) {
+            return STANDARD_DATE_AND_TIME_SHORT_FOMATTER.format(localDateTime);
+        } else {
+            ZonedDateTime serverDateTime = ZonedDateTime.of(localDateTime, ZoneId.systemDefault());
+            ZonedDateTime userDateTime = serverDateTime.withZoneSameInstant(ZoneId.of(userZoneId));
+            return STANDARD_DATE_AND_TIME_SHORT_FOMATTER.format(userDateTime);
+        }
+    }
 
-		long hours = totalSeconds / 3600;
-		long minutes = (totalSeconds % 3600) / 60;
-		long seconds = totalSeconds % 60;
+    /**
+     * We could use org.apache.commons.lang3.time.DurationFormatUtils but it seems overkill for what we need and I didn't want to spend the time
+     * to figure out how to conditionally printout the hours, minutes, and seconds.
+     */
+    public static final String formatDurationTime(long totalSeconds) {
+        if (totalSeconds == 0)
+            return "0 sec";
 
-		return  (hours > 0 ? (hours + " hr ") : "") +
-				(minutes > 0 ? (minutes + " min ") : "") +
-				(seconds > 0 ? (seconds + " sec") : "");
-	}
-	
-	public static String formatETA(long totalSeconds)
-	{
-		long hours = Math.round(totalSeconds / 3600d);
-		long minutes = Math.round((totalSeconds % 3600) / 60d);
+        long hours = totalSeconds / 3600;
+        long minutes = (totalSeconds % 3600) / 60;
+        long seconds = totalSeconds % 60;
 
-		if(hours >= 12) 
-			return "1 day";
-		else if (hours > 0)
-			return hours + " hr";
-		else if (minutes > 0)
-			return minutes + " min";
-		else
-			return "less than a minute";
-	}
+        return (hours > 0 ? (hours + " hr ") : "") +
+                (minutes > 0 ? (minutes + " min ") : "") +
+                (seconds > 0 ? (seconds + " sec") : "");
+    }
 
-	public static NumberFormat getElapsedTimeNumberFormat() {
-		return new NumberFormat() {
-			@Override
-			public StringBuffer format(double number, StringBuffer toAppendTo, FieldPosition pos) {
-				throw new RuntimeException("Invalid use of Elapsed Time Formatter");
-			}
+    public static String formatETA(long totalSeconds) {
+        long hours = Math.round(totalSeconds / 3600d);
+        long minutes = Math.round((totalSeconds % 3600) / 60d);
 
-			@Override
-			public StringBuffer format(long number, StringBuffer toAppendTo, FieldPosition pos) {
-				return new StringBuffer(formatDurationTime(number));
-			}
+        if (hours > 0)
+            return hours + " hr";
+        else if (minutes > 0)
+            return minutes + " min";
+        else
+            return "less than a minute";
+    }
 
-			@Override
-			public Number parse(String source, ParsePosition parsePosition) {
-				return Long.parseLong(source);
-			}
-		};
-	}
+    public static NumberFormat getElapsedTimeNumberFormat() {
+        return new NumberFormat() {
+            @Override
+            public StringBuffer format(double number, StringBuffer toAppendTo, FieldPosition pos) {
+                throw new RuntimeException("Invalid use of Elapsed Time Formatter");
+            }
 
-	/**
-	 * This is used in Stripe integration, since Stripe keeps Timestamps as epochSeconds
-	 */
-	public static LocalDateTime fromEpoch(long epoch) {
-		return LocalDateTime.ofInstant(Instant.ofEpochSecond(epoch), ZoneId.systemDefault());
-	}
+            @Override
+            public StringBuffer format(long number, StringBuffer toAppendTo, FieldPosition pos) {
+                return new StringBuffer(formatDurationTime(number));
+            }
+
+            @Override
+            public Number parse(String source, ParsePosition parsePosition) {
+                return Long.parseLong(source);
+            }
+        };
+    }
+
+    /**
+     * This is used in Stripe integration, since Stripe keeps Timestamps as epochSeconds
+     */
+    public static LocalDateTime fromEpoch(long epoch) {
+        return LocalDateTime.ofInstant(Instant.ofEpochSecond(epoch), ZoneId.systemDefault());
+    }
 }
