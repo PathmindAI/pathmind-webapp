958ae67c3f0c0921bfa9ca79015416fa52152cd6
kepricon <kepricon@gmail.com>
2020-04-06 13:43:18 -0700

    change the default instance type to 16 cpus and 32gb memory

--- pathmind-services/src/main/java/io/skymind/pathmind/services/training/cloud/aws/api/AWSApiClient.java
+++ pathmind-services/src/main/java/io/skymind/pathmind/services/training/cloud/aws/api/AWSApiClient.java
@@ -136,7 +136,7 @@ public class AWSApiClient {
     public String jobSubmit(String jobId, RunType type) throws JsonProcessingException {
         final String mockType = type == null ? null : type.toString();
         Job job = new Job(bucketName, jobId, mockCycle, mockType);
-        job.setEc2InstanceType(EC2InstanceType.DEFAULT);
+        job.setEc2InstanceType(EC2InstanceType.IT_16CPU_32GB);
 
         SendMessageRequest send_msg_request = new SendMessageRequest()
                 .withQueueUrl(queueUrl)
