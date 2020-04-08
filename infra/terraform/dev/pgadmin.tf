292fe189b79e319d597c3b254be6139de667b8a7
Daniel Jaramillo <xneyder@gmail.com>
2020-04-08 12:43:57 -0500

    Add pgadmin to helm

--- infra/terraform/dev/pgadmin.tf
+++ infra/terraform/dev/pgadmin.tf
@@ -1,11 +1,11 @@
 #pgadmin
 resource "null_resource" "pgadmin" {
   provisioner "local-exec" {
-    command = "kubectl apply -f ../../k8s/pgadmin.yaml"
+    command = "helm install pgadmin ../../helm/pgadmin"
   }
   provisioner "local-exec" {
     when    = "destroy"
-    command = "kubectl delete deployment pgadmin; kubectl delete svc pgadmin"
+    command = "helm delete pgadmin"
   }
   depends_on = ["null_resource.validate_k8s"]
 }
