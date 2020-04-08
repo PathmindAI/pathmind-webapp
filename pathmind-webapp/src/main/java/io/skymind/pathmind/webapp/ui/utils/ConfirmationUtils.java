00184316e5be5249eef4a9669b21261d4b3164f0
Onur Idrisoglu <onur@vaadin.com>
2020-04-08 14:13:21 +0300

    Added ConfirmationUtils for managing confirmation messages from one place

---
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/utils/ConfirmationUtils.java
@@ -0,0 +1,29 @@
+package io.skymind.pathmind.webapp.ui.utils;
+
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
+import com.vaadin.flow.server.Command;
+
+import io.skymind.pathmind.shared.utils.PathmindStringUtils;
+
+public class ConfirmationUtils {
+
+	public static void archive(String entityName, Command confirmAction) {
+		String header = String.format("Archive this %s?", entityName);
+		String text = String.format("This hides it from your main workspaces so you can stay organized. You can always unarchive %ss.", entityName);
+		String confirmText = String.format("Archive %s", PathmindStringUtils.toCamelCase(entityName));
+		
+		ConfirmDialog dialog = new ConfirmDialog(header, text, confirmText, evt -> confirmAction.execute());
+		dialog.setCancelButton("Cancel", evt -> dialog.close());
+		dialog.open();
+	}
+	
+	public static void unarchive(String entityName, Command confirmAction) {
+		String header = "Confirm Unarchive";
+		String text = String.format("Are you sure you want to unarchive this %s?", entityName);
+		String confirmText = String.format("Unarchive %s", PathmindStringUtils.toCamelCase(entityName));
+		
+		ConfirmDialog dialog = new ConfirmDialog(header, text, confirmText, evt -> confirmAction.execute());
+		dialog.setCancelButton("Cancel", evt -> dialog.close());
+		dialog.open();
+	}
+}
