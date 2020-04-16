8f11a6d55ec3e6f9a872509d57f9d89fb093e6c7
Alexander <alexander.makarov@toptal.com>
2020-04-16 18:48:52 +0800

    (de-)serialization of run and policy; send/receive serialized objects and firing corresponding EvenBus event

--- pathmind-shared/src/main/java/io/skymind/pathmind/shared/data/RewardScore.java
+++ pathmind-shared/src/main/java/io/skymind/pathmind/shared/data/RewardScore.java
@@ -1,5 +1,10 @@
 package io.skymind.pathmind.shared.data;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class RewardScore {
     // May be NaN!
     private Double max;
@@ -9,9 +14,6 @@ public class RewardScore {
     private Integer iteration;
     private Integer episodeCount;
 
-    // for deserialization
-    private RewardScore(){}
-
     public RewardScore(Double max, Double min, Double mean, Integer iteration, Integer episodeCount) {
         this.max = max;
         this.min = min;
@@ -20,43 +22,4 @@ public class RewardScore {
         this.episodeCount = episodeCount;
     }
 
-    public Double getMax() {
-        return max;
-    }
-
-    public void setMax(Double max) {
-        this.max = max;
-    }
-
-    public Double getMin() {
-        return min;
-    }
-
-    public void setMin(Double min) {
-        this.min = min;
-    }
-
-    public Double getMean() {
-        return mean;
-    }
-
-    public void setMean(Double mean) {
-        this.mean = mean;
-    }
-
-    public Integer getIteration() {
-        return iteration;
-    }
-
-    public void setIteration(Integer iteration) {
-        this.iteration = iteration;
-    }
-
-    public Integer getEpisodeCount() {
-        return episodeCount;
-    }
-
-    public void setEpisodeCount(Integer episodeCount) {
-        this.episodeCount = episodeCount;
-    }
 }
--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/bus/UpdaterListener.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/bus/UpdaterListener.java
@@ -7,11 +7,20 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.skymind.pathmind.services.training.cloud.aws.api.AWSApiClient;
 import io.skymind.pathmind.services.training.cloud.aws.api.dto.UpdateEvent;
+import io.skymind.pathmind.shared.data.Policy;
+import io.skymind.pathmind.shared.data.Run;
+import io.skymind.pathmind.webapp.bus.events.PolicyUpdateBusEvent;
+import io.skymind.pathmind.webapp.bus.events.RunUpdateBusEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.Collections;
 import java.util.List;
+
+import static io.skymind.pathmind.services.training.cloud.aws.api.dto.UpdateEvent.*;
 
 @Slf4j
 @Component
@@ -33,6 +42,7 @@ public class UpdaterListener {
         do {
             log.debug("pulling next 10");
             ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(awsApiClient.getUpdaterQueueUrl())
+                    .withMessageAttributeNames("All")
                     .withWaitTimeSeconds(1)
                     .withMaxNumberOfMessages(10);
 
@@ -41,9 +51,35 @@ public class UpdaterListener {
 
             sqsMessages.parallelStream().forEach(message -> {
                 try {
+
+                    ByteBuffer buf = message.getMessageAttributes().get(CARGO_ATTRIBUTE).getBinaryValue();
+                    byte[] arr = new byte[buf.remaining()];
+                    buf.get(arr);
+
                     UpdateEvent event = objectMapper.readValue(message.getBody(), UpdateEvent.class);
-                    log.warn("{}", event);
-                    // TODO: EvenBus.....
+
+                    try (ByteArrayInputStream bis = new ByteArrayInputStream(arr);
+                         ObjectInputStream in = new ObjectInputStream(bis)) {
+
+                        switch (event.getType().toLowerCase()) {
+                            case TYPE_POLICY: {
+                                Policy policy = (Policy) in.readObject();
+                                EventBus.post(new PolicyUpdateBusEvent(Collections.singletonList(policy)));
+                                break;
+                            }
+                            case TYPE_RUN: {
+                                Run run = (Run) in.readObject();
+                                EventBus.post(new RunUpdateBusEvent(run));
+                                break;
+                            }
+                            default:
+                                log.warn("Unexpected object event: {}", event);
+                        }
+
+                    } catch (IOException | ClassNotFoundException e) {
+                        log.error("Failed to deserialize event: {}", event, e);
+                    }
+
                 } catch (JsonProcessingException e) {
                     log.error("Failed to process message [{}]", message, e);
                 }
