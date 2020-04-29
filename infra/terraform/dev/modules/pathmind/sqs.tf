80e1f4ac18680b0e2a4f851a435499ce21dbf604
Daniel Jaramillo <xneyder@gmail.com>
2020-04-28 20:14:37 -0500

    Add sns subscriptions

--- infra/terraform/dev/modules/pathmind/sqs.tf
+++ infra/terraform/dev/modules/pathmind/sqs.tf
@@ -18,3 +18,112 @@ resource "aws_sqs_queue" "updater_queue" {
   }
 }
 
+
+resource "aws_sqs_queue" "updater_queue_alexander" {
+  name                      = "alexander-updater-queue-${var.environment}"
+  message_retention_seconds   = 60
+
+  tags = {
+    Environment = "pathmind"
+  }
+}
+
+resource "aws_sqs_queue" "updater_queue_ali" {
+  name                      = "ali-updater-queue-${var.environment}"
+  message_retention_seconds   = 60
+
+  tags = {
+    Environment = "pathmind"
+  }
+}
+
+resource "aws_sqs_queue" "updater_queue_brett" {
+  name                      = "brett-updater-queue-${var.environment}"
+  message_retention_seconds   = 60
+
+  tags = {
+    Environment = "pathmind"
+  }
+}
+
+resource "aws_sqs_queue" "updater_queue_bruno" {
+  name                      = "bruno-updater-queue-${var.environment}"
+  message_retention_seconds   = 60
+
+  tags = {
+    Environment = "pathmind"
+  }
+}
+
+resource "aws_sqs_queue" "updater_queue_daeh" {
+  name                      = "daeh-updater-queue-${var.environment}"
+  message_retention_seconds   = 60
+
+  tags = {
+    Environment = "pathmind"
+  }
+}
+
+resource "aws_sqs_queue" "updater_queue_edward" {
+  name                      = "edward-updater-queue-${var.environment}"
+  message_retention_seconds   = 60
+
+  tags = {
+    Environment = "pathmind"
+  }
+}
+
+resource "aws_sqs_queue" "updater_queue_evgeniy" {
+  name                      = "evgeniy-updater-queue-${var.environment}"
+  message_retention_seconds   = 60
+
+  tags = {
+    Environment = "pathmind"
+  }
+}
+
+resource "aws_sqs_queue" "updater_queue_fionna" {
+  name                      = "fionna-updater-queue-${var.environment}"
+  message_retention_seconds   = 60
+
+  tags = {
+    Environment = "pathmind"
+  }
+}
+
+resource "aws_sqs_queue" "updater_queue_henri" {
+  name                      = "henri-updater-queue-${var.environment}"
+  message_retention_seconds   = 60
+
+  tags = {
+    Environment = "pathmind"
+  }
+}
+
+resource "aws_sqs_queue" "updater_queue_onur" {
+  name                      = "onur-updater-queue-${var.environment}"
+  message_retention_seconds   = 60
+
+  tags = {
+    Environment = "pathmind"
+  }
+}
+
+resource "aws_sqs_queue" "updater_queue_slin" {
+  name                      = "slin-updater-queue-${var.environment}"
+  message_retention_seconds   = 60
+
+  tags = {
+    Environment = "pathmind"
+  }
+}
+
+resource "aws_sqs_queue" "updater_queue_steph" {
+  name                      = "steph-updater-queue-${var.environment}"
+  message_retention_seconds   = 60
+
+  tags = {
+    Environment = "pathmind"
+  }
+}
+
