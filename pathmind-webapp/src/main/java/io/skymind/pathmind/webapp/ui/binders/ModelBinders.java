d9800b5ad4ca4dddd8ceedc27b3bf971d76c1e1a
Bruno Rezende <brunovianarezende@gmail.com>
2020-04-29 17:19:25 -0300

    70: Notes might have at most 1000 characters

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/binders/ModelBinders.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/binders/ModelBinders.java
@@ -2,6 +2,7 @@ package io.skymind.pathmind.webapp.ui.binders;
 
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.validator.StringLengthValidator;
 import io.skymind.pathmind.shared.data.Model;
 
 public class ModelBinders
@@ -9,6 +10,7 @@ public class ModelBinders
 	public static void bindNotesFieldTextArea(Binder<Model> binder, TextArea notesFieldTextArea)
 	{
 		binder.forField(notesFieldTextArea)
+				.withValidator(new StringLengthValidator("Notes might have at most 1000 characters", 0, 1000))
 				.bind(Model::getUserNotes, Model::setUserNotes);
 	}
 }
