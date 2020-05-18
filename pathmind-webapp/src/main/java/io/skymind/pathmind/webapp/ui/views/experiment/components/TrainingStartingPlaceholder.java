19a866fbf7addf507ad7c4e4a9c1eb41dde53e6d
Fionna <fionnacst@gmail.com>
2020-05-18 11:59:21 +0800

    Adjust position of training starting placeholder text

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/experiment/components/TrainingStartingPlaceholder.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/experiment/components/TrainingStartingPlaceholder.java
@@ -7,10 +7,10 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 public class TrainingStartingPlaceholder extends VerticalLayout {
 	
 	public TrainingStartingPlaceholder() {
-		add(new Span("Starting the training…"),
+		add(new VerticalLayout(new Span("Starting the training…"),
 			new Paragraph("You’ll see the first results here within a few minutes."),
 			new Paragraph("The Pathmind training process starts a cluster to explore multiple combinations of hyperparameters automatically. We train the policy for your simulation using the best configuration."),
-			new Paragraph("Training will take a few hours. We’ll send you an email when it’s complete!"));
+			new Paragraph("Training will take a few hours. We’ll send you an email when it’s complete!")));
 		addClassName("training-starting-placeholder");
 	}
 }
