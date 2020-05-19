c4296e157b435b0441142fe07a98b9e2f96fad84
Onur Idrisoglu <onur@vaadin.com>
2020-05-19 12:51:48 +0300

    Moved experiment creation logic form UI to DAO level

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/experiment/utils/ExperimentViewNavigationUtils.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/experiment/utils/ExperimentViewNavigationUtils.java
@@ -1,20 +1,15 @@
 package io.skymind.pathmind.webapp.ui.views.experiment.utils;
 
+
 import com.vaadin.flow.component.UI;
 
-import io.skymind.pathmind.shared.data.Experiment;
-import io.skymind.pathmind.webapp.data.utils.ExperimentUtils;
 import io.skymind.pathmind.db.dao.ExperimentDAO;
 import io.skymind.pathmind.webapp.ui.views.experiment.NewExperimentView;
 
 public class ExperimentViewNavigationUtils
 {
-	public static void createAndNavigateToNewExperiment(UI ui, ExperimentDAO experimentDAO, long modelId, String defaultRewardFunction) {
-		String experimentName = Integer.toString (experimentDAO.getExperimentCount(modelId) + 1);
-    	Experiment lastExperiment = experimentDAO.getLastExperimentForModel(modelId);
-    	String rewardFunction = lastExperiment != null ? lastExperiment.getRewardFunction() : defaultRewardFunction; 
-    	Experiment newExperiment = ExperimentUtils.generateNewDefaultExperiment(modelId, experimentName, rewardFunction);
-    	long newExperimentId = experimentDAO.setupNewExperiment(newExperiment);
+	public static void createAndNavigateToNewExperiment(UI ui, ExperimentDAO experimentDAO, long modelId) {
+    	long newExperimentId = experimentDAO.createNewExperiment(modelId);
     	ui.navigate(NewExperimentView.class, newExperimentId);
 	}
 }
