80c351c50a6c81dcb58b92d5e16a7fd451e19c0b
kepricon <kepricon@gmail.com>
2020-05-25 15:50:13 +0900

    handle permission for views

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/security/VaadinSecurityUtils.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/security/VaadinSecurityUtils.java
@@ -1,10 +1,17 @@
 package io.skymind.pathmind.webapp.security;
 
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import com.vaadin.flow.server.ServletHelper;
 import com.vaadin.flow.shared.ApplicationConstants;
+import io.skymind.pathmind.shared.constants.ViewPermission;
 import io.skymind.pathmind.shared.security.SecurityUtils;
+import io.skymind.pathmind.webapp.security.annotation.Permission;
 import io.skymind.pathmind.webapp.ui.views.errors.PageNotFoundView;
 import io.skymind.pathmind.webapp.ui.views.login.EmailVerificationView;
 import io.skymind.pathmind.webapp.ui.views.login.LoginView;
@@ -12,9 +19,12 @@ import io.skymind.pathmind.webapp.ui.views.login.ResetPasswordView;
 import io.skymind.pathmind.webapp.ui.views.login.SignUpView;
 import io.skymind.pathmind.webapp.ui.views.login.VerificationEmailSentView;
 import javax.servlet.http.HttpServletRequest;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+@Slf4j
 public class VaadinSecurityUtils {
 
 	private VaadinSecurityUtils()
@@ -42,10 +52,30 @@ public class VaadinSecurityUtils {
 		}
 
 		Authentication userAuthentication = SecurityContextHolder.getContext().getAuthentication();
+		Optional<Annotation> optionalAnnotation = Arrays.stream(securedClass.getAnnotations())
+                .filter(a -> a.annotationType().equals(Permission.class))
+                .findFirst();
 
 		// All other views require authentication
 		if (!SecurityUtils.isUserLoggedIn(userAuthentication)) {
 			return false;
+		}
+
+		if (optionalAnnotation.isPresent()) {
+			Permission permission = (Permission)optionalAnnotation.get();
+			List<String> neededPermissions = Arrays.asList(permission.permissions()).stream()
+                    .map(ViewPermission::getPermission)
+                    .collect(Collectors.toList());
+            log.info("kepricondebug need : " + neededPermissions);
+			List have = userAuthentication.getAuthorities().stream()
+                    .map(authority -> authority.getAuthority())
+                    .collect(Collectors.toList());
+			log.info("kepricondebug have : " + have);
+            neededPermissions.removeAll(have);
+			if (neededPermissions.size() > 0) {
+				log.info("kepricondebug : need more authority : " + neededPermissions);
+				return false;
+			}
 		}
 		return true;
 
