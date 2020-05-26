c034a818cce4a7476c37043d35e4c1456945e6bb
kepricon <kepricon@gmail.com>
2020-05-26 16:20:27 +0900

    clean up debugging message

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/security/VaadinSecurityUtils.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/security/VaadinSecurityUtils.java
@@ -1,28 +1,23 @@
 package io.skymind.pathmind.webapp.security;
 
-import java.lang.annotation.Annotation;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import com.vaadin.flow.server.ServletHelper;
 import com.vaadin.flow.shared.ApplicationConstants;
 import io.skymind.pathmind.shared.constants.ViewPermission;
 import io.skymind.pathmind.shared.security.SecurityUtils;
 import io.skymind.pathmind.webapp.security.annotation.Permission;
 import io.skymind.pathmind.webapp.ui.views.errors.PageNotFoundView;
-import io.skymind.pathmind.webapp.ui.views.login.EmailVerificationView;
-import io.skymind.pathmind.webapp.ui.views.login.LoginView;
-import io.skymind.pathmind.webapp.ui.views.login.ResetPasswordView;
-import io.skymind.pathmind.webapp.ui.views.login.SignUpView;
-import io.skymind.pathmind.webapp.ui.views.login.VerificationEmailSentView;
-import javax.servlet.http.HttpServletRequest;
-
+import io.skymind.pathmind.webapp.ui.views.login.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 public class VaadinSecurityUtils {
@@ -52,43 +47,46 @@ public class VaadinSecurityUtils {
 		}
 
 		Authentication userAuthentication = SecurityContextHolder.getContext().getAuthentication();
-		Optional<Annotation> optionalAnnotation = Arrays.stream(securedClass.getAnnotations())
-                .filter(a -> a.annotationType().equals(Permission.class))
-                .findFirst();
 
 		// All other views require authentication
 		if (!SecurityUtils.isUserLoggedIn(userAuthentication)) {
 			return false;
 		}
+		return isAuthorityGranted(securedClass);
+	}
 
-		if (optionalAnnotation.isPresent()) {
-			Permission permission = (Permission)optionalAnnotation.get();
-			List<String> neededPermissions = Arrays.asList(permission.permissions()).stream()
+    /**
+     * Checks if authority is granted for the current user's role for the given secured view,
+     * defined by the view class.
+     *
+     * @param securedClass View class
+     * @return true if authority is granted, false otherwise.
+     */
+    public static boolean isAuthorityGranted(Class<?> securedClass) {
+        Authentication userAuthentication = SecurityContextHolder.getContext().getAuthentication();
+        Optional<Annotation> optionalAnnotation = Arrays.stream(securedClass.getAnnotations())
+                .filter(a -> a.annotationType().equals(Permission.class))
+                .findFirst();
+
+        if (optionalAnnotation.isPresent()) {
+            Permission permission = (Permission)optionalAnnotation.get();
+            List<String> neededPermissions = Arrays.asList(permission.permissions()).stream()
                     .map(ViewPermission::getPermission)
                     .collect(Collectors.toList());
-            log.info("kepricondebug need : " + neededPermissions);
-			List have = userAuthentication.getAuthorities().stream()
+
+            List<String> have = userAuthentication.getAuthorities().stream()
                     .map(authority -> authority.getAuthority())
                     .collect(Collectors.toList());
-			log.info("kepricondebug have : " + have);
+
             neededPermissions.removeAll(have);
-			if (neededPermissions.size() > 0) {
-				log.info("kepricondebug : need more authority : " + neededPermissions);
-				return false;
-			}
-		}
-		return true;
+            if (neededPermissions.size() > 0) {
+                log.info("need more authority : " + neededPermissions);
+                return false;
+            }
+        }
 
-		//// Allow if no roles are required.
-		//Secured secured = AnnotationUtils.findAnnotation(securedClass, Secured.class);
-		//if (secured == null) {
-		//	return true;
-		//}
-
-		//List<String> allowedRoles = Arrays.asList(secured.value());
-		//return userAuthentication.getAuthorities().stream().map(GrantedAuthority::getAuthority)
-		//		.anyMatch(allowedRoles::contains);
-	}
+        return  true;
+    }
 
 	/**
 	 * Tests if the request is an internal framework request. The test consists of
