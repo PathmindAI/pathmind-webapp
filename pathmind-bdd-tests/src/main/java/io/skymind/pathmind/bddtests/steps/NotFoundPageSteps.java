02b33272854ffa3a845d838c00e85c8839542b82
Bruno Rezende <brunovianarezende@gmail.com>
2020-06-05 18:31:38 -0300

    766: add link to status.pathmind.com to all error pages (#1687)
    * 766: add link to status.pathmind.com to all error pages
    
    * Add error page tests
    
    * fixed typo
    
    * fix oops title
    
    Co-authored-by: EvgeniyEA <evegeniy@skymind.io>

--- pathmind-bdd-tests/src/main/java/io/skymind/pathmind/bddtests/steps/NotFoundPageSteps.java
+++ pathmind-bdd-tests/src/main/java/io/skymind/pathmind/bddtests/steps/NotFoundPageSteps.java
@@ -7,7 +7,25 @@ public class NotFoundPageSteps {
     private NotFoundPage notFoundPage;
 
     @Step
-    public void check404PageOpened(){
+    public void check404PageOpened() {
         notFoundPage.check404PageOpened();
+        notFoundPage.checkStatusMessageText();
+    }
+
+    @Step
+    public void checkThatOopsPageOpened() {
+        notFoundPage.checkThatOopsPageOpened();
+        notFoundPage.checkStatusMessageText();
+    }
+
+    @Step
+    public void checkThatInvalidDataErrorPageOpened() {
+        notFoundPage.checkThatInvalidDataErrorPageOpened();
+        notFoundPage.checkStatusMessageText();
+    }
+
+    @Step
+    public void checkThatStatusPageOpened(String url) {
+        notFoundPage.checkThatStatusPageOpened(url);
     }
 }
