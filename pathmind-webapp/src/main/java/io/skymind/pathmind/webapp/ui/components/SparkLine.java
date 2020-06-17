cdf539a68021a50f2cdd8de275ceb116ca30e065
Fionna <fionnacst@gmail.com>
2020-06-17 18:16:55 +0800

    Add NpmPackage annotation for new npm package

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/SparkLine.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/SparkLine.java
@@ -5,10 +5,12 @@ import elemental.json.JsonArray;
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.templatemodel.TemplateModel;
 
 @Tag("spark-line")
+@NpmPackage(value = "@fnando/sparkline", version = "0.3.10")
 @JsModule("/src/experiment/spark-line.js")
 public class SparkLine extends PolymerTemplate<TemplateModel> implements HasStyle {
     public SparkLine() {
