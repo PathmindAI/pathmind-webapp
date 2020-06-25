71d4035bafae8f3c2cf4bbe51f5deede06af1070
FollowSteph <steph@followsteph.com>
2020-06-25 00:18:32 -0400

    #1553 - Reset S3 either in maven build script or in liquibase script - Implemented it assuming no injection and so on.

--- pathmind-database/src/main/java/io/skymind/pathmind/db/changeset/UpdatePolicy_ChangeSet_20200624_01.java
+++ pathmind-database/src/main/java/io/skymind/pathmind/db/changeset/UpdatePolicy_ChangeSet_20200624_01.java
@@ -1,5 +1,11 @@
 package io.skymind.pathmind.db.changeset;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
 import liquibase.change.custom.CustomTaskChange;
 import liquibase.database.Database;
 import liquibase.exception.CustomChangeException;
@@ -7,21 +13,66 @@ import liquibase.exception.SetupException;
 import liquibase.exception.ValidationErrors;
 import liquibase.resource.ResourceAccessor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
- * IMPORTANT -> This changeset is to be able to automatically reset the S3 bucket for the developer
+ * This changeset is to be able to automatically reset the S3 bucket for the developers
  */
 @Slf4j
 public class UpdatePolicy_ChangeSet_20200624_01 implements CustomTaskChange
 {
     @Override
     public void execute(Database database) throws CustomChangeException {
-        // @DH -> I'm not sure how it's all wired but would it be possible to call the new method I created called AWSApiClient.emptyBucket() from here?
-        // @DH -> I don't think we need the check for the production environment since AWSApiClient would check the bucket.
-        // @DH -> In addition to getting the instance of AWSApiClient I wasn't sure how the environment variables are injected.
-        // awsApiClient.emptyBucket(bucket);
-        // throw new CustomChangeException("Stop if production");
+        String region = getEnvironmentVariable("AWS_DEFAULT_REGION");
+        String accessKey = getEnvironmentVariable("AWS_ACCESS_KEY_ID");
+        String secretAccessKey = getEnvironmentVariable("AWS_SECRET_ACCESS_KEY");
+        String bucket = getEnvironmentVariable("S3_BUCKET");
 
+        log.info("Emptying bucket : " + bucket);
+
+        // Extra security precaution. I'm not throwing an exception because in those cases we just want to stop.
+        if(Stream.of("dev", "prod", "test").anyMatch(specialBuckets -> bucket.startsWith(specialBuckets)))
+            return;
+
+        AmazonS3 s3Client = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretAccessKey)))
+                .withRegion(region)
+                .build();
+
+        // Amazon S3 limits deletions to 1000 objects per call.
+        ObjectListing objectListing = s3Client.listObjects(bucket);
+        deleteObjects(objectListing, s3Client, bucket);
+        while(objectListing.isTruncated()) {
+            objectListing = s3Client.listNextBatchOfObjects(objectListing);
+            deleteObjects(objectListing, s3Client, bucket);
+        }
+    }
+
+    private String getEnvironmentVariable(String name) throws CustomChangeException {
+        // This is so that developers get notified to update their environment variables to be able to properly delete their S3 buckets as part of a database reset.
+        return Optional.ofNullable(System.getenv(name)).orElseThrow(
+                () -> new CustomChangeException(name + " environment variable is not set"));
+    }
+
+    private void deleteObjects(ObjectListing objectListing, AmazonS3 s3Client, String bucket) {
+        String[] objectKeys = objectListing.getObjectSummaries().stream()
+                .map(object -> object.getKey())
+                .toArray(String[]::new);
+
+        // In case the S3 bucket is already empty.
+        if(ArrayUtils.isEmpty(objectKeys))
+            return;
+
+        DeleteObjectsRequest deleteObjectsRequest = new DeleteObjectsRequest(bucket)
+                .withKeys(objectKeys);
+
+        s3Client.deleteObjects(deleteObjectsRequest);
     }
 
     @Override
