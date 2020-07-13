7ad387860deccdedd756cad8c94bdb08c474bc5d
Bruno Rezende <brunovianarezende@gmail.com>
2020-07-13 15:33:48 -0300

    1664: fix getEnumFromValue

--- pathmind-shared/src/main/java/io/skymind/pathmind/shared/constants/ObservationDataType.java
+++ pathmind-shared/src/main/java/io/skymind/pathmind/shared/constants/ObservationDataType.java
@@ -1,7 +1,9 @@
 package io.skymind.pathmind.shared.constants;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 public enum ObservationDataType {
     NUMBER("number"), INTEGER("int"), BOOLEAN("boolean"), NUMBER_ARRAY("number[]"), INTEGER_ARRAY("int[]");
@@ -22,14 +24,14 @@ public enum ObservationDataType {
 
     private static final Map<String, ObservationDataType> BY_NAME;
     static {
-		Map<Integer,ObservationDataType> map = new ConcurrentHashMap<>();
-		for (RunStatus instance : ObservationDataType.values()) {
+		Map<String,ObservationDataType> map = new ConcurrentHashMap<>();
+		for (ObservationDataType instance : ObservationDataType.values()) {
 			map.put(instance.getValue(), instance);
 		}
 		BY_NAME = Collections.unmodifiableMap(map);
     }
     public static Optional<ObservationDataType> getEnumFromValue(String value) {
-        return Optional.ofNullable(BY_NAME.get(value));
+        return value == null ? Optional.empty() : Optional.ofNullable(BY_NAME.get(value));
     }
     
     public static boolean isNumeric(ObservationDataType dataType) {
