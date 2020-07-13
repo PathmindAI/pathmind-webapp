576c71500a600c02fa2df436d18e2adcfbe60260
Daniel Jaramillo <xneyder@gmail.com>
2020-07-13 13:35:47 -0500

    Stop training after 24 hours (#1764)
    * Stop training after 24 hours
    
    * 1757: added line to training_error. Now the error will be shown in UI.
    
    * 1757: improved error message shown to the user
    
    * Stop training when it crashes more than 3 times
    
    * 1757: fix migration
    
    Co-authored-by: Bruno Rezende <brunovianarezende@gmail.com>

--- infra/images/rl_training/do_training.sh
+++ infra/images/rl_training/do_training.sh
@@ -1,4 +1,74 @@
 #!/bin/bash
+
+NAMESPACE="default"
+if [ "${ENVIRONMENT}" != "prod" ]
+then
+	NAMESPACE=${ENVIRONMENT}
+fi
+
+#Increase the restrats count
+psql "$DB_URL_CLI" << EOF
+update public.trainer_job
+set restarts=restarts+1
+where job_id='${S3PATH}';
+commit;
+EOF
+
+#Checked if pod crashed more than 3 times already
+CRASHES=`kubectl get pods -n ${ENVIRONMENT} | grep ${S3PATH} | grep Evicted | wc -l`
+if [ $CRASHES -ge 3 ]
+then
+        description="Job crashed more than 3 times, job is killed"
+        curl -X POST -H 'Content-type: application/json' \
+        --data "{'text':':x:Job ${S3PATH}\nDescription: ${description}\nEnv: ${ENVIRONMENT}\nUser: ${EMAIL}\nhttps://s3.console.aws.amazon.com/s3/buckets/${s3_url_link}/'}" \
+        https://hooks.slack.com/services/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
+        echo "Job crashed more than 3 times, job is killed"
+        #Set the status in trainer_job
+        psql "$DB_URL_CLI" << EOF
+update public.trainer_job
+set status=5,ec2_end_date=now(),update_date=NOW(),description='${description}'
+where job_id='${S3PATH}';
+commit;
+EOF
+        aws s3 cp ${log_file} ${s3_url}/output/${log_file} > /dev/null
+        echo ${description} > errors.log
+        aws s3 cp errors.log ${s3_url}/output/errors.log > /dev/null
+        aws sqs send-message \
+                --queue-url ${SQS_URL} \
+                --message-body '{"S3Bucket": "'${S3BUCKET}'", "S3Path":"'${S3PATH}'", "destroy":"0"}' \
+                --message-group-id training
+        sleep 1h
+fi
+
+#Check training age and kill it if is older than 24 hours
+AGE=`psql -t "$DB_URL_CLI" << EOF
+select extract(hour from age(NOW(),create_date))  from public.trainer_job where job_id='${S3PATH}'
+EOF`
+
+if [ $AGE -ge 24 ]
+then
+	description="Job running for more than 24 hours, job is killed"
+	curl -X POST -H 'Content-type: application/json' \
+	--data "{'text':':x:Job ${S3PATH}\nDescription: ${description}\nEnv: ${ENVIRONMENT}\nUser: ${EMAIL}\nhttps://s3.console.aws.amazon.com/s3/buckets/${s3_url_link}/'}" \
+	https://hooks.slack.com/services/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
+	echo "Killing on timeout"
+	#Set the status in trainer_job
+	psql "$DB_URL_CLI" << EOF
+update public.trainer_job
+set status=5,ec2_end_date=now(),update_date=NOW(),description='${description}'
+where job_id='${S3PATH}';
+commit;
+EOF
+	aws s3 cp ${log_file} ${s3_url}/output/${log_file} > /dev/null
+	echo ${description} > errors.log
+	aws s3 cp errors.log ${s3_url}/output/errors.log > /dev/null
+	aws sqs send-message \
+		--queue-url ${SQS_URL} \
+		--message-body '{"S3Bucket": "'${S3BUCKET}'", "S3Path":"'${S3PATH}'", "destroy":"0"}' \
+		--message-group-id training
+	sleep 1h
+fi
+
 sleep_time=60
 training_update_timeout=3600
 s3_url="s3://${S3BUCKET}/${S3PATH}"
@@ -48,11 +118,12 @@ then
 	touch restarted
 	aws s3 cp restarted ${s3_url}/output/
 	set +e
+fi
 else
 	#Set the status in trainer_job
 	psql "$DB_URL_CLI" << EOF
 update public.trainer_job
-set status=3,ec2_create_date=now(),update_date=NOW(),ec2_instance_type='${instance_type}',ec2_max_price='${instance_price}'
+set status=3,ec2_create_date=now(),update_date=NOW(),ec2_instance_type='${instance_type}',ec2_max_price='${instance_price}',restarts=0
 where job_id='${S3PATH}';
 commit;
 EOF
@@ -107,7 +178,7 @@ EOF
 			--queue-url ${SQS_URL} \
 			--message-body '{"S3Bucket": "'${S3BUCKET}'", "S3Path":"'${S3PATH}'", "destroy":"0"}' \
 			--message-group-id training
-                sleep 12h
+                sleep 1h
         fi
         sleep $sleep_time
 done
@@ -200,4 +271,4 @@ aws sqs send-message \
 kill -9 $pid_tail
 
 #Sleep until is destroyed
-sleep 12h
+sleep 1h
