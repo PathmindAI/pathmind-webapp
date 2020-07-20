1d6d0a48523dba55760c9111861b325dab72ec31
Fionna <fionnacst@gmail.com>
2020-07-20 13:31:30 +0800

    Fix unaligned row CSS issue for >16 metrics

--- pathmind-webapp/frontend/src/experiment/spark-line.js
+++ pathmind-webapp/frontend/src/experiment/spark-line.js
@@ -13,6 +13,7 @@ class SparkLine extends PolymerElement {
                     display: flex;
                     align-items: center;
                     height: 2rem;
+                    flex: 1 0 2rem;
                 }
                 svg {
                     --sparkline-color: 255, 255, 255;
