2c1fb4db2658a5438197459334e4eee59f5c3e42
Bruno Rezende <brunovianarezende@gmail.com>
2020-07-22 15:54:06 -0300

    1780: allows searches like 'experiment 1' to match the experiment

--- pathmind-database/src/main/java/io/skymind/pathmind/db/dao/SearchDAO.java
+++ pathmind-database/src/main/java/io/skymind/pathmind/db/dao/SearchDAO.java
@@ -1,6 +1,6 @@
 package io.skymind.pathmind.db.dao;
 
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -14,52 +14,54 @@ import io.skymind.pathmind.shared.data.SearchResult;
 public class SearchDAO {
     
     private final DSLContext ctx;
-    private SearchResultItemType targetType = null;
-
     SearchDAO(DSLContext ctx) {
         this.ctx = ctx;
     }
 
     public List<SearchResult> findSearchResults(long userId, String keyword, int offset, int limit){
-        // String processedKeyword = processKeyword(keyword.toLowerCase());
-        List<SearchResult> originalSearchResults = SearchRepository.findSearchResults(ctx, userId, keyword, offset, limit);
-        // List<SearchResult> originalSearchResults = SearchRepository.findSearchResults(ctx, userId, processedKeyword, offset, limit);
-        // if (targetType != null) {
-        //     return originalSearchResults
-        //             .stream()
-        //             .filter(searchResult -> searchResult.getItemType().equals(targetType))
-        //             .collect(Collectors.toList());
-        // }
+        ProcessKeywordResult processedKeyword = processKeyword(keyword.toLowerCase());
+        List<SearchResult> originalSearchResults = SearchRepository.findSearchResults(ctx, processedKeyword.itemsTypes,
+                userId, processedKeyword.keyword, offset, limit);
         return originalSearchResults;
     }
     
     public int countSearchResults(long userId, String keyword){
-        // String processedKeyword = processKeyword(keyword.toLowerCase());
-        // if (targetType != null) {
-        //     return 0; // TODO: count search results with targetType as itemType and keyword in the
-        // }
-        return SearchRepository.countSearchResults(ctx, userId, keyword); 
+        ProcessKeywordResult processedKeyword = processKeyword(keyword.toLowerCase());
+        return SearchRepository.countSearchResults(ctx, processedKeyword.itemsTypes, userId, processedKeyword.keyword);
     }
 
-    private String processKeyword(String originalKeyword) {
-        String keyword = originalKeyword;
+    private ProcessKeywordResult processKeyword(String originalKeyword) {
+
         String modelName = SearchResultItemType.MODEL.getName().toLowerCase();
         String experimentName = SearchResultItemType.EXPERIMENT.getName().toLowerCase();
 
-        if (findTypeInKeyword(modelName, keyword) && !modelName.equals(keyword)) {
-            targetType = SearchResultItemType.MODEL;
-            keyword = keyword.replaceFirst(modelName+" #", "");
-            keyword = keyword.replaceFirst(modelName+" ", "");
-        } else if (findTypeInKeyword(SearchResultItemType.EXPERIMENT.getName(), keyword) && !experimentName.equals(keyword)) {
-            targetType = SearchResultItemType.EXPERIMENT;
-            keyword = keyword.replaceFirst(experimentName+" #", "");
-            keyword = keyword.replaceFirst(experimentName+" ", "");
+        if (findTypeInKeyword(modelName, originalKeyword) && !modelName.equals(originalKeyword)) {
+            ProcessKeywordResult result = new ProcessKeywordResult();
+            result.itemsTypes = Collections.singleton(SearchResultItemType.MODEL);
+            result.keyword = originalKeyword.replaceFirst(modelName + " #", "")
+                    .replaceFirst(modelName + " ", "");
+            return result;
+        } else if (findTypeInKeyword(SearchResultItemType.EXPERIMENT.getName(), originalKeyword) && !experimentName.equals(originalKeyword)) {
+            ProcessKeywordResult result = new ProcessKeywordResult();
+            result.itemsTypes = Collections.singleton(SearchResultItemType.EXPERIMENT);
+            result.keyword = originalKeyword.replaceFirst(experimentName + " #", "")
+                    .replaceFirst(experimentName + " ", "");
+            return result;
         }
-
-        return keyword;
+        else {
+            ProcessKeywordResult result = new ProcessKeywordResult();
+            result.itemsTypes = new HashSet<>(Arrays.asList(SearchResultItemType.values()));
+            result.keyword = originalKeyword;
+            return result;
+        }
     }
 
     private Boolean findTypeInKeyword(String typeName, String keyword) {
         return Pattern.compile(Pattern.quote(typeName), Pattern.CASE_INSENSITIVE).matcher(keyword).find();
+    }
+
+    private static class ProcessKeywordResult {
+        private Collection<SearchResultItemType> itemsTypes;
+        private String keyword;
     }
 }
--- pathmind-database/src/main/java/io/skymind/pathmind/db/dao/SearchRepository.java
+++ pathmind-database/src/main/java/io/skymind/pathmind/db/dao/SearchRepository.java
@@ -5,11 +5,11 @@ import static io.skymind.pathmind.db.jooq.tables.Model.MODEL;
 import static io.skymind.pathmind.db.jooq.tables.Project.PROJECT;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-import org.jooq.DSLContext;
-import org.jooq.Record9;
-import org.jooq.Table;
+import org.jooq.*;
 import org.jooq.impl.DSL;
 
 import io.skymind.pathmind.shared.constants.SearchResultItemType;
@@ -17,8 +17,10 @@ import io.skymind.pathmind.shared.data.SearchResult;
 
 public class SearchRepository {
 
-    protected static List<SearchResult> findSearchResults(DSLContext ctx, long userId, String keyword, int offset, int limit) {
-        Table<Record9<String, Long, Boolean, LocalDateTime, LocalDateTime, String, String, String, String>> searchQueryTable = generateSearchQueryTable(ctx, userId, keyword);
+    protected static List<SearchResult> findSearchResults(DSLContext ctx, Collection<SearchResultItemType> itemsTypes, long userId,
+                                                          String keyword, int offset, int limit) {
+        assert !itemsTypes.isEmpty();
+        Table<Record9<String, Long, Boolean, LocalDateTime, LocalDateTime, String, String, String, String>> searchQueryTable = generateSearchQueryTable(ctx, itemsTypes, userId, keyword);
         return ctx
                 .selectFrom(searchQueryTable)
                 .orderBy(searchQueryTable.field("lastActivity").desc())
@@ -27,25 +29,41 @@ public class SearchRepository {
                 .fetch(record -> fetchIntoSearchResult(record));
     }
     
-    protected static int countSearchResults(DSLContext ctx, long userId, String keyword) {
-        return ctx.selectCount().from(generateSearchQueryTable(ctx, userId, keyword)).fetchOne(DSL.count());
+    protected static int countSearchResults(DSLContext ctx, Collection<SearchResultItemType> itemsTypes, long userId,
+                                            String keyword) {
+        assert !itemsTypes.isEmpty();
+        return ctx.selectCount().from(generateSearchQueryTable(ctx, itemsTypes, userId, keyword)).fetchOne(DSL.count());
     }
     
-    private static Table<Record9<String, Long, Boolean, LocalDateTime, LocalDateTime, String, String, String, String>> generateSearchQueryTable(DSLContext ctx, long userId, String keyword){
+    private static Table<Record9<String, Long, Boolean, LocalDateTime, LocalDateTime, String, String, String, String>> generateSearchQueryTable(DSLContext ctx, Collection<SearchResultItemType> itemsTypes, long userId, String keyword){
+        assert !itemsTypes.isEmpty();
         String searchValue = "%" + keyword.replace("%", "\\%").replace("_", "\\_") + "%";
-        return ctx.select(DSL.inline(SearchResultItemType.PROJECT.getName()).as("itemType"), PROJECT.ID, PROJECT.ARCHIVED, PROJECT.DATE_CREATED, PROJECT.LAST_ACTIVITY_DATE.as("lastActivity"), PROJECT.USER_NOTES, PROJECT.NAME.as("projectName"), DSL.inline("").as("modelName"), DSL.inline("").as("experimentName"))
-                .from(PROJECT)
-                .where(PROJECT.PATHMIND_USER_ID.eq(userId).and(PROJECT.NAME.likeIgnoreCase(searchValue).or(PROJECT.USER_NOTES.likeIgnoreCase(searchValue))))
-        .union(ctx.select(DSL.inline(SearchResultItemType.MODEL.getName()).as("itemType"), MODEL.ID, MODEL.ARCHIVED, MODEL.DATE_CREATED, MODEL.LAST_ACTIVITY_DATE.as("lastActivity"), MODEL.USER_NOTES, PROJECT.NAME.as("projectName"), MODEL.NAME.as("modelName"), DSL.inline("").as("experimentName"))
-                .from(MODEL)
-                .innerJoin(PROJECT).on(MODEL.PROJECT_ID.eq(PROJECT.ID))
-                .where(PROJECT.PATHMIND_USER_ID.eq(userId)).and(MODEL.NAME.equal(keyword).or(MODEL.USER_NOTES.likeIgnoreCase(searchValue))))
-        .union(ctx.select(DSL.inline(SearchResultItemType.EXPERIMENT.getName()).as("itemType"), EXPERIMENT.ID, EXPERIMENT.ARCHIVED, EXPERIMENT.DATE_CREATED, EXPERIMENT.LAST_ACTIVITY_DATE.as("lastActivity"), EXPERIMENT.USER_NOTES, PROJECT.NAME.as("projectName"), MODEL.NAME.as("modelName"), EXPERIMENT.NAME.as("experimentName"))
-                .from(EXPERIMENT)
-                .innerJoin(MODEL).on(EXPERIMENT.MODEL_ID.eq(MODEL.ID))
-                .innerJoin(PROJECT).on(MODEL.PROJECT_ID.eq(PROJECT.ID))
-                .where(PROJECT.PATHMIND_USER_ID.eq(userId).and(EXPERIMENT.NAME.equal(keyword).or(EXPERIMENT.USER_NOTES.likeIgnoreCase(searchValue)))))
-                .asTable();
+
+        List<SelectOptionStep<Record9<String, Long, Boolean, LocalDateTime, LocalDateTime, String, String, String, String>>> queries = new ArrayList<>();
+
+        if (itemsTypes.contains(SearchResultItemType.PROJECT)) {
+            SelectConditionStep<Record9<String, Long, Boolean, LocalDateTime, LocalDateTime, String, String, String, String>> projectsQuery = ctx.select(DSL.inline(SearchResultItemType.PROJECT.getName()).as("itemType"), PROJECT.ID, PROJECT.ARCHIVED, PROJECT.DATE_CREATED, PROJECT.LAST_ACTIVITY_DATE.as("lastActivity"), PROJECT.USER_NOTES, PROJECT.NAME.as("projectName"), DSL.inline("").as("modelName"), DSL.inline("").as("experimentName"))
+                    .from(PROJECT)
+                    .where(PROJECT.PATHMIND_USER_ID.eq(userId).and(PROJECT.NAME.likeIgnoreCase(searchValue).or(PROJECT.USER_NOTES.likeIgnoreCase(searchValue))));
+            queries.add(projectsQuery);
+        }
+        if (itemsTypes.contains(SearchResultItemType.MODEL)) {
+            SelectConditionStep<Record9<String, Long, Boolean, LocalDateTime, LocalDateTime, String, String, String, String>> modelsQuery = ctx.select(DSL.inline(SearchResultItemType.MODEL.getName()).as("itemType"), MODEL.ID, MODEL.ARCHIVED, MODEL.DATE_CREATED, MODEL.LAST_ACTIVITY_DATE.as("lastActivity"), MODEL.USER_NOTES, PROJECT.NAME.as("projectName"), MODEL.NAME.as("modelName"), DSL.inline("").as("experimentName"))
+                    .from(MODEL)
+                    .innerJoin(PROJECT).on(MODEL.PROJECT_ID.eq(PROJECT.ID))
+                    .where(PROJECT.PATHMIND_USER_ID.eq(userId)).and(MODEL.NAME.equal(keyword).or(MODEL.USER_NOTES.likeIgnoreCase(searchValue)));
+            queries.add(modelsQuery);
+        }
+        if (itemsTypes.contains(SearchResultItemType.EXPERIMENT)) {
+            SelectConditionStep<Record9<String, Long, Boolean, LocalDateTime, LocalDateTime, String, String, String, String>> experimentsQuery = ctx.select(DSL.inline(SearchResultItemType.EXPERIMENT.getName()).as("itemType"), EXPERIMENT.ID, EXPERIMENT.ARCHIVED, EXPERIMENT.DATE_CREATED, EXPERIMENT.LAST_ACTIVITY_DATE.as("lastActivity"), EXPERIMENT.USER_NOTES, PROJECT.NAME.as("projectName"), MODEL.NAME.as("modelName"), EXPERIMENT.NAME.as("experimentName"))
+                    .from(EXPERIMENT)
+                    .innerJoin(MODEL).on(EXPERIMENT.MODEL_ID.eq(MODEL.ID))
+                    .innerJoin(PROJECT).on(MODEL.PROJECT_ID.eq(PROJECT.ID))
+                    .where(PROJECT.PATHMIND_USER_ID.eq(userId).and(EXPERIMENT.NAME.equal(keyword).or(EXPERIMENT.USER_NOTES.likeIgnoreCase(searchValue))));
+            queries.add(experimentsQuery);
+        }
+
+        return queries.stream().reduce(SelectUnionStep::union).orElseThrow(() -> new RuntimeException("I can't happen")).asTable();
     }
 
     private static SearchResult fetchIntoSearchResult(Record9<String, Long, Boolean, LocalDateTime, LocalDateTime, String, String, String, String> record) {
