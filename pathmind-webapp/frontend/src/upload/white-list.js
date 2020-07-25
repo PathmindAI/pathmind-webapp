92eb0babadf3565446385c0a4ffc5a28b5c5fc8a
kepricon <kepricon@gmail.com>
2020-07-24 17:28:27 -0700

    upload cache folder

--- pathmind-webapp/frontend/src/upload/white-list.js
+++ pathmind-webapp/frontend/src/upload/white-list.js
@@ -2,5 +2,8 @@ export const WHITE_LIST = [
   "^model.jar",
   "^database/db.properties",
   "^database/db.script",
-  "^database/db.data"
+  "^database/db.data",
+  "^cache/giscache",
+  "^cache/giscache.p",
+  "^cache/giscache.t"
 ];
