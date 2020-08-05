f5e477ce49f6b08a6f1404711dcf3f4cad293f48
Bruno Rezende <brunovianarezende@gmail.com>
2020-08-05 15:59:23 -0300

    1780: refactored tests to remove repetitive code

--- pathmind-database/src/test/java/io/skymind/pathmind/db/dao/SearchParserTest.java
+++ pathmind-database/src/test/java/io/skymind/pathmind/db/dao/SearchParserTest.java
@@ -8,110 +8,89 @@ import static io.skymind.pathmind.db.dao.SearchDescription.Field.NAME;
 import static io.skymind.pathmind.db.dao.SearchDescription.Field.USERNOTES;
 
 public class SearchParserTest {
+    private void assertParseResultWillBe(String keyword, SearchDescription expected) {
+        SearchParser searchParser = new SearchParser();
+        SearchDescription searchDescription = searchParser.parseKeyword(keyword);
+        Assert.assertEquals(expected, searchDescription);
+    }
+
     @Test
     public void testSimpleProjectSearch() {
-        SearchParser searchParser = new SearchParser();
-        SearchDescription searchDescription = searchParser.parseKeyword("project:some search");
-        SearchDescription expected = new SearchDescription()
+        assertParseResultWillBe("project:some search", new SearchDescription()
                 .withProjectClauses(
                         new OrClause(NAME, "some search"),
                         new OrClause(USERNOTES, "some search")
-                );
-        Assert.assertEquals(expected, searchDescription);
+                ));
     }
 
     @Test
     public void testProjectSearchHasNoMatch() {
-        SearchParser searchParser = new SearchParser();
-        SearchDescription searchDescription = searchParser.parseKeyword("project:project nice");
-        SearchDescription expected = new SearchDescription()
+        assertParseResultWillBe("project:project nice", new SearchDescription()
                 .withProjectClauses(
                         new OrClause(NAME, "project nice"),
                         new OrClause(USERNOTES, "project nice")
-                );
-        Assert.assertEquals(expected, searchDescription);
-
+                ));
     }
 
     @Test
     public void testSimpleModelSearch() {
-        SearchParser searchParser = new SearchParser();
-        SearchDescription searchDescription = searchParser.parseKeyword("model:some search");
-        SearchDescription expected = new SearchDescription()
+        assertParseResultWillBe("model:some search", new SearchDescription()
                 .withModelClauses(
                         new OrClause(NAME, "some search"),
                         new OrClause(USERNOTES, "some search")
-                );
-        Assert.assertEquals(expected, searchDescription);
+                ));
     }
 
     @Test
     public void testModelSearchWithMatch() {
-        SearchParser searchParser = new SearchParser();
-        SearchDescription searchDescription = searchParser.parseKeyword("model:model 1");
-        SearchDescription expected = new SearchDescription()
+        assertParseResultWillBe("model:model 1", new SearchDescription()
                 .withModelClauses(
                         new OrClause(NAME, "1"),
                         new OrClause(USERNOTES, "model 1")
-                );
-        Assert.assertEquals(expected, searchDescription);
+                ));
     }
 
 
     @Test
     public void testModelSearchWithMatchVariant() {
-        SearchParser searchParser = new SearchParser();
-        SearchDescription searchDescription = searchParser.parseKeyword("model:model #1");
-        SearchDescription expected = new SearchDescription()
+        assertParseResultWillBe("model:model #1", new SearchDescription()
                 .withModelClauses(
                         new OrClause(NAME, "1"),
                         new OrClause(USERNOTES, "model #1")
-                );
-        Assert.assertEquals(expected, searchDescription);
+                ));
     }
 
     @Test
     public void testSimpleExperimentSearch() {
-        SearchParser searchParser = new SearchParser();
-        SearchDescription searchDescription = searchParser.parseKeyword("experiment:some search");
-        SearchDescription expected = new SearchDescription()
+        assertParseResultWillBe("experiment:some search", new SearchDescription()
                 .withExperimentClauses(
                         new OrClause(NAME, "some search"),
                         new OrClause(USERNOTES, "some search")
-                );
-        Assert.assertEquals(expected, searchDescription);
+                ));
     }
 
     @Test
     public void testExperimentSearchWithMatch() {
-        SearchParser searchParser = new SearchParser();
-        SearchDescription searchDescription = searchParser.parseKeyword("experiment:experiment 1");
-        SearchDescription expected = new SearchDescription()
+        assertParseResultWillBe("experiment:experiment 1", new SearchDescription()
                 .withExperimentClauses(
                         new OrClause(NAME, "1"),
                         new OrClause(USERNOTES, "experiment 1")
-                );
-        Assert.assertEquals(expected, searchDescription);
+                ));
     }
 
 
     @Test
     public void testExperimentSearchWithMatchVariant() {
-        SearchParser searchParser = new SearchParser();
-        SearchDescription searchDescription = searchParser.parseKeyword("experiment:experiment #1");
-        SearchDescription expected = new SearchDescription()
+        assertParseResultWillBe("experiment:experiment #1", new SearchDescription()
                 .withExperimentClauses(
                         new OrClause(NAME, "1"),
                         new OrClause(USERNOTES, "experiment #1")
-                );
-        Assert.assertEquals(expected, searchDescription);
+                ));
     }
 
     @Test
     public void testSimpleAllSearch() {
-        SearchParser searchParser = new SearchParser();
-        SearchDescription searchDescription = searchParser.parseKeyword("all:some search");
-        SearchDescription expected = new SearchDescription()
+        assertParseResultWillBe("all:some search", new SearchDescription()
                 .withProjectClauses(
                         new OrClause(NAME, "some search"),
                         new OrClause(USERNOTES, "some search")
@@ -123,15 +102,12 @@ public class SearchParserTest {
                 .withExperimentClauses(
                         new OrClause(NAME, "some search"),
                         new OrClause(USERNOTES, "some search")
-                );
-        Assert.assertEquals(expected, searchDescription);
+                ));
     }
 
     @Test
     public void testAllSearchWithExperimentMatch() {
-        SearchParser searchParser = new SearchParser();
-        SearchDescription searchDescription = searchParser.parseKeyword("all:experiment 1");
-        SearchDescription expected = new SearchDescription()
+        assertParseResultWillBe("all:experiment 1", new SearchDescription()
                 .withProjectClauses(
                         new OrClause(NAME, "experiment 1"),
                         new OrClause(USERNOTES, "experiment 1")
@@ -143,15 +119,12 @@ public class SearchParserTest {
                 .withExperimentClauses(
                         new OrClause(NAME, "1"),
                         new OrClause(USERNOTES, "experiment 1")
-                );
-        Assert.assertEquals(expected, searchDescription);
+                ));
     }
 
     @Test
     public void testAllSearchWithModelMatch() {
-        SearchParser searchParser = new SearchParser();
-        SearchDescription searchDescription = searchParser.parseKeyword("all:model 1");
-        SearchDescription expected = new SearchDescription()
+        assertParseResultWillBe("all:model 1", new SearchDescription()
                 .withProjectClauses(
                         new OrClause(NAME, "model 1"),
                         new OrClause(USERNOTES, "model 1")
@@ -163,26 +136,26 @@ public class SearchParserTest {
                 .withExperimentClauses(
                         new OrClause(NAME, "model 1"), // it actually doesn't make sense, but it won't hurt
                         new OrClause(USERNOTES, "model 1")
-                );
-        Assert.assertEquals(expected, searchDescription);
+                ));
+    }
+
+    private void assertParseResultEquivalence(String keyword, String equivalentToKeyword) {
+        SearchParser searchParser = new SearchParser();
+        SearchDescription actual = searchParser.parseKeyword(keyword);
+        SearchDescription expected = searchParser.parseKeyword(equivalentToKeyword);
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
     public void testSearchWithoutAType() {
         // this shouldn't happen in practice, unless someone direct change the URL, but let's prevent anyway.
-        SearchParser searchParser = new SearchParser();
-        SearchDescription searchDescription = searchParser.parseKeyword("some search");
-        SearchDescription expected = searchParser.parseKeyword("all:some search");
-        Assert.assertEquals(expected, searchDescription);
+        assertParseResultEquivalence("some search", "all:some search");
     }
 
     @Test
     public void testSearchWithATypeThatDoesntExist() {
         // this shouldn't happen in practice, unless someone direct change the URL, but let's prevent anyway.
-        SearchParser searchParser = new SearchParser();
-        SearchDescription searchDescription = searchParser.parseKeyword("i dont exist:some search");
-        SearchDescription expected = searchParser.parseKeyword("all:some search");
-        Assert.assertEquals(expected, searchDescription);
+        assertParseResultEquivalence("i dont exist:new search", "all:new search");
     }
 
 }
