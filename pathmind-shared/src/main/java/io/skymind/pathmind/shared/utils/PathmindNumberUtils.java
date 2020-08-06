caeff33b7d17ada14e71520fc78349c07d89ebe7
Fionna <fionnacst@gmail.com>
2020-08-06 18:17:24 +0800

    Calculate and format metrics with uncertainty

--- pathmind-shared/src/main/java/io/skymind/pathmind/shared/utils/PathmindNumberUtils.java
+++ pathmind-shared/src/main/java/io/skymind/pathmind/shared/utils/PathmindNumberUtils.java
@@ -1,12 +1,20 @@
 package io.skymind.pathmind.shared.utils;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class PathmindNumberUtils {
     private PathmindNumberUtils() {
     }
 
+    /**
+     * Strip decimals off numbers >=10 and round them.
+     * For numbers < 10, show 2 sig. fig. at most.
+     * @param originalNumber
+     *            the original number to be formatted
+    */
     public static String formatNumber(Double originalNumber) {
         if (originalNumber >= 10) {
             return String.valueOf((int) Math.rint(originalNumber));
@@ -14,5 +22,27 @@ public class PathmindNumberUtils {
         BigDecimal bd = BigDecimal.valueOf(originalNumber);
         int newScale = 2-bd.precision()+bd.scale();
         return bd.setScale(newScale, RoundingMode.HALF_UP).toString();
+    }
+
+    public static String setSigFigBasedOnAnotherDouble(Double originalNumber, Double refNumber) {
+        BigDecimal bdRefNumber;
+        BigDecimal bdOriginalNumber = BigDecimal.valueOf(originalNumber);
+        DecimalFormat df = new DecimalFormat();
+        String refNumber2SigFig = formatToSigFig(refNumber, 2);
+        if (refNumber2SigFig.contains(".")) {
+            bdRefNumber = BigDecimal.valueOf(Double.parseDouble(refNumber2SigFig));
+        } else {
+            bdRefNumber = BigDecimal.valueOf(Integer.parseInt(refNumber2SigFig));
+        }
+        int sigFig = bdOriginalNumber.precision() - (bdOriginalNumber.scale() - bdRefNumber.scale());
+        int newScale = sigFig-bdOriginalNumber.precision()+bdOriginalNumber.scale();
+        df.setMaximumFractionDigits(bdRefNumber.scale());
+        return df.format(bdOriginalNumber.setScale(newScale, RoundingMode.HALF_EVEN)).toString().replace(",", "");
+    }
+
+    public static String formatToSigFig(Double originalNumber, int sigFig) {
+        MathContext mathContext = new MathContext(sigFig, RoundingMode.HALF_EVEN);
+        BigDecimal formatted = new BigDecimal(originalNumber, mathContext);
+        return formatted.toPlainString();
     }
 }
--- pathmind-shared/src/test/java/io/skymind/pathmind/shared/utils/PathmindNumberUtilsTest.java
+++ pathmind-shared/src/test/java/io/skymind/pathmind/shared/utils/PathmindNumberUtilsTest.java
@@ -18,4 +18,27 @@ public class PathmindNumberUtilsTest {
         Assert.assertEquals("42372", PathmindNumberUtils.formatNumber(42371.95234));
     }
 
+	@Test
+    public void testSetSigFigBasedOnAnotherDouble() {
+        Assert.assertEquals("1.13", PathmindNumberUtils.setSigFigBasedOnAnotherDouble(1.134, 0.22));
+        Assert.assertEquals("145.1", PathmindNumberUtils.setSigFigBasedOnAnotherDouble(145.134, 0.2));
+        Assert.assertEquals("0.56", PathmindNumberUtils.setSigFigBasedOnAnotherDouble(0.55823, 0.23));
+        Assert.assertEquals("123", PathmindNumberUtils.setSigFigBasedOnAnotherDouble((double) 123, (double) 30));
+        Assert.assertEquals("841", PathmindNumberUtils.setSigFigBasedOnAnotherDouble((double) 840.58, (double) 37));
+        Assert.assertEquals("1234", PathmindNumberUtils.setSigFigBasedOnAnotherDouble((double) 1234.5, (double) 6));
+        Assert.assertEquals("1236", PathmindNumberUtils.setSigFigBasedOnAnotherDouble((double) 1235.5, (double) 6));
+    }
+
+	@Test
+    public void testFormatToSigFig() {
+        Assert.assertEquals("145.1", PathmindNumberUtils.formatToSigFig(145.134, 4));
+        Assert.assertEquals("0.56", PathmindNumberUtils.formatToSigFig(0.55823, 2));
+        Assert.assertEquals("0.558", PathmindNumberUtils.formatToSigFig(0.55823, 3));
+        Assert.assertEquals("18", PathmindNumberUtils.formatToSigFig(18.5, 2));
+        Assert.assertEquals("120", PathmindNumberUtils.formatToSigFig(123.25, 2));
+        Assert.assertEquals("1000", PathmindNumberUtils.formatToSigFig(1149.33, 1));
+        Assert.assertEquals("1100", PathmindNumberUtils.formatToSigFig(1149.33, 2));
+        Assert.assertEquals("1150", PathmindNumberUtils.formatToSigFig(1149.33, 3));
+    }
+
 }
