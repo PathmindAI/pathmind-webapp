b33f425a4f88651040503e09698cf7ebde5f7b94
Onur Idrisoglu <onur@vaadin.com>
2020-08-12 13:10:18 +0300

    Updated description in reward variables panel

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/model/components/RewardVariablesPanel.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/model/components/RewardVariablesPanel.java
@@ -1,12 +1,10 @@
 package io.skymind.pathmind.webapp.ui.views.model.components;
 
-import static io.skymind.pathmind.webapp.ui.constants.CssPathmindStyles.BOLD_LABEL;
 import static io.skymind.pathmind.webapp.ui.constants.CssPathmindStyles.NO_TOP_MARGIN_LABEL;
 
 import java.util.List;
 
 import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -54,23 +52,12 @@ public class RewardVariablesPanel extends VerticalLayout
 
 	private void setupForm() {
         rewardVariablesTable = new RewardVariablesTable();
-		formPanel.add(new Paragraph("You have created a function to gather reward variables in your simulation. Let’s give them variable names to make it easier to remember what they reference."));
-		formPanel.add(getRewardVariablesPanel());
+		formPanel.add(new Paragraph("You have created a function to gather reward variables in your simulation. Here is the list of reward variables we extracted from your simulation:"));
 		formPanel.setPadding(false);
 		formPanel.add(rewardVariablesTable);
 	}
 
 	public void setupRewardVariablesTable(int rewardVariablesCount, List<RewardVariable> rewardVariables) {
 	    rewardVariablesTable.setRewardVariables(rewardVariables);
-	}
-
-	private Component getRewardVariablesPanel() {
-        VerticalLayout wrapper = WrapperUtils.wrapWidthFullVertical(
-                LabelFactory.createLabel("Let’s give each variable a name", BOLD_LABEL),
-                LabelFactory.createLabel("This will make it easier to understand when you’re creating reward functions."),
-                LabelFactory.createLabel("If a name isn't provided, the reward variable will be named 'var-X', where 'X' is its index, e.g. 'var-0'.")
-        );
-		GuiUtils.removeMarginsPaddingAndSpacing(wrapper);
-		return wrapper;
 	}
 }
