c4ae675f1fc60ccd84db2f079df94af362708a75
Fionna Chan <fionnacst@gmail.com>
2020-08-13 16:33:57 +0800

    Update dashboard styles (#1966)
    * Update dashboard styles
    
    * Fix width for progress stage
    
    * Center align text in stage
    
    * Update training failed text

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/PathmindTrainingProgress.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/PathmindTrainingProgress.java
@@ -1,5 +1,6 @@
 package io.skymind.pathmind.webapp.ui.components;
 
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -8,31 +9,40 @@ import com.vaadin.flow.component.progressbar.ProgressBar;
 import io.skymind.pathmind.shared.utils.DateAndTimeUtils;
 
 public class PathmindTrainingProgress extends VerticalLayout {
-	private ProgressBar progressBar = new ProgressBar(0, 100);
-	private Span progressValueLabel = LabelFactory.createLabel("");
+    private ProgressBar progressBar = new ProgressBar(0, 100);
+    private Span trainingText = new Span("Training....");
+    private Span progressValueLabel = new Span();
+    private Span etaLabel = new Span();
+    private boolean textMode = false;
 	
 	public PathmindTrainingProgress() {
 		setPadding(false);
 		setSpacing(false);
-		add(progressBar, progressValueLabel);
+		add(progressBar, new Div(trainingText, progressValueLabel, etaLabel));
 		setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
-	}
-	
-	public void setValue(double progress, double estimatedTime) {
-		progressBar.setValue(progress);
-		String formattedEstimatedTime = DateAndTimeUtils.formatETA((long) estimatedTime);
-		progressValueLabel.setText(formatProgressLabel(progress, formattedEstimatedTime));
 	}
 	
 	/**
 	 * Formats in progress training status.<br/>
-	 * Example output: <code>44 % (ETA: 54 min)</code>
+	 * Example output: <code>44% (ETA: 54 min)</code>
 	 */
-	private String formatProgressLabel(double progress, String formattedEstimatedTime) {
-	    if (progress > 0) {
-	        return String.format("%.0f%% (ETA: %s)", progress, formattedEstimatedTime);
-	    } else {
-	        return String.format("%.0f%%", progress);
-	    }
-	}
+	public void setValue(double progress, double estimatedTime) {
+		progressBar.setValue(progress);
+		String formattedEstimatedTime = DateAndTimeUtils.formatETA((long) estimatedTime);
+        progressValueLabel.setText(String.format("%.0f%%", progress));
+        etaLabel.setText(String.format("(ETA: %s)", formattedEstimatedTime));
+        etaLabel.setVisible(progress > 0);
+        setVisibleComponents();
+    }
+    
+    public void setTextMode(Boolean textMode) {
+        getElement().setProperty("textmode", textMode);
+        this.textMode = textMode;
+        setVisibleComponents();
+    }
+
+    public void setVisibleComponents() {
+        progressBar.setVisible(!textMode);
+        trainingText.setVisible(textMode);
+    }
 }
