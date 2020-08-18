74cd9a0c596d3866ff4a64a7b8ac5becf0a80de6
Daniel Jaramillo <xneyder@gmail.com>
2020-08-17 20:10:17 -0500

    Add mockup recording job

--- infra/images/trainer/trigger.py
+++ infra/images/trainer/trigger.py
@@ -142,7 +142,13 @@ def process_message(message):
     if not message:
         return
     global mockup_status
-    hw_type_list=['16cpu_32gb','16cpu_64gb','8cpu_16gb','8cpu_32gb','36cpu_72gb']
+    hw_type_list=['16cpu_32gb', \
+        '16cpu_64gb', \
+        '8cpu_16gb', \
+        '8cpu_32gb', \
+        '36cpu_72gb', \
+        '36cpu_72gb_mockup', \
+        ]
     app_logger.info('Received {message}'.format(message=message['Body']))
     body=json.loads(message['Body'])
     s3bucket=body['S3Bucket']
