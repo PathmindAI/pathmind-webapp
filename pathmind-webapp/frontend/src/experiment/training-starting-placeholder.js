4a3d04996b5ae1dfc428efb4bab6ed26467d8c63
Fionna <fionnacst@gmail.com>
2020-08-27 22:06:17 +0800

    Add spin keyframes to training starting placeholder

--- pathmind-webapp/frontend/src/experiment/training-starting-placeholder.js
+++ pathmind-webapp/frontend/src/experiment/training-starting-placeholder.js
@@ -63,6 +63,23 @@ class TrainingStartingPlaceholder extends PolymerElement {
                     -webkit-animation: spin 2s linear infinite; /* Safari */
                     animation: spin 2s linear infinite;
                 }
+                /* Safari */
+                @-webkit-keyframes spin {
+                    0% {
+                        -webkit-transform: rotate(0deg);
+                    }
+                    100% {
+                        -webkit-transform: rotate(360deg);
+                    }
+                }
+                @keyframes spin {
+                    0% {
+                        transform: rotate(0deg);
+                    }
+                    100% {
+                        transform: rotate(360deg);
+                    }
+                }
             </style>
             <chart-placeholder></chart-placeholder>
             <vaadin-vertical-layout>
