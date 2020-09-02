4a9472de634e2f6b6fb9114ac7157c0cf0895be1
Fionna <fionnacst@gmail.com>
2020-09-02 13:57:53 +0800

    Style goal table on Uploda Model View

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/model/components/GoalsTable.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/model/components/GoalsTable.java
@@ -8,15 +8,19 @@ import com.vaadin.flow.component.AbstractCompositeField;
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.HasValidation;
 import com.vaadin.flow.component.customfield.CustomField;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
 
 import io.skymind.pathmind.shared.data.RewardVariable;
+import io.skymind.pathmind.webapp.ui.components.LabelFactory;
 import io.skymind.pathmind.webapp.ui.utils.GuiUtils;
 
+@CssImport(value = "./styles/components/goals-table.css")
 public class GoalsTable extends CustomField<List<RewardVariable>> implements HasStyle {
 
 	private List<RowField> goalFields = new ArrayList<>();
@@ -35,6 +39,8 @@ public class GoalsTable extends CustomField<List<RewardVariable>> implements Has
     
     public void setItems(List<RewardVariable> goals) {
         goalsList = goals;
+
+        container.add(LabelFactory.createLabel("Goal", "header-row"));
 
         for (int i = 0; i < goals.size(); i++) {
             container.add(createRow());
@@ -64,6 +70,7 @@ public class GoalsTable extends CustomField<List<RewardVariable>> implements Has
         private final Select<String> goalOperatorSelect;
         private RewardVariable currentRewardVariable;
         private boolean readOnly = false;
+        private String goalOperatorSelectThemeNames = "goals small align-center";
 
 		private RowField(boolean readOnly) {
             super(null);
@@ -73,18 +80,36 @@ public class GoalsTable extends CustomField<List<RewardVariable>> implements Has
             this.goalOperatorSelect = new Select<>();
             goalOperatorSelect.setItems("None", "\u2264", "\u2265");
             goalOperatorSelect.setValue("None");
+            goalOperatorSelect.getElement().setAttribute("theme", goalOperatorSelectThemeNames);
+            goalOperatorSelect.addValueChangeListener(event -> {
+                setNumberFieldVisibility();
+            });
             getContent().add(goalOperatorSelect);
             if (readOnly) {
                 getContent().add(goalSpan);
             } else {
-                goalField.addClassName("reward-variable-name-field");
+                goalField.addClassName("goal-field");
+                goalField.addThemeVariants(TextFieldVariant.LUMO_SMALL);
+                goalField.addThemeVariants(TextFieldVariant.LUMO_ALIGN_RIGHT);
                 goalField.addValueChangeListener(e -> {
                 });
                 getContent().add(goalField);
+                setNumberFieldVisibility();
             }
-			getContent().setWidthFull();
+            getContent().setWidthFull();
 			GuiUtils.removeMarginsPaddingAndSpacing(getContent());
-		}
+        }
+        
+        private void setNumberFieldVisibility() {
+            Boolean goalFieldUsable = !goalOperatorSelect.getValue().equals("None");
+            if (goalFieldUsable) {
+                goalOperatorSelect.getElement().setAttribute("theme", goalOperatorSelectThemeNames+" not-none");
+            } else {
+                goalOperatorSelect.getElement().setAttribute("theme", goalOperatorSelectThemeNames);
+            }
+            goalField.setVisible(goalFieldUsable);
+            goalField.setEnabled(goalFieldUsable);
+        }
 
 		@Override
 		public RewardVariable getValue() {
@@ -97,12 +122,14 @@ public class GoalsTable extends CustomField<List<RewardVariable>> implements Has
 
 		@Override
 		protected void setPresentationValue(RewardVariable newPresentationValue) {
-            Double goalValue = 0.0;
+            Double goalValue = newPresentationValue.getGoalValue();
             this.currentRewardVariable = newPresentationValue;
-            if (readOnly) {
-                goalSpan.setText(""+goalValue);
-            } else {
-                goalField.setValue(goalValue);
+            if (goalValue != null) {
+                if (readOnly) {
+                    goalSpan.setText(""+goalValue);
+                } else {
+                    goalField.setValue(goalValue);
+                }
             }
 		}
 
