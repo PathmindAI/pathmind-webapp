c4f0ee1256afbe3dcd2d66895f0da014e66805e2
Fionna <fionnacst@gmail.com>
2020-09-07 18:59:17 +0800

    Add Goals Reached Status polymer component

---
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/atoms/GoalsReachedStatus.java
@@ -0,0 +1,20 @@
+package io.skymind.pathmind.webapp.ui.components.atoms;
+
+import com.vaadin.flow.component.HasStyle;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.templatemodel.TemplateModel;
+
+@Tag("goals-reached-status")
+@JsModule("./src/components/atoms/goals-reached-status.js")
+public class GoalsReachedStatus extends PolymerTemplate<TemplateModel> implements HasStyle {
+    public GoalsReachedStatus(boolean goalsReached) {
+        super();
+        setValue(goalsReached);
+    }
+
+    public void setValue(boolean goalsReached) {
+        getElement().setProperty("goalsReached", goalsReached);
+    }
+}
