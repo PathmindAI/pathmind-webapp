8b592f72db8aeed6b39fb1482a293f681f535bc2
Fionna <fionnacst@gmail.com>
2020-09-10 16:13:17 +0800

    Set up all 3 types of charts on storybook

--- pathmind-webapp/.storybook/stories/chart/chart.stories.js
+++ pathmind-webapp/.storybook/stories/chart/chart.stories.js
@@ -14,7 +14,7 @@ storiesOf('Charts', module)
 .add('Curved Line Chart', () => `
     <h1>Curved Line Chart</h1>
     <div class="block-wrapper">
-        <p>This is based on the Line Chart type from <a href="https://www.webcomponents.org/element/@google-web-components/google-chart/elements/google-chart" target="_blank">Google Chart</a>.</p>
+        <p>This is based on the APIs from <a href="https://www.webcomponents.org/element/@google-web-components/google-chart/elements/google-chart" target="_blank">Google Chart</a>.</p>
     </div>
     <div class="block-wrapper">
         <h3>Area Chart</h3>
@@ -63,5 +63,167 @@ storiesOf('Charts', module)
                 }'
             ></data-chart>
         </div>
+        <h3>Sparkline Chart (Metric with Goall <=22, calibrated)</h3>
+        <div class="col-wrapper">
+            <div style="width: 100px; height: 32px">
+                <data-chart
+                    type='combo'
+                    cols='[
+                        {"label":"Iteration", "type":"number"},
+                        {"label":"Mean Metric Value", "type":"number"},
+                        {"label":"goal base", "type": "number"},
+                        {"label":"goal", "type": "number"}
+                    ]'
+                    rows='[
+                        [1,  134.9, 0, 22],
+                        [2,  125, 0, 22],
+                        [3,  100, 0, 22],
+                        [4,  90, 0, 22],
+                        [5,  65, 0, 22],
+                        [6,  60, 0, 22],
+                        [7,  55, 0, 22],
+                        [8,  54, 0, 22]
+                    ]'
+                    options='{
+                        "tooltip": { "trigger": "none" },
+                        "curveType": "function",
+                        "hAxis": {"textPosition": "none", "ticks": []},
+                        "vAxis": {
+                            "textPosition": "none",
+                            "ticks": [],
+                            "viewWindowMode": "maximized"
+                        },
+                        "legend": {"position": "none"},
+                        "series": {
+                            "0": {
+                                "type": "line",
+                                "enableInteractivity": true
+                            },
+                            "1": {
+                                "lineWidth": 0,
+                                "enableInteractivity": false,
+                                "color": "transparent"
+                            },
+                            "2": {
+                                "lineWidth": 0,
+                                "type": "area",
+                                "enableInteractivity": false,
+                                "color": "green"
+                            }
+                        }
+                    }'
+                ></data-chart>
+            </div>
+        </div>
+        <h3>Metric Chart (Metric with Goal <=22, calibrated)</h3>
+        <div class="col-wrapper">
+            <data-chart
+                type='combo'
+                cols='[
+                    {"label":"Iteration", "type":"number"},
+                    {"label":"Mean Metric Value", "type":"number"}, {"role": "tooltip", "type":"string", "p": {"html": true}},
+                    {"label":"goal base", "type": "number"},
+                    {"label":"goal", "type": "number"}
+                ]'
+                rows='[
+                    [1,  134.9, "<div><b>Iteration #</b>1<br><b>Mean Reward</b> 134.9<br><b>Episode Count</b> 20</div>", 0, 22],
+                    [2,  125, "nah", 0, 22],
+                    [3,  100, "nah", 0, 22],
+                    [4,  90, "nah", 0, 22],
+                    [5,  65, "nah", 0, 22],
+                    [6,  60, "nah", 0, 22],
+                    [7,  55, "nah", 0, 22],
+                    [8,  54, "nah", 0, 22]
+                ]'
+                options='{
+                    "seriesType": "area",
+                    "tooltip": { "isHtml": true },
+                    "isStacked": true,
+                    "hAxis": {"title": "Iteration", "titleTextStyle": {"italic": false}},
+                    "vAxis": {
+                        "title": "Mean Metric Value", 
+                        "titleTextStyle": {"italic": false},
+                        "viewWindow": {
+                            "min": 0
+                        }
+                    },
+                    "legend": {"position": "none"},
+                    "series": {
+                        "0": {
+                            "type": "line",
+                            "enableInteractivity": true
+                        },
+                        "1": {
+                            "lineWidth": 0,
+                            "enableInteractivity": false,
+                            "color": "transparent"
+                        },
+                        "2": {
+                            "lineWidth": 0,
+                            "type": "area",
+                            "enableInteractivity": false,
+                            "color": "green"
+                        }
+                    }
+                }'
+            ></data-chart>
+        </div>
+        <h3>Metric Chart (Metric with Goal >=5200, calibrated)</h3>
+        <div class="col-wrapper">
+            <data-chart
+                type='combo'
+                cols='[
+                    {"label":"Iteration", "type":"number"},
+                    {"label":"Mean Metric Value", "type":"number"}, {"role": "tooltip", "type":"string", "p": {"html": true}},
+                    {"label":"goal base", "type": "number"},
+                    {"label":"goal", "type": "number"}
+                ]'
+                rows='[
+                    [1,  5340.9, "<div><b>Iteration #</b>1<br><b>Mean Reward</b> 134.9<br><b>Episode Count</b> 20</div>", 5200, 300],
+                    [2,  5340, "nah", 5200, 300],
+                    [3,  5210, "nah", 5200, 300],
+                    [4,  5150, "nah", 5200, 300],
+                    [5,  5325, "nah", 5200, 300],
+                    [6,  5123, "nah", 5200, 300],
+                    [7,  5500, "nah", 5200, 300],
+                    [8,  5400, "nah", 5200, 300]
+                ]'
+                options='{
+                    "seriesType": "area",
+                    "tooltip": { "isHtml": true },
+                    "isStacked": true,
+                    "hAxis": {
+                        "title": "Iteration", 
+                        "titleTextStyle": {"italic": false}
+                    },
+                    "vAxis": {
+                        "title": "Mean Metric Value", 
+                        "titleTextStyle": {"italic": false},
+                        "viewWindow": {
+                            "min": 5120
+                        }
+                    },
+                    "legend": {"position": "none"},
+                    "series": {
+                        "0": {
+                            "type": "line",
+                            "enableInteractivity": true
+                        },
+                        "1": {
+                            "lineWidth": 0,
+                            "enableInteractivity": false,
+                            "color": "transparent"
+                        },
+                        "2": {
+                            "lineWidth": 0,
+                            "enableInteractivity": false,
+                            "color": "green"
+                        }
+                    }
+                }'
+            ></data-chart>
+        </div>
     </div>
 `, storyParamConfig);
+
+// "viewWindowMode": "maximized"
