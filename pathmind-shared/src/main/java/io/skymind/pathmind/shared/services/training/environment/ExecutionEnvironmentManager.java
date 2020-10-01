0c51b061c32411f12a2e9771f9c2d27e745ac548
DH <kepricon@gmail.com>
2020-10-01 11:25:30 -0700

    change the default library version (#2188)

--- pathmind-shared/src/main/java/io/skymind/pathmind/shared/services/training/environment/ExecutionEnvironmentManager.java
+++ pathmind-shared/src/main/java/io/skymind/pathmind/shared/services/training/environment/ExecutionEnvironmentManager.java
@@ -14,11 +14,11 @@ public class ExecutionEnvironmentManager {
     private static Map<Long, ExecutionEnvironment> environmentMap= new HashMap();
 
     private ExecutionEnvironment defaultEnvironment() {
-        return new ExecutionEnvironment(AnyLogic.VERSION_8_6_0,
-                PathmindHelper.VERSION_1_2_0,
-                NativeRL.VERSION_1_2_0,
+        return new ExecutionEnvironment(AnyLogic.VERSION_8_6_1,
+                PathmindHelper.VERSION_1_3_0,
+                NativeRL.VERSION_1_3_0,
                 JDK.VERSION_8_222,
-                Conda.VERSION_0_8_6,
+                Conda.VERSION_0_8_7,
                 EC2InstanceType.IT_36CPU_72GB);
     }
 
