5579d3f8e4a597b8968b3520321f1f126dc5f8f6
FollowSteph <steph@followsteph.com>
2020-10-10 00:55:58 -0400

    #2243 - Also fixed the same issue for the simulation metrics panel.

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/experiment/simulationMetrics/subscribers/SimulationMetricsPolicyUpdateSubscriber.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/experiment/simulationMetrics/subscribers/SimulationMetricsPolicyUpdateSubscriber.java
@@ -22,12 +22,7 @@ public class SimulationMetricsPolicyUpdateSubscriber extends PolicyUpdateSubscri
 
     @Override
     public void handleBusEvent(PolicyUpdateBusEvent event) {
-        PushUtils.push(getUiSupplier(), ui -> {
-            // Only for the best policy.
-            Policy policy = PolicyUtils.selectBestPolicy(event.getPolicies());
-            if (simulationMetricsPanel.isShowSimulationMetrics() && policy!= null && policy.getMetrics() != null && policy.getMetrics().size() > 0)
-                simulationMetricsPanel.updateSimulationMetrics(policy, true); // TODO -> it should only be true for the first time, but this is a hotfix
-        });
+        PushUtils.push(getUiSupplier(), ui -> simulationMetricsPanel.updatePolicies(event.getPolicies()));
     }
 
     @Override
