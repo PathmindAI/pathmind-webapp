c2c3e08e16e97ec7d4b6246eda3c746efdd7298f
Fionna <fionnacst@gmail.com>
2020-10-15 12:29:38 +0800

    Add content left panel and add google optimize to signup view

--- pathmind-webapp/frontend/src/pages/account/sign-up-view.js
+++ pathmind-webapp/frontend/src/pages/account/sign-up-view.js
@@ -3,7 +3,21 @@ import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
 class SignUpView extends PolymerElement {
   static get template() {
     return html`
-    <style include="shared-styles pathmind-dialog-view sign-up-view-styles"></style>
+    <style include="shared-styles pathmind-dialog-view sign-up-view-styles">
+        .inner-content {
+            margin-left: var(--lumo-space-m);
+            margin-right: var(--lumo-space-m);
+        }
+        .info {
+            align-items: flex-start;
+            flex: 1 0 auto;
+            max-width: none;
+            width: auto;
+        }
+        .info li {
+            padding-bottom: var(--lumo-space-s);
+        }
+    </style>
     <vaadin-horizontal-layout class="panel-wrapper">
       <div class="content">
         <span class="welcome-text">Welcome to</span>
@@ -12,62 +26,72 @@ class SignUpView extends PolymerElement {
           src="frontend/images/pathmind-logo.svg"
           alt="Pathmind logo"
         />
-        <vaadin-vertical-layout class="inner-content" id="emailPart">
-          <h3>{{title}}</h3>
-          <vaadin-text-field
-            id="firstName"
-            label="First Name"
-          ></vaadin-text-field>
-          <vaadin-text-field
-            id="lastName"
-            label="Last Name"
-          ></vaadin-text-field>
-          <vaadin-text-field id="email" label="Work Email"></vaadin-text-field>
-          <p class="notes" hidden$="{{isEmailUsed}}">
-            The email will be used as the User Email during sign in
-          </p>
-          <vaadin-button
-            id="forgotPasswordBtn"
-            theme="tertiary small"
-            hidden$="{{!isEmailUsed}}"
-            onclick="window.location.href='/reset-password'"
-            >Want to reset password?</vaadin-button>
-          <vaadin-vertical-layout id="buttonsCont">
-            <vaadin-button id="signUp" theme="primary">
-              Sign up
-            </vaadin-button>
-          </vaadin-vertical-layout>
-          <p class="notes">No credit card required</p>
-          <vaadin-button
-            theme="tertiary small"
-            onclick="window.location.href='/sign-in'"
-          >Already have an account?
-          </vaadin-button>
-        </vaadin-vertical-layout>
-        <vaadin-vertical-layout class="inner-content" id="passwordPart">
-          <h3>{{title}}</h3>
-          <vaadin-password-field
-            id="newPassword"
-            label="Password"
-          ></vaadin-password-field>
-          <vaadin-vertical-layout
-            id="newPassNotes"
-            class="notes"
-            style="width: 100%;"
-          ></vaadin-vertical-layout>
-          <vaadin-password-field
-            id="confirmNewPassword"
-            label="Confirm Password"
-          ></vaadin-password-field>
-          <vaadin-vertical-layout id="buttonsCont">
-            <vaadin-button id="signIn" theme="primary">
-              Get Started!
-            </vaadin-button>
-            <vaadin-button id="cancelSignInBtn" theme="tertiary">
-              Back
-            </vaadin-button>
-          </vaadin-vertical-layout>
-        </vaadin-vertical-layout>
+        <vaadin-horizontal-layout>
+            <vaadin-vertical-layout class="inner-content info">
+                <b>Create your free Pathmind account to:</b>
+                <ul>
+                    <li>Access additional Pathmind-ready simulation models</li>
+                    <li>Get tips on how to guide and reward your AI agents</li>
+                    <li>Apply AI to your simulation and beat your heuristic</li>
+                </ul>
+            </vaadin-vertical-layout>
+            <vaadin-vertical-layout class="inner-content" id="emailPart">
+                <h3>{{title}}</h3>
+                <vaadin-text-field
+                id="firstName"
+                label="First Name"
+                ></vaadin-text-field>
+                <vaadin-text-field
+                id="lastName"
+                label="Last Name"
+                ></vaadin-text-field>
+                <vaadin-text-field id="email" label="Work Email"></vaadin-text-field>
+                <p class="notes" hidden$="{{isEmailUsed}}">
+                The email will be used as the User Email during sign in
+                </p>
+                <vaadin-button
+                id="forgotPasswordBtn"
+                theme="tertiary small"
+                hidden$="{{!isEmailUsed}}"
+                onclick="window.location.href='/reset-password'"
+                >Want to reset password?</vaadin-button>
+                <vaadin-vertical-layout id="buttonsCont">
+                <vaadin-button id="signUp" theme="primary">
+                    Sign up
+                </vaadin-button>
+                </vaadin-vertical-layout>
+                <p class="notes">No credit card required</p>
+                <vaadin-button
+                theme="tertiary small"
+                onclick="window.location.href='/sign-in'"
+                >Already have an account?
+                </vaadin-button>
+            </vaadin-vertical-layout>
+            <vaadin-vertical-layout class="inner-content" id="passwordPart">
+            <h3>{{title}}</h3>
+            <vaadin-password-field
+                id="newPassword"
+                label="Password"
+            ></vaadin-password-field>
+            <vaadin-vertical-layout
+                id="newPassNotes"
+                class="notes"
+                style="width: 100%;"
+            ></vaadin-vertical-layout>
+            <vaadin-password-field
+                id="confirmNewPassword"
+                label="Confirm Password"
+            ></vaadin-password-field>
+            <vaadin-vertical-layout id="buttonsCont">
+                <vaadin-button id="signIn" theme="primary">
+                Get Started!
+                </vaadin-button>
+                <vaadin-button id="cancelSignInBtn" theme="tertiary">
+                Back
+                </vaadin-button>
+            </vaadin-vertical-layout>
+            </vaadin-vertical-layout>
+        </vaadin-horizontal-layout>
         <div class="policy" id="policyText">
           <span>By clicking Get Started, you agree to Pathmind's </span>
           <a href="https://pathmind.com/subscription-agreement" target="_blank"
@@ -80,6 +104,20 @@ class SignUpView extends PolymerElement {
         <a class="support" href="{{contactLink}}">Contact Support</a>
       </div>
     </vaadin-horizontal-layout>`;
+  }
+
+  ready() {
+      super.ready();
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = "https://www.googleoptimize.com/optimize.js?id=GTM-T2DSBKT";
+      document.head.appendChild(script);
+      // The documentation asked to put it at the top of the head tags
+      // However we use Java and Vaadin to run it so by the time this part is run,
+      // the head tags would have been parsed and executed
+      // so there's no point in appending it to the top.
+      // We'll need to look into the documentation and see how it impacts Google Optimize's usage
+      // https://support.google.com/optimize/answer/10106536?hl=en&ref_topic=6197443
   }
 
   static get is() {
