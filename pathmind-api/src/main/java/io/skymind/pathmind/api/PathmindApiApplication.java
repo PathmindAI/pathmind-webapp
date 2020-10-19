027b18ac8cc5813c9ce00bfc7830805e3b0f0c39
Alexander <alexander.makarov@toptal.com>
2020-10-19 12:11:22 +0200

    add Pathmind API maven submodule

---
+++ pathmind-api/src/main/java/io/skymind/pathmind/api/PathmindApiApplication.java
@@ -0,0 +1,13 @@
+package io.skymind.pathmind.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathmindApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathmindApiApplication.class, args);
+	}
+
+}
