1913667ca15f491ddc76b67730d398c2b98128ec
Alexander <alexander.makarov@toptal.com>
2020-10-19 12:46:32 +0200

    integrate api with services

--- pathmind-api/src/main/java/io/skymind/pathmind/api/PathmindApiApplication.java
+++ pathmind-api/src/main/java/io/skymind/pathmind/api/PathmindApiApplication.java
@@ -2,8 +2,10 @@ package io.skymind.pathmind.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "io.skymind.pathmind")
+@PropertySource({"application.properties", "shared.properties"})
 public class PathmindApiApplication {
 
 	public static void main(String[] args) {
