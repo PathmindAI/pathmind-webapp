56be929c47fa8498db39874323f298d3358359d6
Fionna Chan <fionnacst@gmail.com>
2020-10-21 12:21:24 +0800

    Match sparkline colors to simulation metric label (#2303)
    * Match sparkline colors to simulation metric label
    
    * Move colors to ChartUtils

---
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/experiment/components/chart/ChartUtils.java
@@ -0,0 +1,26 @@
+package io.skymind.pathmind.webapp.ui.views.experiment.components.chart;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ChartUtils {
+
+    private ChartUtils() {
+    }
+
+    public static List<String> colors() {
+        return Arrays.asList(
+            "#17a747",
+            "#214e96",
+            "#318c81",
+            "#7550e5",
+            "#62a540",
+            "#e0667d",
+            "#5f8fd6",
+            "#931901",
+            "#bd52a3",
+            "#887100"
+        );
+    }
+
+}
