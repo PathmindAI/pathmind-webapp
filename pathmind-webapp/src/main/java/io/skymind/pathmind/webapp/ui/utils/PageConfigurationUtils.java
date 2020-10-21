bb7bb4edea1b6189a6b3e6cdcc55818eb5c5082b
Fionna <fionnacst@gmail.com>
2020-10-21 17:26:45 +0800

    Move google optimize to public pages only

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/utils/PageConfigurationUtils.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/utils/PageConfigurationUtils.java
@@ -6,14 +6,6 @@ public class PageConfigurationUtils {
     
     public static void defaultPageConfiguration(InitialPageSettings settings) {
         VaadinUtils.setupFavIcon(settings);
-        settings.addInlineWithContents(InitialPageSettings.Position.PREPEND,
-                "<script src=\"https://www.googleoptimize.com/optimize.js?id=GTM-T2DSBKT\"></script>", InitialPageSettings.WrapMode.NONE);
-        settings.addInlineWithContents(InitialPageSettings.Position.PREPEND,
-                "<style>.async-hide { opacity: 0 !important} </style><script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;"+
-                "h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};"+
-                "(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;"+
-                "})(window,document.documentElement,'async-hide','dataLayer',4000,"+
-                "{'CONTAINER_ID':true});</script>", InitialPageSettings.WrapMode.NONE);
 	}
 
 }
