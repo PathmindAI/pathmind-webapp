7de6e6dc60390ebd0fda39a6e593b737fef94297
FollowSteph <steph@followsteph.com>
2020-10-22 00:21:13 -0400

    #2085 - Merging dev

--- pathmind-database/src/main/java/io/skymind/pathmind/db/jooq/tables/Experiment.java
+++ pathmind-database/src/main/java/io/skymind/pathmind/db/jooq/tables/Experiment.java
@@ -43,7 +43,6 @@ import org.jooq.impl.TableImpl;
 public class Experiment extends TableImpl<ExperimentRecord> {
 
     private static final long serialVersionUID = 710565154;
-    private static final long serialVersionUID = -1834605390;
 
     /**
      * The reference instance of <code>public.experiment</code>
