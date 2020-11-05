450cfa56d4b0961ded77ec44321cf845711001e2
Fionna <fionnacst@gmail.com>
2020-11-05 14:05:42 +0800

    Redesign Sign Up View

--- pathmind-webapp/frontend/src/pages/account/sign-up-view.js
+++ pathmind-webapp/frontend/src/pages/account/sign-up-view.js
@@ -1,4 +1,5 @@
 import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import "../../components/organisms/public-header-menu.js";
 
 class SignUpView extends PolymerElement {
   static get template() {
@@ -12,10 +13,15 @@ class SignUpView extends PolymerElement {
             justify-content: center;
             width: 100%;
         }
+        .panel-wrapper {
+          padding: var(--lumo-space-xl);
+        }
         .info {
             align-items: flex-start;
             flex: 1 0 60%;
-            max-width: 400px;
+            max-width: 600px;
+            font-size: var(--lumo-font-size-l);
+            padding-top: var(--lumo-space-xxl);
         }
         .video-wrapper {
             position: relative;
@@ -30,8 +36,37 @@ class SignUpView extends PolymerElement {
             top: 0;
             left: 0;
         }
+        .info h1 {
+          color: var(--lumo-primary-color);
+          line-height: 1.5;
+          margin: 0;
+        }
+        .info p {
+          margin: var(--lumo-space-xl) 0 var(--lumo-space-m);
+        }
+        .info ul {
+          padding: 0 0 0 var(--lumo-font-size-xxl);
+          margin-top: 0;
+        }
         .info li {
-            padding-bottom: var(--lumo-space-s);
+          list-style: none;
+          position: relative;
+          padding-left: var(--lumo-font-size-xl);
+          margin-bottom: var(--lumo-space-s);
+        }
+        .info li::before {
+          content: '';
+          display: block;
+          position: absolute;
+          top: 50%;
+          left: 0;
+          width: var(--lumo-space-s);
+          height: var(--lumo-space-s);
+          background-color: var(--lumo-primary-color);
+          transform: translate(0, -50%);
+        }
+        .info li::marker {
+          display: none;
         }
         @media screen and (max-width: 768px) {
             .content-wrapper {
@@ -45,95 +80,82 @@ class SignUpView extends PolymerElement {
           display: none;
         }
     </style>
+    <public-header-menu contactlink="{{contactLink}}"></public-header-menu>
     <vaadin-horizontal-layout class="panel-wrapper">
       <div class="content">
-        <span class="welcome-text">Welcome to</span>
-        <a href="https://pathmind.com/"><img
-          class="logo"
-          src="frontend/images/pathmind-logo.svg"
-          alt="Pathmind logo"
-        /></a>
         <vaadin-horizontal-layout class="content-wrapper">
-            <vaadin-vertical-layout class="inner-content info">
-                <!--<div class="video-wrapper">
-                    <iframe src="//fast.wistia.net/embed/iframe/py4nssath2" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen></iframe>
-                </div>-->
-                <b>Create your free Pathmind account to:</b>
+            <vaadin-vertical-layout class="info">
+                <h1>
+                  Optimize your simulation with AI.<br/>
+                  Quick, simple, and effective.
+                </h1>
+                <p>Get your free Pathmind account to:</p>
                 <ul>
                     <li>Access additional AI-ready simulation models</li>
                     <li>Get tips on how to guide and reward your AI agents</li>
                     <li>Apply AI to your simulation and beat your heuristic</li>
                 </ul>
+                <!--<div class="video-wrapper">
+                    <iframe src="//fast.wistia.net/embed/iframe/py4nssath2" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+                </div>-->
             </vaadin-vertical-layout>
             <vaadin-vertical-layout class="inner-content" id="emailPart">
-                <h3>{{title}}</h3>
+                <h3>Make Better Decisions With AI</h3>
                 <vaadin-text-field
-                id="firstName"
-                label="First Name"
-                value=""
-                ></vaadin-text-field>
+                  id="firstName"
+                  label="First Name"
+                  value=""
+                  ></vaadin-text-field>
                 <vaadin-text-field
-                id="lastName"
-                label="Last Name"
-                value=""
-                ></vaadin-text-field>
+                  id="lastName"
+                  label="Last Name"
+                  value=""
+                  ></vaadin-text-field>
                 <vaadin-text-field id="email" label="Work Email"></vaadin-text-field>
                 <p class="notes" hidden$="{{isEmailUsed}}">
                 The email will be used as the User Email during sign in
                 </p>
+                <vaadin-password-field
+                    id="newPassword"
+                    label="Create Password"
+                ></vaadin-password-field>
+                <vaadin-vertical-layout
+                    id="newPassNotes"
+                    class="notes"
+                    style="width: 100%;"
+                ></vaadin-vertical-layout>
+                <vaadin-password-field
+                    id="confirmNewPassword"
+                    label="Confirm Password"
+                ></vaadin-password-field>
                 <vaadin-button
-                id="forgotPasswordBtn"
-                theme="tertiary small"
-                hidden$="{{!isEmailUsed}}"
-                onclick="window.location.href='/reset-password'"
+                  id="forgotPasswordBtn"
+                  theme="tertiary small"
+                  hidden$="{{!isEmailUsed}}"
+                  onclick="window.location.href='/reset-password'"
                 >Want to reset password?</vaadin-button>
                 <vaadin-vertical-layout id="buttonsCont">
-                <vaadin-button id="signUp" theme="primary">
-                    Sign up
-                </vaadin-button>
+                  <vaadin-button id="signIn" theme="primary">
+                    Create Free Account
+                  </vaadin-button>
                 </vaadin-vertical-layout>
                 <vaadin-button
-                id="alreadyHaveAccount"
-                theme="tertiary small"
-                onclick="window.location.href='/sign-in'"
-                >Already have an account?
+                  id="alreadyHaveAccount"
+                  theme="tertiary small"
+                  onclick="window.location.href='/sign-in'"
+                  >Already have an account?
                 </vaadin-button>
-            </vaadin-vertical-layout>
-            <vaadin-vertical-layout class="inner-content" id="passwordPart">
-            <h3>{{title}}</h3>
-            <vaadin-password-field
-                id="newPassword"
-                label="Password"
-            ></vaadin-password-field>
-            <vaadin-vertical-layout
-                id="newPassNotes"
-                class="notes"
-                style="width: 100%;"
-            ></vaadin-vertical-layout>
-            <vaadin-password-field
-                id="confirmNewPassword"
-                label="Confirm Password"
-            ></vaadin-password-field>
-            <vaadin-vertical-layout id="buttonsCont">
-                <vaadin-button id="signIn" theme="primary">
-                Get Started!
-                </vaadin-button>
-                <vaadin-button id="cancelSignInBtn" theme="tertiary">
-                Back
-                </vaadin-button>
-            </vaadin-vertical-layout>
+                <div class="policy" id="policyText">
+                  <span>By submitting the form, you agree to Pathmind's </span>
+                  <a href="https://pathmind.com/subscription-agreement" target="_blank"
+                    >Terms of Use</a
+                  ><span> and </span>
+                  <a href="https://pathmind.com/privacy" target="_blank"
+                    >Privacy Policy</a
+                  ><span>.</span>
+                </div>
             </vaadin-vertical-layout>
         </vaadin-horizontal-layout>
-        <div class="policy" id="policyText">
-          <span>By clicking Get Started, you agree to Pathmind's </span>
-          <a href="https://pathmind.com/subscription-agreement" target="_blank"
-            >Terms of Use</a
-          ><span> and </span>
-          <a href="https://pathmind.com/privacy" target="_blank"
-            >Privacy Policy</a
-          ><span>.</span>
-        </div>
-        <a class="support" href="{{contactLink}}">Contact Support</a>
       </div>
     </vaadin-horizontal-layout>`;
   }
