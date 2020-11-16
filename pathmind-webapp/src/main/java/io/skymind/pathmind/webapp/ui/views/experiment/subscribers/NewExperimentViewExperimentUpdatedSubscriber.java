bd892249b42dfe4dff1af1cbfb59dcf02c9b9664
Fionna Chan <fionnacst@gmail.com>
2020-11-16 12:49:13 +0800

    isSameExperiment method usage cleanup (#2416)
    * Replace private isSameModel usage with ExperimentUtils method
    
    * Remove unused import
    
    * Restyle else clause to else if clause

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/experiment/subscribers/NewExperimentViewExperimentUpdatedSubscriber.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/experiment/subscribers/NewExperimentViewExperimentUpdatedSubscriber.java
@@ -1,6 +1,5 @@
 package io.skymind.pathmind.webapp.ui.views.experiment.subscribers;
 
-import io.skymind.pathmind.shared.data.Experiment;
 import io.skymind.pathmind.webapp.bus.events.main.ExperimentUpdatedBusEvent;
 import io.skymind.pathmind.webapp.bus.subscribers.main.ExperimentUpdatedSubscriber;
 import io.skymind.pathmind.webapp.data.utils.ExperimentUtils;
@@ -17,15 +16,13 @@ public class NewExperimentViewExperimentUpdatedSubscriber extends ExperimentUpda
 
     @Override
     public void handleBusEvent(ExperimentUpdatedBusEvent event) {
-        if (isSameExperiment(event.getExperiment()) && event.isStartedTrainingEventType()) {
-            getUiSupplier().get().get().navigate(ExperimentView.class, event.getExperiment().getId());
-        } else if (!isSameExperiment(event.getExperiment()) && ExperimentUtils.isSameModel(newExperimentView.getExperiment(), event.getModelId())) {
+        if (ExperimentUtils.isSameExperiment(event.getExperiment(), newExperimentView.getExperiment())) {
+            if (event.isStartedTrainingEventType()) {
+                getUiSupplier().get().get().navigate(ExperimentView.class, event.getExperiment().getId());
+            }
+        } else if (ExperimentUtils.isSameModel(newExperimentView.getExperiment(), event.getModelId())) {
             newExperimentView.updateExperimentComponents();
         }
-    }
-
-    private boolean isSameExperiment(Experiment eventExperiment) {
-        return ExperimentUtils.isSameModel(newExperimentView.getExperiment(), eventExperiment.getModelId()) && newExperimentView.getExperiment().equals(eventExperiment);
     }
 
 }
