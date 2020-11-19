3b8f217b80f96734b419f237d2ec2604123e806a
Fionna <fionnacst@gmail.com>
2020-11-19 12:01:45 +0800

    Move elements on new project view to light DOM

--- pathmind-webapp/frontend/src/pages/new-project-view-content.js
+++ pathmind-webapp/frontend/src/pages/new-project-view-content.js
@@ -3,8 +3,9 @@ import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
 class NewProjectView extends PolymerElement {
   static get template() {
     return html`
-    <style include="shared-styles pathmind-dialog-view">
-        p, vaadin-text-field {
+    <style>
+        new-project-view p,
+        new-project-view vaadin-text-field {
             text-align: left;
         }
     </style>
@@ -32,6 +33,10 @@ class NewProjectView extends PolymerElement {
         </vaadin-vertical-layout>
       </div>
     </vaadin-horizontal-layout>`;
+  }
+
+  _attachDom(dom) {
+    this.appendChild(dom);
   }
 
   ready() {
