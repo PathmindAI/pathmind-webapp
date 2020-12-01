d797baee9c1a6a5c2871236e22be19a6ccbb8ef8
Alexander <alexander.makarov@toptal.com>
2020-12-01 13:20:55 +0100

    better validation error

--- pathmind-api/src/main/java/io/skymind/pathmind/api/domain/user/SingupController.java
+++ pathmind-api/src/main/java/io/skymind/pathmind/api/domain/user/SingupController.java
@@ -1,9 +1,16 @@
 package io.skymind.pathmind.api.domain.user;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import io.skymind.pathmind.api.domain.user.dto.SignUpRequest;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +28,20 @@ public class SingupController {
     @PostMapping(path = {"", "/"}, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public String signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
         return null;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, ?> processInputError(MethodArgumentNotValidException exception) {
+        Map<String, String> fieldErrors = new HashMap<>();
+        for(FieldError fe : exception.getBindingResult().getFieldErrors()) {
+            fieldErrors.put(fe.getField(), fe.getDefaultMessage());
+        }
+        return Map.of("timestamp", LocalDateTime.now(),
+                "status", HttpStatus.BAD_REQUEST.value(),
+                "error", HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "message", "Validation errors on form. Total " + exception.getBindingResult().getFieldErrorCount(),
+                "path", SIGNUP_URL,
+                "errors", fieldErrors);
     }
 
 }
