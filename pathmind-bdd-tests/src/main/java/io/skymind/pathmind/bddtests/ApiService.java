060617395b585ca328b988b7d0e47efdf1ce5466
EvgeniyEA <evegeniy@skymind.io>
2020-12-01 14:20:27 +0200

    Add debug sout to show api endpoint

--- pathmind-bdd-tests/src/main/java/io/skymind/pathmind/bddtests/ApiService.java
+++ pathmind-bdd-tests/src/main/java/io/skymind/pathmind/bddtests/ApiService.java
@@ -45,6 +45,7 @@ public class ApiService extends PageObject {
     }
 
     public JsonObject getProjectByProjectName(String projectName) {
+        System.out.println("API Endpoint: " + PATHMIND_API_URL);
         JsonArray jsonArray = SerenityRest.
             given().
             header("X-PM-API-TOKEN", Serenity.sessionVariableCalled("apiKey")).
