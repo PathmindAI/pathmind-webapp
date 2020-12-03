c8b09561c035df059b89ed6dffe5dd0dbda5c813
Fionna <fionnacst@gmail.com>
2020-12-03 10:59:38 +0800

    Remove antiflicker script because we load Google Optimize synchronously

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/utils/PageConfigurationUtils.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/utils/PageConfigurationUtils.java
@@ -8,12 +8,6 @@ public class PageConfigurationUtils {
         VaadinUtils.setupFavIcon(settings);
         settings.addInlineWithContents(InitialPageSettings.Position.PREPEND,
                 "<script src=\"https://www.googleoptimize.com/optimize.js?id=GTM-T2DSBKT\"></script>", InitialPageSettings.WrapMode.NONE);
-        settings.addInlineWithContents(InitialPageSettings.Position.PREPEND,
-                "<style>.async-hide { opacity: 0 !important} </style><script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;" +
-                        "h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};" +
-                        "(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;" +
-                        "})(window,document.documentElement,'async-hide','dataLayer',4000," +
-                        "{'GTM-T2DSBKT':true});</script>", InitialPageSettings.WrapMode.NONE);
     }
 
 }
