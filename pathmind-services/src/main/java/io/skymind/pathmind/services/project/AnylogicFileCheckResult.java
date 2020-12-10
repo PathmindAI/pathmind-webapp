b8664764b2da5e55bb545a9cdbd0683c0a0ed40c
DH <kepricon@gmail.com>
2020-12-10 14:42:36 -0800

    dev-2501- support user-defined class names for Simulation, RLExperiment, Main, PathmindHelper (#2531)
    * add debug message
    
    * pass more information to MA
    
    * extract user-defined names for Simulation, RLExperiment, Main, PathmindHelper
    
    * add DB columns
    
    * pass the names for training
    
    * change variable names
    
    * avoid NPE
    
    * regenerate jooq
    
    * show error messages for invalid RL platform and invalid PM helpers
    
    * change the size of model info
    
    * add unit tests
    
    * update error message
    
    * use StringUtils
    
    * add bdd tests for the changes
    
    * Fix model error
    
    Co-authored-by: EvgeniyEA <evegeniy@skymind.io>

--- pathmind-services/src/main/java/io/skymind/pathmind/services/project/AnylogicFileCheckResult.java
+++ pathmind-services/src/main/java/io/skymind/pathmind/services/project/AnylogicFileCheckResult.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Setter
@@ -24,13 +25,15 @@ public class AnylogicFileCheckResult implements FileCheckResult {
     private List<String> observationTypes;
     private String modelType;
     private int numberOfAgents;
+    private List<AnyLogicModelInfo> modelInfos = new ArrayList<>();
+    private String rlPlatform;
 
     @Override
     public boolean isFileCheckSuccessful() {
-        boolean isAllSuccessful = isCorrectFileType() && isModelJarFilePresent() && isHelperPresent() && isHelperUnique();
+        boolean isAllSuccessful = isCorrectFileType() && isModelJarFilePresent() && isHelperPresent() && isHelperUnique() && getPriorityModelInfo() != null && isValidRLPlatform();
         if (!isAllSuccessful) {
-            log.info("Correct File Type: {}, Model Jar Present: {}, Helper Present: {}, Helper Unique: {}",
-                isCorrectFileType(), isModelJarFilePresent(), isHelperPresent(), isHelperUnique());
+            log.info("Correct File Type: {}, Model Jar Present: {}, Helper Present: {}, Helper Unique: {}, Helper: {}, Models: {}, RL Platform: {}",
+                isCorrectFileType(), isModelJarFilePresent(), isHelperPresent(), isHelperUnique(), definedHelpers, modelInfos, rlPlatform);
         }
         return isAllSuccessful;
     }
@@ -52,5 +55,45 @@ public class AnylogicFileCheckResult implements FileCheckResult {
             log.info("Helper classes is not unique : {}", this.definedHelpers);
             return false;
         }
+    }
+
+    @Override
+    public boolean isValidRLPlatform() {
+        if (rlPlatform == null) {
+            // if there's no information of `ReinforcementLearningPlatform`
+            // it's Simulation model
+            return true;
+        } else if (rlPlatform.endsWith("PathmindLearningPlatform")) {
+            // if `ReinforcementLearningPlatform` set to PM platform
+            // it's valid RLExperiment model
+            return true;
+        } else {
+            // unless '`ReinforcementLearningPlatform`' is set to PM platform
+            // it's for another platform such as MS
+            return false;
+        }
+    }
+
+    public AnyLogicModelInfo getPriorityModelInfo() {
+        AnyLogicModelInfo priorityModelInfo;
+        if (modelInfos.size() > 1) {
+            // Simulation has a higher priority than RLExperiment when they exist together
+            List<AnyLogicModelInfo> simulations = modelInfos.stream()
+                .filter(m -> m.getExperimentType().equals(AnyLogicModelInfo.ExperimentType.Simulation))
+                .collect(Collectors.toList());
+
+            // if there are more than two simulations, the simulation that has "Simulation" is higher priority
+            if (simulations.size() > 1) {
+                priorityModelInfo = simulations.stream()
+                    .filter(m -> m.getExperimentClass().endsWith("/Simulation"))
+                    .findFirst().orElse(null);
+            } else {
+                priorityModelInfo = simulations.get(0);
+            }
+        } else {
+            return modelInfos.get(0);
+        }
+
+        return priorityModelInfo;
     }
 }
