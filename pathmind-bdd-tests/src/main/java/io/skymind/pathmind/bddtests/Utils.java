b35d8ee16c403a8762d0294b20463ee375fc722d
EvgeniyEA <evegeniy@skymind.io>
2020-12-22 20:48:04 +0200

    Fix loading bar locator

--- pathmind-bdd-tests/src/main/java/io/skymind/pathmind/bddtests/Utils.java
+++ pathmind-bdd-tests/src/main/java/io/skymind/pathmind/bddtests/Utils.java
@@ -138,7 +138,7 @@ public class Utils extends PageObject {
 
     public void waitForLoadingBar() {
         try {
-            waitFor(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='v-loading-indicator first' and @style='display: none;']")));
+            waitFor(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@style='display: none;' and not(@role)]")));
         }catch (org.openqa.selenium.NoSuchElementException ex){
             ex.printStackTrace();
         }
