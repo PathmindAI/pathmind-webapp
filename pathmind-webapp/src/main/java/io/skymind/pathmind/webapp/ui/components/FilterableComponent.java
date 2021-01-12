143392426b858b2ad5530922fdd83eaaa2674c2e
Fionna <fionnacst@gmail.com>
2021-01-12 15:20:39 +0800

    Remove unused files

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/FilterableComponent.java
+++
@@ -1,9 +0,0 @@
-package io.skymind.pathmind.webapp.ui.components;
-
-import java.util.List;
-
-public interface FilterableComponent<T> {
-    public List<T> getData();
-
-    public void setFilteredData(List<T> filteredData);
-}
