8f6e5922a41690a68a4469beb276d06b5b65e0f5
Daniel Jaramillo <xneyder@gmail.com>
2021-01-12 16:34:59 -0500

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ sleep 12h
 
 HOSTNAME=$(hostname)
 aws sqs create-queue --queue-name ${HOSTNAME}-updater-queue --attributes "{\"MessageRetentionPeriod\":\"60\"}"
-cat ./infra/sqs_policy.json | sed \"s/BUILD_ID/${HOSTNAME}/g\" > /tmp/${HOSTNAME}-sqs_policy.json
-aws sqs set-queue-attributes --queue-url https://sqs.us-east-1.amazonaws.com/839270835622/${HOSTNAME}-updater-queue --attributes file:/tmp/${HOSTNAME}-sqs_policy.json
+cat ./infra/sqs_policy.json | sed "s/BUILD_ID/${HOSTNAME}/g" > /tmp/${HOSTNAME}-sqs_policy.json
+aws sqs set-queue-attributes --queue-url https://sqs.us-east-1.amazonaws.com/839270835622/${HOSTNAME}-updater-queue --attributes file://tmp/${HOSTNAME}-sqs_policy.json
 aws sns subscribe --topic-arn arn:aws:sns:us-east-1:839270835622:${ENVIRONMENT}-updater-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:839270835622:${HOSTNAME}-updater-queue --attributes "{\"RawMessageDelivery\":\"true\"}"
 
 export SQS_UPDATER_URL="https://sqs.us-east-1.amazonaws.com/839270835622/${ENVIRONMENT}-updater-queue"
