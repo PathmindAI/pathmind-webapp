b6411b7be15738040e8c4ee9877d8f6d13ca3ae6
Daniel Jaramillo <xneyder@gmail.com>
2021-01-12 14:52:02 -0500

    Add sleep for debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+sleep 12h
+
 HOSTNAME=$(hostname)
 aws sqs create-queue --queue-name ${HOSTNAME}-updater-queue --attributes "{\"MessageRetentionPeriod\":\"60\"}"
 cat ./infra/sqs_policy.json | sed \"s/BUILD_ID/${HOSTNAME}/g\" > /tmp/${HOSTNAME}-sqs_policy.json
