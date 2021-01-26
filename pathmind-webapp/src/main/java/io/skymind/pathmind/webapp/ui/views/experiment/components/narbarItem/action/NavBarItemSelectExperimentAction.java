3f5c7e5d8f6130301433e720e7b477e50dae895b
FollowSteph <steph@followsteph.com>
2021-01-25 23:52:13 -0500

    #2763 - Reward variable selection should stay fixed when toggling between experiments.

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/experiment/components/narbarItem/action/NavBarItemSelectExperimentAction.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/experiment/components/narbarItem/action/NavBarItemSelectExperimentAction.java
@@ -1,11 +1,14 @@
 package io.skymind.pathmind.webapp.ui.views.experiment.components.narbarItem.action;
 
 import io.skymind.pathmind.shared.data.Experiment;
+import io.skymind.pathmind.shared.data.RewardVariable;
 import io.skymind.pathmind.shared.security.Routes;
 import io.skymind.pathmind.shared.utils.CloneUtils;
 import io.skymind.pathmind.webapp.ui.views.experiment.AbstractExperimentView;
 import io.skymind.pathmind.webapp.ui.views.experiment.ExperimentView;
 import io.skymind.pathmind.webapp.ui.views.experiment.NewExperimentView;
+
+import java.util.List;
 
 public class NavBarItemSelectExperimentAction {
     public static void selectExperiment(Experiment experiment, AbstractExperimentView abstractExperimentView) {
@@ -36,7 +39,12 @@ public class NavBarItemSelectExperimentAction {
         } else {
             abstractExperimentView.getUI().ifPresent(ui -> ui.getPage().getHistory().pushState(null, Routes.EXPERIMENT + "/" + experiment.getId()));
             synchronized (abstractExperimentView.getExperimentLock()) {
-                abstractExperimentView.setExperiment(experiment);
+                // We have to clone the selected reward variables from the original and not the one coming from the event. We have to set the selected
+                // reward variables after the experiment has been loaded from the database. We also delay the rendering until the very end to avoid
+                // re-rendering multiple times.
+                List<RewardVariable> selectedRewardVariables = CloneUtils.shallowCloneList(abstractExperimentView.getExperiment().getSelectedRewardVariables());
+                abstractExperimentView.setExperiment(experiment, false);
+                abstractExperimentView.getExperiment().setSelectedRewardVariables(selectedRewardVariables);
                 // If we're in the experimentView in the experimentComparison mode then we need to select the rewardVariables from the comparison experiment
                 // to keep everything synchronized. This has to be done after setExperiment(experiment) above because setExperiment reloads the experiment from
                 // the database because the navbar only has a partial experiment (we don't load all the experiment data for the navbar for performance reasons.
@@ -46,8 +54,9 @@ public class NavBarItemSelectExperimentAction {
                             CloneUtils.shallowCloneList(((ExperimentView)abstractExperimentView).getComparisonExperiment().getSelectedRewardVariables()));
                     // We could update just the chart and reward variable component but since the chart is the only expensive component to render we may as well
                     // just re-render everything for now to keep the code simple.
-                    abstractExperimentView.updateComponents();
                 }
+                // The delayed rendering. This is needed for both setting the experiment, the reward variables, and possibly the comparisonExperiment if need be.
+                abstractExperimentView.updateComponents();
             }
         }
     }
