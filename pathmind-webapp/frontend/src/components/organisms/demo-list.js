3850afa880f9f09ac3fc9fdcb2858641c8b979f9
Fionna <fionnacst@gmail.com>
2021-02-01 17:18:58 +0800

    Move demo list to light dom

--- pathmind-webapp/frontend/src/components/organisms/demo-list.js
+++ pathmind-webapp/frontend/src/components/organisms/demo-list.js
@@ -9,7 +9,7 @@ class DemoList extends PolymerElement {
     static get template() {
         return html`
             <style>
-                :host {
+                demo-list {
                     box-sizing: border-box;
                     display: flex;
                     flex-direction: column;
@@ -20,35 +20,35 @@ class DemoList extends PolymerElement {
                     padding: 0 var(--lumo-space-m);
                     margin-bottom: var(--lumo-space-l);
                 }
-                vaadin-horizontal-layout {
+                demo-list vaadin-horizontal-layout {
                     flex-wrap: wrap;
                     justify-content: center;
                     align-items: stretch;
                 }
-                ul {
+                demo-list ul {
                     display: flex;
                     list-style: none;
                     padding: 0;
                     margin: 0;
                 }
-                li {
+                demo-list li {
                     margin: 0 var(--lumo-space-s);
                 }
-                h4 {
+                demo-list h4 {
                     width: 100%;
                     font-family: var(--lumo-font-family-header);
                     text-align: center;
                     margin: 0 0 var(--lumo-space-s);
                 }
-                p {
+                demo-list p {
                     line-height: 1.4;
                     margin-bottom: 0;
                 }
-                img {
+                demo-list img {
                     display: block;
                     width: 100%;
                 }
-                .demo-item {
+                demo-list .demo-item {
                     flex: 0 1 calc(33% - var(--lumo-space-l)*2);
                     max-width: 20rem;
                     font-size: var(--lumo-font-size-s);
@@ -58,18 +58,11 @@ class DemoList extends PolymerElement {
                     border-radius: var(--lumo-border-radius);
                     margin: 0 var(--lumo-space-l) var(--lumo-space-l);
                 }
-                details {
-                    width: 100%;
-                    cursor: pointer;
-                }
-                summary:hover {
-                    color: var(--lumo-primary-color);
-                }
-                :host([list-view]) {
+                demo-list[list-view] {
                     flex-direction: column;
                     padding: 0;
                 }
-                :host([list-view]) .demo-item {
+                demo-list[list-view] .demo-item {
                     align-items: center;
                     flex-direction: row;
                     flex: 1 0 100%;
@@ -78,32 +71,32 @@ class DemoList extends PolymerElement {
                     box-shadow: none;
                     border-bottom: 1px solid var(--pm-grey-color-lightest);
                 }
-                :host([list-view]) .demo-item:hover {
+                demo-list[list-view] .demo-item:hover {
                     background-color: var(--pm-app-bg-color);
                 }
-                :host([list-view]) .demo-item h4 {
+                demo-list[list-view] .demo-item h4 {
                     flex: 0 0 20%;
                     width: 20%;
                     text-align: left;
                     margin-right: var(--lumo-space-l);
                 }
-                :host([list-view]) p {
+                demo-list[list-view] p {
                     margin: 0;
                 }
-                .description {
+                demo-list .description {
                     margin-bottom: var(--lumo-space-m);
                 }
-                .result {
+                demo-list .result {
                     margin: 0 0 var(--lumo-space-m);
                 }
-                .arrow-icon {
+                demo-list .arrow-icon {
                     display: inline-block;
                     position: relative;
                     width: .7rem;
                     height: .7rem;
                     margin-left: var(--lumo-space-xs);
                 }
-                .arrow-icon::after {
+                demo-list .arrow-icon::after {
                     content: '';
                     box-sizing: border-box;
                     display: block;
@@ -116,40 +109,65 @@ class DemoList extends PolymerElement {
                     border-bottom: 2px solid white;
                     transform: rotate(-45deg) skew(12deg, 12deg);
                 }
-                vaadin-button {
+                demo-list vaadin-button {
                     width: 100%;
                     margin-top: auto;
                 }
-                vaadin-button > span {
+                demo-list vaadin-button > span {
                     display: flex;
                     align-items: center;
                 }
-                vaadin-button[loading] > span.getStartedText,
-                vaadin-button:not([loading]) > span.loadingText {
+                demo-list vaadin-button[loading] > span.getStartedText,
+                demo-list vaadin-button:not([loading]) > span.loadingText {
                     display: none;
                 }
-                loading-spinner {
+                demo-list loading-spinner {
                     display: inline-block;
                     margin-right: var(--lumo-space-xs);
                 }
             </style>
             <vaadin-horizontal-layout>
-                <dom-repeat items="{{demoDataList}}">
-                    <template>
-                        <vaadin-vertical-layout class="demo-item">
-                            <h4>{{item.name}}</h4>
-                            <div class="demo-img-wrapper"><img src="{{item.imageUrl}}"/></div>
-                            <p class="description">{{item.description}}</p>
-                            <p class="result"><b>Result:</b> {{item.result}}</p>
-                            <vaadin-button theme="primary" data-item$="{{item.name}}" on-click="buttonClickedHandler" disabled="[[disableButtons]]">
-                                <span class="getStartedText">Get Started <span class="arrow-icon"></span></span>
-                                <span class="loadingText"><loading-spinner></loading-spinner>Loading...</span>
-                            </vaadin-button>
-                        </vaadin-vertical-layout>
-                    </template>
-                </dom-repeat>
+                <vaadin-vertical-layout class="demo-item">
+                    <h4>[[_getDataItem(demoDataList, '0', 'name')]]</h4>
+                    <div class="demo-img-wrapper"><img src="[[_getDataItem(demoDataList, '0', 'imageUrl')]]"/></div>
+                    <p class="description">[[_getDataItem(demoDataList, '0', 'description')]]</p>
+                    <p class="result"><b>Result:</b> [[_getDataItem(demoDataList, '0', 'result')]]</p>
+                    <vaadin-button theme="primary" data-item$="[[_getDataItem(demoDataList, '0', 'name')]]" on-click="buttonClickedHandler" disabled="[[disableButtons]]">
+                        <span class="getStartedText">Get Started <span class="arrow-icon"></span></span>
+                        <span class="loadingText"><loading-spinner></loading-spinner>Loading...</span>
+                    </vaadin-button>
+                </vaadin-vertical-layout>
+                <vaadin-vertical-layout class="demo-item">
+                    <h4>[[_getDataItem(demoDataList, '1', 'name')]]</h4>
+                    <div class="demo-img-wrapper"><img src="[[_getDataItem(demoDataList, '1', 'imageUrl')]]"/></div>
+                    <p class="description">[[_getDataItem(demoDataList, '1', 'description')]]</p>
+                    <p class="result"><b>Result:</b> [[_getDataItem(demoDataList, '1', 'result')]]</p>
+                    <vaadin-button theme="primary" data-item$="[[_getDataItem(demoDataList, '1', 'name')]]" on-click="buttonClickedHandler" disabled="[[disableButtons]]">
+                        <span class="getStartedText">Get Started <span class="arrow-icon"></span></span>
+                        <span class="loadingText"><loading-spinner></loading-spinner>Loading...</span>
+                    </vaadin-button>
+                </vaadin-vertical-layout>
+                <vaadin-vertical-layout class="demo-item">
+                    <h4>[[_getDataItem(demoDataList, '2', 'name')]]</h4>
+                    <div class="demo-img-wrapper"><img src="[[_getDataItem(demoDataList, '2', 'imageUrl')]]"/></div>
+                    <p class="description">[[_getDataItem(demoDataList, '2', 'description')]]</p>
+                    <p class="result"><b>Result:</b> [[_getDataItem(demoDataList, '2', 'result')]]</p>
+                    <vaadin-button theme="primary" name$="[[_getDataItem(demoDataList, '2', 'name')]]" on-click="buttonClickedHandler" disabled="[[disableButtons]]">
+                        <span class="getStartedText">Get Started <span class="arrow-icon"></span></span>
+                        <span class="loadingText"><loading-spinner></loading-spinner>Loading...</span>
+                    </vaadin-button>
+                </vaadin-vertical-layout>
             </vaadin-horizontal-layout>
         `;
+    }
+
+    _attachDom(dom) {
+      this.appendChild(dom);
+    }
+
+    _getDataItem(demoList, index, key) {
+        if (demoList.length == 0) return "";
+        return demoList[index][key];
     }
 
     setData(demoDataList) {
