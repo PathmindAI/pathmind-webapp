205c35a55c9c0a05ecbc70539b6ae2f9440121f6
Daniel Jaramillo <xneyder@gmail.com>
2021-03-22 15:15:55 -0400

    add api and policy to the path

--- infra/images/policy-deployer/run_helm.sh
+++ infra/images/policy-deployer/run_helm.sh
@@ -7,8 +7,8 @@ NAMESPACE=$5
 helm upgrade --install ${helm_name} \
 	policy-server/helm/policy-server/ \
 	--set image.tag=${ENVIRONMENT}${JobId} \
-	--set 'ingress.hosts[0].host'=${ENVIRONMENT}.${domain_name} \
-	--set 'ingress.hosts[0].paths[0]'="/${JobId}(/|\$)(.*)" \
-	--set 'ingress.tls[0].hosts[0]'=${ENVIRONMENT}.${domain_name} \
+	--set 'ingress.hosts[0].host'=api.${ENVIRONMENT}.${domain_name} \
+	--set 'ingress.hosts[0].paths[0]'="/policy/${JobId}(/|\$)(.*)" \
+	--set 'ingress.tls[0].hosts[0]'=api.${ENVIRONMENT}.${domain_name} \
 	--set 'ingress.tls[0].secretName'=letsencrypt-${ENVIRONMENT} \
 	-n ${ENVIRONMENT}
