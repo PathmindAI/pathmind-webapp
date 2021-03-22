475c257674dee83f5e0f42fa7d1b657353652f7c
Daniel Jaramillo <xneyder@gmail.com>
2021-03-22 14:30:11 -0400

    Fix deomain

--- infra/images/policy-deployer/run_helm.sh
+++ infra/images/policy-deployer/run_helm.sh
@@ -7,8 +7,8 @@ NAMESPACE=$5
 helm upgrade --install ${helm_name} \
 	policy-server/helm/policy-server/ \
 	--set image.tag=${ENVIRONMENT}${JobId} \
-	--set 'ingress.hosts[0].host'=${JobId}.${ENVIRONMENT}.${domain_name} \
-	--set 'ingress.hosts[0].paths[0]'=/ \
-	--set 'ingress.tls[0].hosts[0]'=${JobId}.${ENVIRONMENT}.${domain_name} \
+	--set 'ingress.hosts[0].host'=${ENVIRONMENT}.${domain_name} \
+	--set 'ingress.hosts[0].paths[0]'="/${JobId}(/|\$)(.*)" \
+	--set 'ingress.tls[0].hosts[0]'=${ENVIRONMENT}.${domain_name} \
 	--set 'ingress.tls[0].secretName'=letsencrypt-${ENVIRONMENT} \
 	-n ${ENVIRONMENT}
