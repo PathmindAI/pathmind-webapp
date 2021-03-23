ac2d6b46df2e8d9fb08caed2621220cfa1e74cf6
Fionna <fionnacst@gmail.com>
2021-03-23 16:41:37 +0800

    Add different deployment messages

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/experiment/actions/experiment/ServePolicyAction.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/experiment/actions/experiment/ServePolicyAction.java
@@ -2,6 +2,7 @@ package io.skymind.pathmind.webapp.ui.views.experiment.actions.experiment;
 
 import java.util.function.Supplier;
 
+import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Anchor;
@@ -10,29 +11,35 @@ import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.progressbar.ProgressBar;
+
 import io.skymind.pathmind.shared.data.Experiment;
 import io.skymind.pathmind.shared.services.PolicyServerService;
-import io.skymind.pathmind.webapp.ui.components.atoms.ButtonWithLoading;
 import io.skymind.pathmind.webapp.ui.components.molecules.CopyField;
 
 public class ServePolicyAction {
 
-    public static void servePolicy(Supplier<Experiment> getExperimentSupplier, ButtonWithLoading buttonWithLoading, PolicyServerService policyServerService) {
+    public static void servePolicy(Supplier<Experiment> getExperimentSupplier, PolicyServerService policyServerService) {
+        PolicyServerService.DeploymentStatus deploymentStatus = policyServerService.getPolicyServerStatus(experiment);
         Experiment experiment = getExperimentSupplier.get();
         Dialog dialog = new Dialog();
         Div dialogContent = new Div();
+        ProgressBar progressBar = new ProgressBar(0, 100);
         Button closeButton = new Button(VaadinIcon.CLOSE_SMALL.create());
         closeButton.addClickListener(event -> {
-            buttonWithLoading.setLoading(false);
-            buttonWithLoading.setDisabled(false);
             dialog.close();
         });
 
-        PolicyServerService.DeploymentStatus deploymentStatus = policyServerService.getPolicyServerStatus(experiment);
-
         switch (deploymentStatus) {
             case FAILED: {
-                // TODO: show some message on fail deployment
+                dialogContent.add(
+                        new H3("Error"),
+                        new Paragraph(
+                            new Span("The policy server deployment was unsuccessful."),
+                            new Html("<br/>"),
+                            new Span("Please contact Pathmind for assistance.")
+                        )
+                );
                 break;
             }
             case DEPLOYED: {
@@ -52,10 +59,15 @@ public class ServePolicyAction {
                 // intentional fallthrough to PENDING state
             }
             case PENDING: {
+                progressBar.setValue(42); // need to get updated according to deployment progress
                 dialogContent.add(
-                        // TODO: UI/UX and dynamic update
-                        new H3("Policy Serving is deploying"),
-                        new Paragraph("TODO: Visit this message when it's deployed")
+                        new H3("Deploying Policy Server"),
+                        new Paragraph(
+                            new Span("The policy server may take up to an hour to be ready."),
+                            new Html("<br/>"),
+                            new Span("We’ll send you an email once it’s complete.")
+                        ),
+                        progressBar
                 );
                 break;
             }
