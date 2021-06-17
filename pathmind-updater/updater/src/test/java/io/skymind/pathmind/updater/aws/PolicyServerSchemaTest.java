6cad146196316b5c17432416319b3e9353bb68cf
Alexander <alexander.makarov@toptal.com>
2021-06-17 09:14:19 +0200

    add schema yaml test

--- pathmind-updater/updater/src/test/java/io/skymind/pathmind/updater/aws/PolicyServerSchemaTest.java
+++ pathmind-updater/updater/src/test/java/io/skymind/pathmind/updater/aws/PolicyServerSchemaTest.java
@@ -1,5 +1,8 @@
 package io.skymind.pathmind.updater.aws;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -8,16 +11,21 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import io.skymind.pathmind.shared.data.Observation;
 import io.skymind.pathmind.shared.services.PolicyServerService;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.springframework.util.ResourceUtils;
+import org.yaml.snakeyaml.Yaml;
 
 import static io.skymind.pathmind.shared.services.PolicyServerService.PolicyServerSchema.typeOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class PolicyServerSchemaTest {
 
     private final ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory().enable(YAMLGenerator.Feature.MINIMIZE_QUOTES));
 
     @Test
-    public void test() throws JsonProcessingException {
+    public void test() throws IOException {
 
         Observation oInt = new Observation();
         oInt.setDataType("int");
@@ -49,6 +57,17 @@ public class PolicyServerSchemaTest {
         String yamlString = yamlMapper.writeValueAsString(schema);
 
         System.out.println(yamlString);
+
+        Yaml yaml = new Yaml();
+
+        String expectedSchema = FileUtils.readFileToString(ResourceUtils.getFile("classpath:schema.yaml"), Charset.defaultCharset());
+
+        Map<Object, Object> expected = yaml.load(expectedSchema);
+        Map<Object, Object> actual = yaml.load(yamlString);
+
+        assertThat(actual.entrySet(), equalTo(expected.entrySet()));
+
+
     }
 
 
