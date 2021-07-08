1cbaabda3e054b0ff66ccc12c164c9e27c7f4a98
Fionna <fionnacst@gmail.com>
2021-07-08 16:19:41 +0800

    Migrate StatusIcon from Polymer Template to LitElement (#3293)

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/atoms/StatusIcon.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/atoms/StatusIcon.java
@@ -2,23 +2,22 @@ package io.skymind.pathmind.webapp.ui.components.atoms;
 
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
-import com.vaadin.flow.templatemodel.TemplateModel;
+import com.vaadin.flow.component.littemplate.LitTemplate;
 import io.skymind.pathmind.shared.constants.RunStatus;
 import io.skymind.pathmind.shared.data.Experiment;
 import io.skymind.pathmind.webapp.data.utils.ExperimentGuiUtils;
 
 @Tag("status-icon")
-@JsModule("./src/components/atoms/status-icon.js")
-public class StatusIcon extends PolymerTemplate<StatusIcon.Model> {
+@JsModule("./src/components/atoms/status-icon.ts")
+public class StatusIcon extends LitTemplate {
 
     String statusIconType;
 
     public StatusIcon(Experiment experiment) {
         RunStatus overallExperimentStatus = experiment.getTrainingStatusEnum();
         statusIconType = getIconStatus(experiment, overallExperimentStatus);
-        getModel().setStatus(statusIconType);
-        getModel().setStatusText(overallExperimentStatus.toString());
+        getElement().setProperty("status", statusIconType);
+        getElement().setProperty("statustext", overallExperimentStatus.toString());
     }
 
     private String getIconStatus(Experiment experiment, RunStatus status) {
@@ -27,11 +26,5 @@ public class StatusIcon extends PolymerTemplate<StatusIcon.Model> {
 
     public String getStatusIconType() {
         return statusIconType;
-    }
-
-    public interface Model extends TemplateModel {
-        void setStatus(String iconStatus);
-
-        void setStatusText(String statusText);
     }
 }
