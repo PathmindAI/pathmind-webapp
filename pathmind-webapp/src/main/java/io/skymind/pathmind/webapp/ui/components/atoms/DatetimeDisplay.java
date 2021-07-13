69c756263f98919ee34e3967fed10c160b052965
Fionna <fionnacst@gmail.com>
2021-07-13 16:56:15 +0800

    Migrate DatetimeDisplay from Polymer to LitElement (#3296)

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/atoms/DatetimeDisplay.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/atoms/DatetimeDisplay.java
@@ -7,29 +7,21 @@ import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
-import com.vaadin.flow.templatemodel.TemplateModel;
+import com.vaadin.flow.component.littemplate.LitTemplate;
 
 @Tag("datetime-display")
 @NpmPackage(value = "timeago.js", version = "4.0.2")
-@JsModule("/src/components/atoms/datetime-display.js")
-public class DatetimeDisplay extends PolymerTemplate<DatetimeDisplay.Model> implements HasStyle {
-
+@JsModule("/src/components/atoms/datetime-display.ts")
+public class DatetimeDisplay extends LitTemplate implements HasStyle {
     public DatetimeDisplay(LocalDateTime datetime) {
         super();
-        getModel().setDatetime(datetime.toString());
-        getModel().setServerTimeZoneOffsetFromUTC(calculateTimeZoneOffset());
+        getElement().setProperty("datetime", datetime.toString());
+        getElement().setProperty("serverTimeZoneOffsetFromUTC", calculateTimeZoneOffset());
     }
 
     private String calculateTimeZoneOffset() {
         LocalDateTime now = LocalDateTime.now();
         ZoneId serverTimeZone = ZoneId.systemDefault();
         return serverTimeZone.getRules().getOffset(now).toString();
-    }
-
-    public interface Model extends TemplateModel {
-        void setDatetime(String datetime);
-
-        void setServerTimeZoneOffsetFromUTC(String serverTimeZoneOffsetFromUTC);
     }
 }
