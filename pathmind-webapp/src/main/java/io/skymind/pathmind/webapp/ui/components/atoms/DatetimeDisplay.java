9be0afed773eccef45f0c38bcfc682e71dab3579
Fionna <fionnacst@gmail.com>
2021-07-13 18:48:49 +0800

    Migrate RequestOnboardingServiceButton from Polymer Template to LitElement (#3332) (WIP)

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/atoms/DatetimeDisplay.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/atoms/DatetimeDisplay.java
@@ -3,7 +3,6 @@ package io.skymind.pathmind.webapp.ui.components.atoms;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
@@ -11,8 +10,8 @@ import com.vaadin.flow.component.littemplate.LitTemplate;
 
 @Tag("datetime-display")
 @NpmPackage(value = "timeago.js", version = "4.0.2")
-@JsModule("/src/components/atoms/datetime-display.ts")
-public class DatetimeDisplay extends LitTemplate implements HasStyle {
+@JsModule("./src/components/atoms/datetime-display.ts")
+public class DatetimeDisplay extends LitTemplate {
     public DatetimeDisplay(LocalDateTime datetime) {
         super();
         getElement().setProperty("datetime", datetime.toString());
