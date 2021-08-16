9d7a43f15b33da2cb9837ba5db881f7e5e4a2f12
kepricon <kepricon@gmail.com>
2021-08-16 12:34:11 -0700

    handle NPE

--- pathmind-shared/src/main/java/io/skymind/pathmind/shared/utils/SimulationParameterUtils.java
+++ pathmind-shared/src/main/java/io/skymind/pathmind/shared/utils/SimulationParameterUtils.java
@@ -3,6 +3,7 @@ package io.skymind.pathmind.shared.utils;
 import io.skymind.pathmind.shared.constants.ParamType;
 import io.skymind.pathmind.shared.data.SimulationParameter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -10,6 +11,10 @@ import java.util.stream.Collectors;
 
 public class SimulationParameterUtils {
     public static List<SimulationParameter> convertToSimulationParams(Long modelId, Long experimentId, Map<String, Object> paramMap) {
+        if (paramMap == null || paramMap.isEmpty()) {
+            return new ArrayList<>();
+        }
+
         AtomicInteger index = new AtomicInteger();
         return paramMap.entrySet().stream()
             .map(param -> {
