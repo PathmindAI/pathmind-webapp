16d116e3a2156b15c82e584320b34ded80e782cc
kepricon <kepricon@gmail.com>
2021-08-16 14:28:55 -0700

    generate jooq classes

--- pathmind-database/src/main/java/io/skymind/pathmind/db/jooq/Public.java
+++ pathmind-database/src/main/java/io/skymind/pathmind/db/jooq/Public.java
@@ -25,7 +25,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.jooq.Catalog;
-import org.jooq.Sequence;
 import org.jooq.Table;
 import org.jooq.impl.SchemaImpl;
 
@@ -134,20 +133,6 @@ public class Public extends SchemaImpl {
     @Override
     public Catalog getCatalog() {
         return DefaultCatalog.DEFAULT_CATALOG;
-    }
-
-    @Override
-    public final List<Sequence<?>> getSequences() {
-        return Arrays.<Sequence<?>>asList(
-            Sequences.EXPERIMENT_ID_SEQ,
-            Sequences.MODEL_ID_SEQ,
-            Sequences.OBSERVATION_ID_SEQ,
-            Sequences.PATHMIND_USER_ID_SEQ,
-            Sequences.POLICY_ID_SEQ,
-            Sequences.PROJECT_ID_SEQ,
-            Sequences.REWARD_VARIABLE_ID_SEQ,
-            Sequences.RUN_ID_SEQ,
-            Sequences.TRAINING_ERROR_ID_SEQ);
     }
 
     @Override
