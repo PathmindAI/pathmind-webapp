570ca8973c81cb2a7e2b77c600beb2099573985a
Fionna <fionnacst@gmail.com>
2021-08-18 15:17:55 +0800

    Update pricing page with Educational Plan

--- pathmind-webapp/frontend/src/pages/account/account-upgrade-view-content.js
+++ pathmind-webapp/frontend/src/pages/account/account-upgrade-view-content.js
@@ -16,12 +16,15 @@ class AccountUpgradeViewContent extends PolymerElement {
             align-items: stretch;
             width: 100%;
         }
-        account-upgrade-view-content h1 {
+        account-upgrade-view-content .header {
             margin: 0 auto var(--lumo-space-xl);
         }
+        account-upgrade-view-content .header h1 {
+            margin: 0 auto var(--lumo-space-xxs);
+        }
         account-upgrade-view-content .inner-content {
-            flex: 1 0 calc((100% - var(--lumo-space-l) * 2) / 3);
-          width: calc((100% - var(--lumo-space-l) * 2) / 3);
+          flex: 1 0 calc((100% - var(--lumo-space-l) * 3) / 4);
+          width: calc((100% - var(--lumo-space-l) * 3) / 4);
           font-size: var(--lumo-font-size-m);
           padding: var(--lumo-space-l);
           border: 1px solid var(--pm-grey-color-lighter);
@@ -31,8 +34,10 @@ class AccountUpgradeViewContent extends PolymerElement {
             width: 100%;
             margin: auto auto var(--lumo-space-m);
         }
-        account-upgrade-view-content .inner-content:nth-child(2) {
-          margin: 0 var(--lumo-space-xl) 0;
+        account-upgrade-view-content .inner-content:nth-child(2),
+        account-upgrade-view-content .inner-content:nth-child(3),
+        account-upgrade-view-content .inner-content:nth-child(4) {
+          margin: 0 0 0 var(--lumo-space-l);
         }
         @media screen and (max-width: 1023px) {
             account-upgrade-view-content {
@@ -48,8 +53,10 @@ class AccountUpgradeViewContent extends PolymerElement {
                 width: 100%;
                 margin: 0;
             }
-            account-upgrade-view-content .inner-content:nth-child(2) {
-                margin: var(--lumo-space-xxl) 0;
+            account-upgrade-view-content .inner-content:nth-child(2),
+            account-upgrade-view-content .inner-content:nth-child(3),
+            account-upgrade-view-content .inner-content:nth-child(4) {
+                margin: var(--lumo-space-xxl) 0 0 0;
             }
         }
         account-upgrade-view-content .card-header {
@@ -69,10 +76,29 @@ class AccountUpgradeViewContent extends PolymerElement {
             font-weight: bold;
             line-height: 1;
             letter-spacing: .04rem;
-            background-color: var(--pm-friendly-color);
+            background-color: rgb(0 177 169);
             padding: var(--lumo-space-xxs) var(--lumo-space-xs);
             border-radius: var(--lumo-border-radius-s);
             box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.16);
+            cursor: default;
+            animation: 2s ease 2s 3 forwards move;
+        }
+        @keyframes move {
+            0% {
+                transform: rotate(0deg);
+            }
+            12% {
+                transform: rotate(-13deg);
+            }
+            27% {
+                transform: rotate(12deg);
+            }
+            35% {
+                transform: rotate(-4deg);
+            }
+            42% {
+                transform: rotate(0deg);
+            }
         }
         account-upgrade-view-content .inner-content:nth-child(1) .card-header {
             background-color: var(--pm-grey-color-lightest);
@@ -81,15 +107,43 @@ class AccountUpgradeViewContent extends PolymerElement {
             color: var(--pm-grey-color-darkest);
         }
         account-upgrade-view-content .inner-content:nth-child(2) .card-header {
-            background-color: rgb(216 238 245);
+            background-color: rgb(230 241 247);
         }
         account-upgrade-view-content .inner-content:nth-child(2) .title {
-            color: var(--pm-primary-color-dark);
+            color: rgb(107 118 148);
         }
         account-upgrade-view-content .inner-content:nth-child(3) .card-header {
-            background-color: var(--pm-blue-color-light);
+            background: linear-gradient(158deg, rgb(222 245 253), rgb(172 178 255));
+            background-size: 160% 160%;
+            animation: floating 5s infinite;
+        }
+        @keyframes floating {
+            0% {
+                background-position: 0% 0%;
+            }
+            20% {
+                background-position: 15% 15%;
+            }
+            30% {
+                background-position: 18% 27%;
+            }
+            50% {
+                background-position: 27% 16%;
+            }
+            60% {
+                background-position: 8% 12%;
+            }
+            80% {
+                background-position: 3% 5%;
+            }
         }
         account-upgrade-view-content .inner-content:nth-child(3) .title {
+            color: var(--pm-grey-color-darkest);
+        }
+        account-upgrade-view-content .inner-content:nth-child(4) .card-header {
+            background-color: rgb(233 242 255);
+        }
+        account-upgrade-view-content .inner-content:nth-child(4) .title {
             color: var(--pm-blue-color-dark);
         }
         account-upgrade-view-content .title {
@@ -113,6 +167,8 @@ class AccountUpgradeViewContent extends PolymerElement {
         }
         account-upgrade-view-content .additional-info {
             font-size: var(--lumo-font-size-s);
+            text-align: left;
+            margin-top: auto;
         }
         account-upgrade-view-content .features {
             text-align: left;
@@ -152,7 +208,10 @@ class AccountUpgradeViewContent extends PolymerElement {
           margin-top: var(--lumo-space-xl);
         }
       </style>
-    <h1>Subscription Plans</h1>
+    <div class="header">
+      <h1>Subscription Plans</h1>
+      <span>Billed monthly per user</span>
+    </div>
     <vaadin-horizontal-layout>
         <vaadin-vertical-layout class="inner-content">
             <vaadin-vertical-layout class="card-header">
@@ -161,13 +220,30 @@ class AccountUpgradeViewContent extends PolymerElement {
             </vaadin-vertical-layout>
             <span class="price-cont">
                 <span class="price">Free</span>
-                <span class="additional-info">&nbsp;</span>
             </span>
             <ul class="features">
                 <li><b>One Concurrent</b> Experiment</li>
                 <li>Unlimited Policy Export</li>
             </ul>
-            <vaadin-button id="freeBtn" theme="tertiary" disabled>Current Plan</vaadin-button>
+            <vaadin-button id="freeBtn" theme="tertiary" disabled>Current plan</vaadin-button>
+        </vaadin-vertical-layout>
+        <vaadin-vertical-layout class="inner-content">
+            <vaadin-vertical-layout class="card-header">
+                <h2 class="title">Educational</h2>
+                <span class="details">For Students & Academics*</span>
+            </vaadin-vertical-layout>
+            <span class="price-cont">
+                <span><span class="price">$99</span>/Month</span>
+            </span>
+            <ul class="features">
+                <li><b>Unlimited Concurrent</b> Experiments</li>
+                <li>Unlimited Policy Export</li>
+                <li>Technical Support Included</li>
+            </ul>
+            <i class="additional-info">*Promo code required. Contact us for more info.</i>
+            <a href="{{contactLink}}">
+                <vaadin-button id="studentBtn" theme="primary">Get in touch</vaadin-button>
+            </a>
         </vaadin-vertical-layout>
         <vaadin-vertical-layout class="inner-content">
             <vaadin-vertical-layout class="card-header">
@@ -177,14 +253,13 @@ class AccountUpgradeViewContent extends PolymerElement {
             </vaadin-vertical-layout>
             <span class="price-cont">
                 <span><span class="price">$499</span>/Month</span>
-                <span class="additional-info">Billed month-to-month</span>
             </span>
             <ul class="features">
                 <li><b>Unlimited Concurrent</b> Experiments</li>
                 <li>Unlimited Policy Export</li>
                 <li>Technical Support Included</li>
             </ul>
-            <vaadin-button id="proBtn" theme="primary" on-click="handleProClick">Choose Pro</vaadin-button>
+            <vaadin-button id="proBtn" theme="primary" on-click="handleProClick">Upgrade now</vaadin-button>
         </vaadin-vertical-layout>
         <vaadin-vertical-layout class="inner-content">
             <vaadin-vertical-layout class="card-header">
@@ -193,7 +268,6 @@ class AccountUpgradeViewContent extends PolymerElement {
             </vaadin-vertical-layout>
             <span class="price-cont">
                 <span><span class="price">$999</span>/Month</span>
-                <span class="additional-info">Billed month-to-month; price is per user</span>
             </span>
             <ul class="features">
                 <li><b>Unlimited Concurrent</b> Experiments</li>
@@ -203,11 +277,10 @@ class AccountUpgradeViewContent extends PolymerElement {
                 <li>RL Advisory and Training</li>
             </ul>
             <a href="{{contactLink}}">
-                <vaadin-button id="enterpriseBtn" theme="primary">Contact Us</vaadin-button>
+                <vaadin-button id="enterpriseBtn" theme="primary">Get in touch</vaadin-button>
             </a>
         </vaadin-vertical-layout>
     </vaadin-horizontal-layout>
-    <p class="caption">Applicable taxes not included</p>
     `;
   }
 
