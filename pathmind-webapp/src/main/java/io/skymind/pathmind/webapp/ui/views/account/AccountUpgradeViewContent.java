d1ff0d6518a14435df8bdf6231c8dda66b1b20d1
Fionna <fionnacst@gmail.com>
2021-09-09 12:30:30 +0800

    Migrate AccountUpgradeViewContent to LitElement (#3473)

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/account/AccountUpgradeViewContent.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/views/account/AccountUpgradeViewContent.java
@@ -3,12 +3,10 @@ package io.skymind.pathmind.webapp.ui.views.account;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.component.polymertemplate.Id;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.spring.annotation.SpringComponent;
-import com.vaadin.flow.templatemodel.TemplateModel;
 
-import io.skymind.pathmind.shared.data.PathmindUser;
 import io.skymind.pathmind.webapp.security.CurrentUser;
 import io.skymind.pathmind.webapp.ui.plugins.SegmentIntegrator;
 
@@ -18,14 +16,12 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 
 @Tag("account-upgrade-view-content")
-@JsModule("./src/pages/account/account-upgrade-view-content.js")
+@JsModule("./src/pages/account/account-upgrade-view-content.ts")
 @SpringComponent
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
-public class AccountUpgradeViewContent extends PolymerTemplate<AccountUpgradeViewContent.Model> {
+public class AccountUpgradeViewContent extends LitTemplate {
     @Id("proBtn")
     private Button proBtn;
-
-    private PathmindUser user;
 
     @Autowired
     public AccountUpgradeViewContent(CurrentUser currentUser,
@@ -33,22 +29,14 @@ public class AccountUpgradeViewContent extends PolymerTemplate<AccountUpgradeVie
                                      @Value("${pathmind.contact-support.address}") String contactLink,
                                      @Value("${pathmind.stripe.public.key}") String publicKey,
                                      SegmentIntegrator segmentIntegrator) {
-        getModel().setContactLink(contactLink);
-        user = currentUser.getUser();
-        getModel().setKey(publicKey);
-        getModel().setUserApiKey(user.getApiKey());
-        getModel().setApiUrl(apiUrl);
+        getElement().setProperty("contactLink", contactLink);
+        getElement().setProperty("key", publicKey);
+        getElement().setProperty("userApiKey", currentUser.getUser().getApiKey());
+        getElement().setProperty("apiUrl", apiUrl);
 
         proBtn.addClickListener(e -> getUI().ifPresent(ui -> {
             segmentIntegrator.upgradeToProPlanClicked();
         }));
-    }
-
-    public interface Model extends TemplateModel {
-        void setContactLink(String contactLink);
-        void setKey(String key);
-        void setUserApiKey(String key);
-        void setApiUrl(String pathmindApiUrl);
     }
 
 }
