fcfddc95a60f9556ef3601bf2886f55584754e4a
Fionna <fionnacst@gmail.com>
2021-09-09 16:12:18 +0800

    Migrate CopyField to LitElement (#3302)

--- pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/molecules/CopyField.java
+++ pathmind-webapp/src/main/java/io/skymind/pathmind/webapp/ui/components/molecules/CopyField.java
@@ -2,25 +2,17 @@ package io.skymind.pathmind.webapp.ui.components.molecules;
 
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
-import com.vaadin.flow.templatemodel.TemplateModel;
+import com.vaadin.flow.component.littemplate.LitTemplate;
 
 @Tag("copy-field")
-@JsModule("./src/components/molecules/copy-field.js")
-public class CopyField extends PolymerTemplate<CopyField.Model> {
-
+@JsModule("./src/components/molecules/copy-field.ts")
+public class CopyField extends LitTemplate {
     public CopyField(String text) {
         this(text, false);
     }
 
     public CopyField(String text, Boolean checkUrlReady) {
-        getModel().setText(text);
-        getModel().setCheckUrlReady(checkUrlReady);
-    }
-
-    public interface Model extends TemplateModel {
-        void setText(String text);
-
-        void setCheckUrlReady(Boolean checkUrlReady);
+        getElement().setProperty("text", text);
+        getElement().setProperty("checkUrlReady", checkUrlReady);
     }
 }
