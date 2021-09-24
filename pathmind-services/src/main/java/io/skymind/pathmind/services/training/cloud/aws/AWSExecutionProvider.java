4d7355ad275b93a17719a42f8e47cf17c309ed22
Daehyun <kepricon@gmail.com>
2021-09-24 10:23:48 -0700

    update natvierl version(1.7.2)

--- pathmind-services/src/main/java/io/skymind/pathmind/services/training/cloud/aws/AWSExecutionProvider.java
+++ pathmind-services/src/main/java/io/skymind/pathmind/services/training/cloud/aws/AWSExecutionProvider.java
@@ -389,6 +389,7 @@ public class AWSExecutionProvider implements ExecutionProvider {
             case VERSION_1_6_2:
             case VERSION_1_7_0:
             case VERSION_1_7_1:
+            case VERSION_1_7_2:
                 nativerlVersion.fileNames().forEach(filename -> {
                     instructions.addAll(Arrays.asList(
                         // Setup NativeRL
