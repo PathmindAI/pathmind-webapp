0102095620b8f17c9dc9f14bf63b111a37995fc4
Fionna Chan <fionnacst@gmail.com>
2021-10-06 20:34:12 +0800

    Add use_auto_norm to internal advanced settings UI (#3574)
    * Add use_auto_norm to internal advanced settings UI
    
    * Clean up reward balance period code in AWSExecutionProvider

--- pathmind-services/src/main/java/io/skymind/pathmind/services/training/cloud/aws/AWSExecutionProvider.java
+++ pathmind-services/src/main/java/io/skymind/pathmind/services/training/cloud/aws/AWSExecutionProvider.java
@@ -598,7 +598,9 @@ public class AWSExecutionProvider implements ExecutionProvider {
                 var("TRAIN_BATCH_MODE", job.getEnv().getBatchMode().toString()),
                 var("ROLLOUT_FRAGMENT_LENGTH", String.valueOf(job.getEnv().getRolloutFragmentLength())),
                 var("NUM_WORKERS", String.valueOf(job.getEnv().getNumWorker())),
-                var("NUM_CPUS", String.valueOf(2))
+                var("NUM_CPUS", String.valueOf(2)),
+                var("USE_AUTO_NORM", String.valueOf(job.getEnv().isUseAutoNorm())),
+                var("REWARD_BALANCE_PERIOD", String.valueOf(job.getEnv().getRewardBalancePeriod()))
         ));
 
         if (StringUtils.isNotEmpty(job.getTermsWeight())) {
@@ -607,11 +609,10 @@ public class AWSExecutionProvider implements ExecutionProvider {
         }
 
         if (job.getEnv().getTrainBatchSize() != 0) {
+            // this condition is used because train_batch_size is optional in nativerl
+            // it is also used for a null check condition in python
+            // https://github.com/SkymindIO/nativerl/pull/378/files
             instructions.add(var("TRAIN_BATCH_SIZE", String.valueOf(job.getEnv().getTrainBatchSize())));
-        }
-
-        if (job.getEnv().getRewardBalancePeriod() != 1) {
-            instructions.add(var("REWARD_BALANCE_PERIOD", String.valueOf(job.getEnv().getRewardBalancePeriod())));
         }
 
         if (job.getEnv().isLongerTraining()) {
